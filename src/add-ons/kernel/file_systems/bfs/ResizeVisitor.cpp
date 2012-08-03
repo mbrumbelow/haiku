cc19203b4f7d6ec490bbe5ae5914a239542da524
ahenriksson <sausageboy@gmail.com>
2012-08-03 11:04:34 +0200

    Added code to update the block cache size
    Change-Id: I75a6fa87bd2054b3fd6d31b1a664089fbc4c2792

--- src/add-ons/kernel/file_systems/bfs/ResizeVisitor.cpp
+++ src/add-ons/kernel/file_systems/bfs/ResizeVisitor.cpp
@@ -305,6 +305,11 @@ ResizeVisitor::_ResizeVolume()
 	if (status != B_OK)
 		return status;
 
+	// change size of block cache
+	status = GetVolume()->ResizeBlockCache(fNumBlocks);
+	if (status != B_OK)
+		return status;
+
 	// we flush the log before updating file system size, so nothing
 	// on the disk remains unmoved 
 	status = GetVolume()->GetJournal(0)->FlushLogAndBlocks();
