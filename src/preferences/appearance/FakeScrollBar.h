2125e69da8f8122102c16f7dc2058b0f6836cc20
John Scipione <jscipione@gmail.com>
2015-01-23 19:06:28 -0500

    Appearance Prefs: Add scroll bar knobs setting
    Align arrow style and knob style boxes, reduce widths to fit.
    
    FakeScrollBar::SetValue() allow multiple sets of radio buttons - one
    for arrow style: single/double, one for knob style: none/dots/lines.
    
    Compose FakeScrollBar height.
    
    Fixes #9137
    
    Change-Id: I579f4ec7470b7e1e83a76213048b0b09ce72264b

--- src/preferences/appearance/FakeScrollBar.h
+++ src/preferences/appearance/FakeScrollBar.h
@@ -1,10 +1,10 @@
 /*
- *  Copyright 2010-2012 Haiku, Inc. All rights reserved.
+ *  Copyright 2010-2020 Haiku, Inc. All rights reserved.
  *  Distributed under the terms of the MIT license.
  *
  *	Authors:
- *		DarkWyrm <bpmagic@columbus.rr.com>
- *		John Scipione <jscipione@gmail.com>
+ *		DarkWyrm, bpmagic@columbus.rr.com
+ *		John Scipione, jscipione@gmail.com
  */
 #ifndef FAKE_SCROLL_BAR_H
 #define FAKE_SCROLL_BAR_H
@@ -16,13 +16,10 @@
 class FakeScrollBar : public BControl {
 public:
 							FakeScrollBar(bool drawArrows, bool doubleArrows,
-								BMessage* message);
+								uint32 knobStyle, BMessage* message);
 							~FakeScrollBar(void);
 
 	virtual	void			MouseDown(BPoint point);
-	virtual	void			MouseMoved(BPoint point, uint32 transit,
-								const BMessage *message);
-	virtual	void			MouseUp(BPoint point);
 
 	virtual	void			Draw(BRect updateRect);
 
@@ -34,12 +31,10 @@ public:
 			void			SetFromScrollBarInfo(const scroll_bar_info &info);
 
 private:
-			void			_DrawArrowButton(int32 direction, BRect r,
-								const BRect& updateRect);
-
 			bool			fDrawArrows;
 			bool			fDoubleArrows;
-			int32			fKnobStyle;
+			uint32			fKnobStyle;
 };
+
 
 #endif	// FAKE_SCROLL_BAR_H
