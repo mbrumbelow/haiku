588e007daa983d9f62280a9b5678d0a75403d48a
Domonkos Lezsák <lezsakdomi1@gmail.com>
2017-12-06 15:46:18 +0100

    Improve the `setlocale' command
    Now it allows the user to set the system locale with syntax similar to the
    following:
    
      setlocale en
      setlocale en_US -v posix
      setlocale bs -s cyrl
      setlocale bs_BA -s cyrl
    
    Even multiple locales could be set:
    
      setlocale bs_BA_Cyrl en_GB en_US
    
    It also supports the -l flag to list all available locales, but that
    functionality is improved:
    
    - By default, lists only possible locale codes
    - Introduced the -d (--details) flag which simulates the same
      functionality as the previous version
    
    Two (main) additional changes in usage:
    - Introduced the -h (--help) flag
    - Added long options
    - Added a lot of user-friendly error messages
    
    The code is almost completly rewritten and reformatted

--- src/bin/setlocale.cpp
+++ src/bin/setlocale.cpp
@@ -3,9 +3,11 @@
  * Distributed under the terms of the MIT License.
  *
  * Authors:
+ *		Domonkos Lezsák, lezsakdomi1@gmail.com
  *		Andrew Tockman, andy@keyboardfire.com
  */
 
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -18,7 +20,8 @@
 
 using BPrivate::MutableLocaleRoster;
 
-bool matches(const char* a, const char* b)
+bool
+matches(const char* a, const char* b)
 {
 	if (a == NULL && b == NULL)
 		return true;
@@ -28,104 +31,204 @@ bool matches(const char* a, const char* b)
 }
 
 
-int main(int argc, char **argv)
+static const char* kHelpMessage =
+	"Set system-wide locale\n"
+	"\n"
+	"Arguments for long options are mandatory for short options too.\n"
+	"Arguments for getting help:\n"
+	"  -h, --help    Print usage message, this help text and exit\n"
+	"\n"
+	"Arguments for listing locales:\n"
+	"  -l, --list    Switch to list mode.\n"
+	"  -d, --details List in a long, detailed format:\n"
+	"                code<TAB>language<TAB>country<TAB>script<TAB>variant<TAB>"
+		"name\n"
+	"\n"
+	"Arguments for setting locale:\n"
+	"  -c, --country Specify country for locale\n"
+	"  -s, --script  Specify script for locale\n"
+	"  -x, --variant Specify language variant\n"
+	"\n"
+	" Please note, that these can be specified using underscores too.\n"
+	" Example:\n"
+	"   setlocale en_US\n"
+	"   setlocale az_Latn_AZ\n"
+	" When multiple locales are specified, the seconds gets set as secondary, "
+		"the third as tertiary, etc.\n"
+;
+static const char* kListModeButTrailingArgsMessage =
+	"Error: List mode specified, but it seems like locales too.\n";
+static const char* kLocaleNotFound =
+	"Warning: Locale %s not found.\n";
+static const char* kNoCodeSpecifiedMessage =
+	"Error: No language code specified.\n";
+static const char* kUnknownArgMessage = "Error: Unknown argument: -%c\n";
+static const char* kUsageMessage =
+	"Usage: setlocale -l|--list [-d|--details]\n"
+	"or     setlocale -h|--help\n"
+	"or     setlocale locale_code[ locale_code[ locale_code ...]] "
+		"[-c|--country country] [-s|--script script] [-v|--variant variant]\n"
+;
+
+
+void
+listLocales(bool detailed = false)
 {
-	const char* queryCode = NULL;
-	const char* queryCountry = NULL;
-	const char* queryScript = NULL;
-	const char* queryVariant = NULL;
-	bool list = false, err = false;
-
-	// parse command line
-	for (int i = 1; i < argc; ++i) {
-		if (*argv[i] == '-') {
-			// parse a command line flag
-			if (!strcmp(argv[i], "--list"))
-				list = true;
-			else if (argv[i][1] && !argv[i][2]) {
-				// found a single character flag
-				if (argv[i][1] == 'l')
-					list = true;
-				else if (argv[i][1] == 's')
-					queryScript = argv[++i];
-				else if (argv[i][1] == 'v')
-					queryVariant = argv[++i];
-				else {
-					err = true;
-					break;
-				}
-			} else
-				err = false; // unknown flag
-		} else if (queryCode == NULL)
-			// bare argument; fill up code and then country, erroring on
-			// anything more than two arguments
-			queryCode = argv[i];
-		else if (queryCountry == NULL)
-			queryCountry = argv[i];
-		else {
-			err = true;
-			break;
-		}
-	}
-
-	// check for proper command line, requiring exactly one of list or query
-	if (err || ((queryCode == NULL) ^ list)) {
-		fprintf(stderr,
-			"Usage:\n"
-			"  setlocale -l|--list\n"
-			"  setlocale language [country] [-s script] [-v variant]\n");
-		return 1;
-	}
-
 	// query for all available languages
 	BMessage languages;
 	BLocaleRoster::Default()->GetAvailableLanguages(&languages);
 
 	const char* id;
-	for (int32 i = 0; languages.FindString("language", i, &id) == B_OK; ++i) {
-		BLanguage* language;
-		if (BLocaleRoster::Default()->GetLanguage(id, &language) == B_OK) {
-			// extract all the relevant information about the language
-			BString name;
-			language->GetNativeName(name);
-			const char* code = language->Code();
-			const char* country = language->CountryCode();
-			const char* script = language->ScriptCode();
-			const char* variant = language->Variant();
+	for (int32 i = 0; languages.FindString("language", i, &id) == B_OK; i++)
+	{
+		fputs(id, stdout);
+		if (detailed) {
+			BLanguage* language;
+			if (BLocaleRoster::Default()->GetLanguage(id, &language) == B_OK)
+			{
+				// extract all the relevant information about the language
+				const char* code = language->Code();
+				const char* country = language->CountryCode();
+				const char* script = language->ScriptCode();
+				const char* variant = language->Variant();
+				BString name; language->GetNativeName(name);
 
-			// if we're listing, print the information
-			if (list)
-				printf("%s\t%s\t%s\t%s\t%s\n",
-					code,
-					country ? country : "",
-					script ? script : "",
-					variant ? variant : "",
-					name.String());
-			else if (!strcasecmp(code, queryCode)
-				&& matches(country, queryCountry)
-				&& matches(script, queryScript)
-				&& matches(variant, queryVariant)) {
-				// found a match! set the new locale
-				BMessage preferred;
-				preferred.AddString("language", id);
-				MutableLocaleRoster::Default()->
-					SetPreferredLanguages(&preferred);
-				printf("Locale successfully set to %s\n", name.String());
+				// print the information
+				putchar('\t');
+				printf("%s\t", code);
+				printf("%s\t", country);
+				printf("%s\t", script ? script : "");
+				printf("%s\t", variant ? variant : "");
+				printf("%s", name.String());
+
 				delete language;
-				return 0;
+			} else
+				fprintf(stderr, "Failed to get BLanguage for %s\n", id);
+		}
+		putchar('\n');
+	}
+}
+
+
+int
+main(const int argc, char* const argv[])
+{
+	bool queryList = false;
+	bool queryDetails = false;
+	const char* queryCountry = NULL;
+	const char* queryScript = NULL;
+	const char* queryVariant = NULL;
+
+	// parse command line
+	{
+		static const struct option longopts[] = {
+			{ "help", no_argument, NULL, 'h' },
+			{ "list", no_argument, NULL, 'l' },
+			{ "details", no_argument, NULL, 'd' },
+			{ "country", required_argument, NULL, 'c' },
+			{ "script", required_argument, NULL, 's' },
+			{ "variant", required_argument, NULL, 'x' },
+		};
+		const char* optstring = "hldc:s:x:";
+		
+		for (int c = getopt_long(argc, argv, optstring, longopts, NULL);
+			c != -1; c = getopt_long(argc, argv, optstring, longopts, NULL))
+		{
+			switch (c)
+			{
+				case 'h':
+					fputs(kUsageMessage, stdout);
+					fputs(kHelpMessage, stdout);
+					return 0;
+
+				case 'l':
+					queryList = true;
+					break;
+
+				case 'd':
+					queryDetails = true;
+					break;
+
+				case 'c':
+					queryCountry = optarg;
+					break;
+
+				case 's':
+					queryScript = optarg;
+					break;
+
+				case 'x':
+					queryVariant = optarg;
+					break;
+
+				default:
+					// error
+					fprintf(stderr, kUnknownArgMessage, c);
+					fputc('\n', stderr);
+					fputs(kUsageMessage, stderr);
+					return 1;
+			}
+		}
+
+		// Process list mode
+		if (queryList) {
+			if (optind != argc) {
+				fputs(kListModeButTrailingArgsMessage, stderr);
+				fputc('\n', stderr);
+				fputs(kUsageMessage, stderr);
+				return 1;
+			}
+			listLocales(queryDetails);
+			return true;
+		}
+
+		// Process language codes
+		{
+			// Check for number of supplimentary arguments
+			if (optind == argc) {
+				fputs(kNoCodeSpecifiedMessage, stderr);
+				fputc('\n', stderr);
+				fputs(kUsageMessage, stderr);
+				return 1;
 			}
 
-			delete language;
-		} else
-			fprintf(stderr, "Failed to get BLanguage for %s\n", id);
+			// And do the job
+			{
+				BMessage preferred;
+				for (int i = optind; i < argc; i++)
+				{
+					BString targetCode(argv[i]);
+
+					if (queryCountry != NULL)
+						targetCode += BString(queryCountry).Prepend('_', 1);
+
+					if (queryVariant != NULL)
+						targetCode += BString(queryVariant).Prepend('_', 1);
+
+					if (queryScript != NULL)
+						targetCode += BString(queryScript).Prepend('_', 1);
+
+					{
+						BLanguage* language;
+						if (BLocaleRoster::Default()->GetLanguage(
+								targetCode.String(), &language) == B_OK) {
+							// found a match! set the new locale
+							preferred.AddString("language",
+								targetCode.String());
+						} else {
+							fprintf(stderr, kLocaleNotFound,
+								targetCode.String());
+						}
+					}
+				}
+				MutableLocaleRoster::Default()->
+					SetPreferredLanguages(&preferred);
+				return 0;
+			}
+		}
 	}
 
-	// if we've gotten this far and the -l flag wasn't passed, the requested
-	// locale wasn't found
-	if (!list) {
-		fprintf(stderr, "Locale not found\n");
-		return 1;
-	}
 
-	return 0;
+	// if we've gotten this far, then some error happened
+	return 2;
 }
