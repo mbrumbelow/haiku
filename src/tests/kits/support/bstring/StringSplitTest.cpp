6d4b8e6db64f6de57686f50dfc7e6f5313d29a1e
krish_iyer <krishnaniyer97@gmail.com>
2018-03-27 18:28:33 +0530

    Support Kit: Changed CPPUNIT_ASSERT -> CPPUNIT_ASSERT_EQUAL and fixed coding style ticket : #14048

--- src/tests/kits/support/bstring/StringSplitTest.cpp
+++ src/tests/kits/support/bstring/StringSplitTest.cpp
@@ -1,60 +1,55 @@
 #include "StringSplitTest.h"
-#include "cppunit/TestCaller.h"
+#include "cppunit/ TestCaller.h"
 #include <String.h>
 #include <StringList.h>
 
-StringSplitTest::StringSplitTest(std::string name) :
-		BTestCase(name)
-{
-}
-
-
+StringSplitTest::StringSplitTest(std::string name) :BTestCase(name)
+{}
 
 StringSplitTest::~StringSplitTest()
-{
-}
-
+{}
 
 void
 StringSplitTest::PerformTest(void)
 {
-	BString *str1;
+	BString *(str1);
 
 	NextSubTest();
 	BStringList stringList1;
 	str1 = new BString("test::string");
 	str1->Split(":", true, stringList1);
-	CPPUNIT_ASSERT(stringList1.CountStrings() == 2);
+	CPPUNIT_ASSERT_EQUAL(2, stringList1.CountStrings());
 	delete str1;
 
 	NextSubTest();
 	BStringList stringList2;
 	str1 = new BString("test::string");
 	str1->Split("::", true, stringList2);
-	CPPUNIT_ASSERT(stringList2.CountStrings() == 2);
+	CPPUNIT_ASSERT_EQUAL(2, stringList2.CountStrings());
 	delete str1;
 
 	NextSubTest();
 	BStringList stringList3;
 	str1 = new BString("test::string");
 	str1->Split("::", false, stringList3);
-	CPPUNIT_ASSERT(stringList3.CountStrings() == 2);
+	CPPUNIT_ASSERT_EQUAL(2, stringList3.CountStrings());
 	delete str1;
 
 	NextSubTest();
 	BStringList stringList4;
 	str1 = new BString("test::string");
 	str1->Split(":", false, stringList4);
-	CPPUNIT_ASSERT(stringList4.CountStrings() == 3);
+	CPPUNIT_ASSERT_EQUAL(3, stringList4.CountStrings());
 	delete str1;
 
 }
 
 
-CppUnit::Test *StringSplitTest::suite(void)
+CppUnit::Test *(StringSplitTest::suite(void))
 {
 	typedef CppUnit::TestCaller<StringSplitTest>
 		StringSplitTestCaller;
 
-	return(new StringSplitTestCaller("BString::Split Test", &StringSplitTest::PerformTest));
+	return(new StringSplitTestCaller("BString::Split Test", 
+		&StringSplitTest::PerformTest));
 }
