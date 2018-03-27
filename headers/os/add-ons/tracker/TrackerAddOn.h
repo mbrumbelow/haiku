d6121d986f5f2428e120e5b190718bd34968f363
Hrishi Hiraskar <hrishihiraskar@gmail.com>
2018-03-27 10:17:04 +0530

    Tracker Addon: Passing top-level menu to addon

--- headers/os/add-ons/tracker/TrackerAddOn.h
+++ headers/os/add-ons/tracker/TrackerAddOn.h
@@ -9,10 +9,11 @@ struct entry_ref;
 class BMessage;
 class BMenuItem;
 
-extern "C" void process_refs(entry_ref directory, BMessage* refs,
-	void* reserved);
-
-extern "C" void populate_menu(entry_ref directory, BMessage* refs,
-	BMenuItem* item);
+extern "C" {
+	void process_refs(entry_ref directory, BMessage* refs,
+		void* reserved);
+	void populate_menu(entry_ref directory, BMessage* refs,
+		BMenuItem* item);
+}
 
 #endif	// _TRACKER_ADDON_H
