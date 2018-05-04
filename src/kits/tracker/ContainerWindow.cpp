74dff231575e0aacfe95f4d31ef1ae6d1242233c
Hrishi Hiraskar <hrishihiraskar@gmail.com>
2018-05-04 15:26:46 +0000

    Dynamic Tracker Menu: refs and dir_ref added to message

--- src/kits/tracker/ContainerWindow.cpp
+++ src/kits/tracker/ContainerWindow.cpp
@@ -180,7 +180,7 @@ CompareLabels(const BMenuItem* item1, const BMenuItem* item2)
 
 
 static int32
-AddOnMenuGenerate(entry_ref addonRef, BMenu* menu, BHandler* handler)
+AddOnMenuGenerate(entry_ref addonRef, BMenu* menu, BContainerWindow* window)
 {
 	BEntry entry(&addonRef);
 	BPath path;
@@ -196,12 +196,11 @@ AddOnMenuGenerate(entry_ref addonRef, BMenu* menu, BHandler* handler)
 				(void**)&populateMenu);
 
 			if (result >= 0) {
-				BMessage* message = new BMessage(B_TRACKER_ADDON_MESSAGE);
+				BMessage* message = window->AddOnMessage(B_TRACKER_ADDON_MESSAGE);
 				message->AddRef("addon_ref", &addonRef);
 
 				// call add-on code
-				printf("handler %d\n", (int)handler);
-				(*populateMenu)(message, menu, handler);
+				(*populateMenu)(message, menu, window->PoseView());
 
 				unload_add_on(addonImage);
 				return B_OK;
@@ -232,8 +231,7 @@ AddOneAddon(const Model* model, const char* name, uint32 shortcut,
 
 	// hrishi: here
 	entry_ref addonRef = *model->EntryRef();
-	AddOnMenuGenerate(addonRef, menu, window->PoseView());
-	// menu->SetTargetForItems(window->PoseView());
+	AddOnMenuGenerate(addonRef, menu, window);
 
 	if (primary)
 		params->primaryList->AddItem(item);
@@ -3308,6 +3306,27 @@ BContainerWindow::UpdateMenu(BMenu* menu, UpdateMenuContext context)
 }
 
 
+BMessage*
+BContainerWindow::AddOnMessage(int32 what)
+{
+	BMessage* message = new BMessage(what);
+
+	// add selected refs to message
+	BObjectList<BPose>* selectionList = PoseView()->SelectionList();
+
+	int32 index = 0;
+	BPose* pose;
+	while ((pose = selectionList->ItemAt(index++)) != NULL)
+		message->AddRef("refs", pose->TargetModel()->EntryRef());
+
+	message->AddRef("dir_ref", TargetModel()->EntryRef());
+
+	message->AddMessenger("TrackerViewToken", BMessenger(PoseView()));
+
+	return message;
+}
+
+
 void
 BContainerWindow::LoadAddOn(BMessage* message)
 {
@@ -3328,15 +3347,7 @@ BContainerWindow::LoadAddOn(BMessage* message)
 	}
 
 	// add selected refs to message
-	BMessage* refs = new BMessage(B_REFS_RECEIVED);
-	BObjectList<BPose>* selectionList = PoseView()->SelectionList();
-
-	int32 index = 0;
-	BPose* pose;
-	while ((pose = selectionList->ItemAt(index++)) != NULL)
-		refs->AddRef("refs", pose->TargetModel()->EntryRef());
-
-	refs->AddMessenger("TrackerViewToken", BMessenger(PoseView()));
+	BMessage* refs = AddOnMessage(B_REFS_RECEIVED);
 
 	LaunchInNewThread("Add-on", B_NORMAL_PRIORITY, &AddOnThread, refs,
 		addonRef, *TargetModel()->EntryRef());
@@ -3420,23 +3431,13 @@ BContainerWindow::_PassMessageToAddOn(BMessage* message)
 	if (result == B_OK) {
 		image_id addonImage = load_add_on(path.Path());
 		if (addonImage >= 0) {
-			void (*messageReceived)(entry_ref, BMessage*);
+			void (*messageReceived)(BMessage*);
 			result = get_image_symbol(addonImage, "message_received", 2,
 				(void**)&messageReceived);
 
 			if (result >= 0) {
-				// add selected refs to message
-				BObjectList<BPose>* selectionList = PoseView()->SelectionList();
-
-				int32 index = 0;
-				BPose* pose;
-				while ((pose = selectionList->ItemAt(index++)) != NULL)
-					message->AddRef("refs", pose->TargetModel()->EntryRef());
-
-				message->AddMessenger("TrackerViewToken", BMessenger(PoseView()));
-
 				// call add-on code
-				(*messageReceived)(*TargetModel()->EntryRef(), message);
+				(*messageReceived)(message);
 
 				unload_add_on(addonImage);
 				return B_OK;
