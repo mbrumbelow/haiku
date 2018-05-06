0a430f3eff0e280e8e993c337a93957cd55cdaf1
krish_iyer <krishnaniyer97@gmail.com>
2018-05-06 18:35:31 +0530

    Tried adding support for mmc bus and defining resgitering child device

--- src/add-ons/kernel/busses/sdhci/sdhci_pci.cpp
+++ src/add-ons/kernel/busses/sdhci/sdhci_pci.cpp
@@ -8,18 +8,69 @@
 #include "sdhci_pci.h"
 
 #ifdef TRACE_SDHCI
-#	define TRACE(x...) dprintf("\33[33mvirtio_pci:\33[0m " x)
+#	define TRACE(x...) dprintf("\33[33msdhci_pci:\33[0m " x)
 #else
 #	define TRACE(x...) ;
 #endif
-#define TRACE_ALWAYS(x...)	dprintf("\33[33mvirtio_pci:\33[0m " x)
-#define ERROR(x...)			dprintf("\33[33mvirtio_pci:\33[0m " x)
+#define TRACE_ALWAYS(x...)	dprintf("\33[33msdhci_pci:\33[0m " x)
+#define ERROR(x...)			dprintf("\33[33msdhci_pci:\33[0m " x)
 #define CALLED(x...)		TRACE("CALLED %s\n", __PRETTY_FUNCTION__)
 
-#define SDHCI_PCI_DEVICE_MODULE_NAME "busses/mmc/sdhci_pci/driver_v1"
+#define SDHCI_PCI_DEVICE_MODULE_NAME "busses/mmc/sdhci_pci/device/v1"
 
+typedef struct {
+	sdhci_sim sim;
+	uint16 queue;
+} sdhci_pci_queue_cookie;
+
+typedef struct {
+	pci_device_module_info* pci;
+	pci_device* device;
+	addr_t base_addr;
+	uint8 irq;
+	sdhci_irq_type irq_type;
+	sdhci_sim sim;
+	uint16 queue_count;
+
+	device_node* node;
+	pci_info info;
+
+	sdhci_pci_queue_cookie *cookies;
+} sdhci_pci_sim_info;
 
 device_manager_info* gDeviceManager;
+
+int32
+sdhci_pci_queue_interrupt(void *data)
+{
+	sdhci_pci_queue_cookie* cookie = (sdhci_pci_queue_cookie*)data;
+	gSDHCI->queue_interrupt_handler(cookie->sim, cookie->queue);
+	return B_HANDLED_INTERRUPT;
+
+}
+
+int32
+sdhci_pci_interrupt(void *data)
+{
+	sdhci_pci_sim_info* bus = (sdhci_pci_sim_info*)data;
+	uint8 isr = bus->pci->read_io_8(bus->device,
+		bus->base_addr) // incomplete
+}
+
+static void
+bus_removed(void* bus_cookie)
+{
+	return;
+}
+
+static void
+uninit_bus(void* bus_cookie) {}
+
+static void 
+init_bus(void *bus_init){}
+
+
+
 
 static status_t
 init_device(device_node* node, void** device_cookie)
@@ -30,7 +81,15 @@ init_device(device_node* node, void** device_cookie)
 }
 
 static status_t
-register_child_devices(void* cookie){}
+register_child_devices(void* cookie)
+{
+	CALLED();
+	device_node* node = (device_node*)cookie;
+	device_node* parent = gDeviceManager-> get_parent_node(node);
+	pci_device_module_info *pci;
+	pci_device* device; // incomplete 
+
+}
 
 static status_t
 register_device(device_node* parent)
@@ -52,6 +111,8 @@ supports_device(device_node* parent)
 	CALLED();
 	const char* bus;
 	uint16 vendorID, deviceID;
+
+	TRACE("Supports device started, first function has been loaded ")
 
 	if (gDeviceManager->get_attr_string(parent, B_DEVICE_BUS, &bus, false) != B_OK
 		|| gDeviceManager->get_attr_uint16(parent, B_DEVICE_VENDOR_ID,
@@ -90,6 +151,23 @@ supports_device(device_node* parent)
 	return 0;
 }
 
+static sdhci_sim_interface gSDHCIPCIDeviceModule = {
+	{
+		{
+			SDHCI_PCI_SIM_MODULE_NAME,
+			0,
+			NULL
+		},
+
+		NULL,
+		NULL,
+		init_bus,
+		uninit_bus,
+		NULL,
+		NULL,
+		bus_removed
+	}
+}
 
 module_dependency module_dependencies[] = {
 //	{ SDHCI_FOR_CONTROLLER_MODULE_NAME, (module_info**)&gSDHCI },
