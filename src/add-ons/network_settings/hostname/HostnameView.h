6b3dabbcc7910a4739a47073c9a1529d220ee1a3
Rob Gill <rrobgill@protonmail.com>
2019-01-11 11:40:24 +0000

    networking: Hostname viewing & setting
    addon/network settings/hostname
    
    	allowing the hostname for the system to be set by the user using
    	the preferences gui

---
+++ src/add-ons/network_settings/hostname/HostnameView.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2014-2019 Haiku Inc. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ *		Adrien Destugues, pulkomandy@pulkomandy.tk
+ *		Rob Gill, rrobgill@protonmail.com
+ */
+#ifndef HOSTNAME_VIEW_H
+#define HOSTNAME_VIEW_H
+
+
+#include <NetworkSettingsAddOn.h>
+#include <StringList.h>
+#include <View.h>
+
+
+using namespace BNetworkKit;
+
+class BButton;
+class BTextControl;
+
+
+class HostNameView : public BView {
+public:
+								HostnameView(BNetworkSettingsItem* item);
+								~HostnameView();
+
+			status_t			Revert();
+			bool				IsRevertable() const;
+
+	virtual	void				AttachedToWindow();
+	virtual	void				MessageReceived(BMessage* message);
+
+private:
+			status_t			_LoadHostName();
+			status_t			_SaveHostName();
+
+private:
+			BNetworkSettingsItem*
+								fItem;
+			BTextControl*		fHostName;
+
+			BButton*			fApplyButton;
+};
+
+
+#endif // HOSTNAME_VIEW_H
