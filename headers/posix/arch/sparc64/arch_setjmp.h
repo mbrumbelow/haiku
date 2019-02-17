dd3d08dd160f812273bcb30792c700a0e52d8b84
Adrien Destugues <pulkomandy@gmail.com>
2019-02-17 13:42:25 +0100

    sparc: add defines and minimum set of required files
    This gets the bootstrap devel package to build, so bootstrap can
    proceed. Implementation is probably nonsense at this point.
    
    Change-Id: I10876efbb54314b864c0ad951152757cdb2fd366

---
+++ headers/posix/arch/sparc64/arch_setjmp.h
@@ -0,0 +1,9 @@
+/*
+ * Copyright 2005-2012 Haiku, Inc. All Rights Reserved.
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef _ARCH_SETJMP_H_
+#define _ARCH_SETJMP_H_
+
+
+#endif	/* _ARCH_SETJMP_H_ */
