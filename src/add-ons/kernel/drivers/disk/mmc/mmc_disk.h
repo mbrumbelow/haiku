2973e1e65453b541d9d1f624f23a7dee6c7a91b6
Adrien Destugues <pulkomandy@pulkomandy.tk>
2019-02-19 20:12:08 +0100

    Initial work for the mmc_disk driver
    The driver does nothing yet. Following commits add actual MMC/SD
    communication to it.
    
    Change-Id: Ife844a62f3846c0a780259e9a3a08195e2fd965e

--- src/add-ons/kernel/drivers/disk/mmc/mmc_disk.h
+++ src/add-ons/kernel/drivers/disk/mmc/mmc_disk.h
@@ -1,9 +1,10 @@
 /*
- * Copyright 2018 Haiku, Inc. All rights reserved.
+ * Copyright 2018-2019 Haiku, Inc. All rights reserved.
  * Distributed under the terms of the MIT License.
  *
  * Authors:
  *		B Krishnan Iyer, krishnaniyer97@gmail.com
+ *		Adrien Destugues, pulkomandy@pulkomandy.tk
  */
 #ifndef _MMC_DISK_H
 #define _MMC_DISK_H
@@ -12,13 +13,27 @@
 #include <device_manager.h>
 #include <KernelExport.h>
 
+#include <stdint.h>
 
-#define SDHCI_DEVICE_TYPE_ITEM "sdhci/type"
+typedef struct {
+	driver_module_info stdops;
+
+	// TODO specific ops provided by MMC bus go here (for sending commands, etc)
+} mmc_driver_interface;
 
 typedef struct {
 	device_node* 	node;
-	status_t 		media_status;
+	mmc_driver_interface* mmc;
+	void* mmc_device;
+
+	size_t block_size;
+	uint32_t capacity;
 } mmc_disk_driver_info;
+
+
+typedef struct {
+	mmc_disk_driver_info* info;
+} mmc_disk_handle;
 
 
 #endif /*_MMC_DISK_H*/
