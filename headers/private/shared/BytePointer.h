bd4117ccd25a33a1cd17b4a91361818d7f2c808b
PulkoMandy <pulkomandy@pulkomandy.tk>
2019-02-23 18:25:31 +0100

    Add BytePointer to help with pointer conversions
    This behaves mostly like a pointer, but pointer maths works in bytes,
    not the native object size.
    
    This allows to generate misaligned pointers, so use with caution. It is
    not possible to generate such pointers by casting on sparc, the compiler
    rejects them because the architecture doesn't support unaligned access
    (and would generate a SIGBUS).
    
    This can be used when creating the address of an object from a pointer +
    offset. We assume the caller knows what he is doing and will make sure
    the alignment is correct. We could add an explicit alignment check when
    built in debug mode, maybe?
    
    Change-Id: I6a8681a398345f0c7d419a2cfe7244d972ffa62f

---
+++ headers/private/shared/BytePointer.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2019, Adrien Destugues, pulkomandy@pulkomandy.tk.
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef _BYTE_POINTER_H
+#define _BYTE_POINTER_H
+
+
+#include <stdlib.h>
+
+
+// Behaves like a char* pointer, but -> return the right pointed type.
+// Assumes the offsets passed to + and += maintain the alignment for the type
+template<class T> union BytePointer {
+	T* data;
+	char* address;
+
+	BytePointer(void* base) { address = (char*)base; }
+
+	T* operator&() { return data; }
+	T* operator->() { return data; }
+	void operator+=(size_t offset) { address += offset; }
+	char* operator+(size_t offset) const { return address + offset; }
+};
+
+
+#endif
