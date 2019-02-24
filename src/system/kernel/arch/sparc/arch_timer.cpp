92afd9f85a92dea2d95b20f373d6b27e61faa251
PulkoMandy <pulkomandy@pulkomandy.tk>
2019-02-24 14:20:56 +0100

    sparc: add stubs for kernel
    Now we at least try to link the kernel (with lots of undefined
    references)

---
+++ src/system/kernel/arch/sparc/arch_timer.cpp
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2019, Haiku Inc. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ *		Adrien Destugues <pulkomandy@pulkomandy.tk>
+ */
+
+
+#include <kernel.h>
+#include <debug.h>
+#include <timer.h>
+
+
+bigtime_t
+system_time(void)
+{
+	// TODO
+	return 0;
+}
---
+++ src/system/kernel/arch/sparc/arch_vm.cpp
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2001, Travis Geiselbrecht. All rights reserved.
+ * Copyright 2003-2005, Axel Dörfler, axeld@pinc-software.de.
+ * Copyright 2019, Adrien Destugues, pulkomandy@pulkomandy.tk
+ * Distributed under the terms of the MIT License.
+ */
+
+#include <vm/vm.h>
+#include <vm/VMAddressSpace.h>
+#include <arch/vm.h>
+
+
+//#define TRACE_ARCH_VM
+#ifdef TRACE_ARCH_VM
+#	define TRACE(x) dprintf x
+#else
+#	define TRACE(x) ;
+#endif
+
+
+status_t
+arch_vm_init(kernel_args *args)
+{
+	return B_OK;
+}
+
+
+status_t
+arch_vm_init_post_area(kernel_args *args)
+{
+	return B_OK;
+}
+
+
+status_t
+arch_vm_init_post_modules(kernel_args *args)
+{
+	return B_OK;
+}
+
+
+status_t
+arch_vm_init_end(kernel_args *args)
+{
+	TRACE(("arch_vm_init_end(): %lu virtual ranges to keep:\n",
+		args->arch_args.num_virtual_ranges_to_keep));
+
+	for (int i = 0; i < (int)args->arch_args.num_virtual_ranges_to_keep; i++) {
+		addr_range &range = args->arch_args.virtual_ranges_to_keep[i];
+
+		TRACE(("  start: %p, size: 0x%lx\n", (void*)range.start, range.size));
+
+#if 0
+		// skip ranges outside the kernel address space
+		if (!IS_KERNEL_ADDRESS(range.start)) {
+			TRACE(("    no kernel address, skipping...\n"));
+			continue;
+		}
+
+		phys_addr_t physicalAddress;
+		void *address = (void*)range.start;
+		if (vm_get_page_mapping(VMAddressSpace::KernelID(), range.start,
+				&physicalAddress) != B_OK)
+			panic("arch_vm_init_end(): No page mapping for %p\n", address);
+		area_id area = vm_map_physical_memory(VMAddressSpace::KernelID(),
+			"boot loader reserved area", &address,
+			B_EXACT_ADDRESS, range.size,
+			B_KERNEL_READ_AREA | B_KERNEL_WRITE_AREA,
+			physicalAddress, true);
+		if (area < 0) {
+			panic("arch_vm_init_end(): Failed to create area for boot loader "
+				"reserved area: %p - %p\n", (void*)range.start,
+				(void*)(range.start + range.size));
+		}
+#endif
+	}
+
+#if 0
+	// Throw away any address space mappings we've inherited from the boot
+	// loader and have not yet turned into an area.
+	vm_free_unused_boot_loader_range(0, 0xffffffff - B_PAGE_SIZE + 1);
+#endif
+
+	return B_OK;
+}
