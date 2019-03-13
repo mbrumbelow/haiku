a3d5bac59c7bf9cc72ef6efd90671dcf4b721b23
Barrett17 <b.vitruvio@gmail.com>
2019-03-13 20:24:00 +0100

    Revert "Decoder: Make constructor protected"
    This reverts commit c09a589891a279061c7d88e305252909e2a41202.

--- headers/os/codec/DecoderPlugin.h
+++ headers/os/codec/DecoderPlugin.h
@@ -23,6 +23,9 @@ public:
 
 class Decoder {
 public:
+								Decoder();
+	virtual						~Decoder();
+
 	virtual	void				GetCodecInfo(media_codec_info* codecInfo) = 0;
 
 	// Setup get's called with the info data from Reader::GetStreamInfo
@@ -46,11 +49,6 @@ public:
 			void				SetChunkProvider(ChunkProvider* provider);
 
 	virtual status_t			Perform(perform_code code, void* data);
-
-protected:
-								Decoder();
-	virtual						~Decoder();
-
 
 private:
 			ChunkProvider*		fChunkProvider;
