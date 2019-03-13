b4139a011f23a9380e8f736c08d718af904d95e5
Barrett17 <b.vitruvio@gmail.com>
2019-03-13 20:23:38 +0100

    Revert "MetaData: Remove support for uint32 keys"
    This reverts commit b6802a953057055b3d87539d16818e811f238488.

--- src/kits/codec/MetaData.cpp
+++ src/kits/codec/MetaData.cpp
@@ -99,6 +99,13 @@ BMetaData::SetBool(const char* key, bool value)
 
 
 bool
+BMetaData::SetUInt32(const char* key, uint32 value)
+{
+	return fMessage->SetUInt32(key, value) == B_OK ? true : false;
+}
+
+
+bool
 BMetaData::SetInt64(const char* key, int64 value)
 {
 	return fMessage->SetInt64(key, value) == B_OK ? true : false;
@@ -116,6 +123,13 @@ bool
 BMetaData::GetBool(const char* key, bool* value) const
 {
 	return fMessage->FindBool(key, value) == B_OK ? true : false;
+}
+
+
+bool
+BMetaData::GetUInt32(const char* key, uint32* value) const
+{
+	return fMessage->FindUInt32(key, value) == B_OK ? true : false;
 }
 
 
