3ec96651a5f09957abccdc8e435d92f8deda7cee
Barrett17 <b.vitruvio@gmail.com>
2019-03-13 20:24:06 +0100

    Revert "ffmpeg: Move AVCodecWriter to BMetaData"
    This reverts commit 854c698a527abe0aa5151849b987f74504afe6be.

--- src/add-ons/media/plugins/ffmpeg/AVFormatWriter.cpp
+++ src/add-ons/media/plugins/ffmpeg/AVFormatWriter.cpp
@@ -425,7 +425,7 @@ AVFormatWriter::Init(const media_file_format* fileFormat)
 
 
 status_t
-AVFormatWriter::SetMetaData(BMetaData* data)
+AVFormatWriter::SetCopyright(const char* copyright)
 {
 	TRACE("AVFormatWriter::SetCopyright(%s)\n", copyright);
 
@@ -541,9 +541,9 @@ AVFormatWriter::FreeCookie(void* _cookie)
 
 
 status_t
-AVFormatWriter::SetMetaData(void* cookie, BMetaData* data)
+AVFormatWriter::SetCopyright(void* cookie, const char* copyright)
 {
-	TRACE("AVFormatWriter::SetMetaData(%p)\n", cookie);
+	TRACE("AVFormatWriter::SetCopyright(%p, %s)\n", cookie, copyright);
 
 	return B_NOT_SUPPORTED;
 }
