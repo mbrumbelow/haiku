91fb0f4b42e87ce86e890e490e996993f18c92d1
Barrett17 <b.vitruvio@gmail.com>
2019-03-13 20:24:09 +0100

    Revert "ffmpeg: Use new BMetaData API"
    This reverts commit c6a23a582742d93cc98e42e546558ba4fccd00e2.

--- src/add-ons/media/plugins/ffmpeg/AVFormatReader.h
+++ src/add-ons/media/plugins/ffmpeg/AVFormatReader.h
@@ -17,10 +17,12 @@ public:
 								AVFormatReader();
 								~AVFormatReader();
 
+	virtual	const char*			Copyright();
+
 	virtual	status_t			Sniff(int32* streamCount);
 
 	virtual	void				GetFileFormatInfo(media_file_format* mff);
-	virtual	status_t			GetMetaData(BMetaData* data);
+	virtual	status_t			GetMetaData(BMessage* _data);
 
 	virtual	status_t			AllocateCookie(int32 streamNumber,
 									void** cookie);
@@ -32,7 +34,7 @@ public:
 									size_t* infoSize);
 
 	virtual	status_t			GetStreamMetaData(void* cookie,
-									BMetaData* data);
+									BMessage* _data);
 
 	virtual	status_t			Seek(void* cookie, uint32 flags, int64* frame,
 									bigtime_t* time);
