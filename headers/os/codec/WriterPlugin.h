bc75628448f8c7b5396ea69a1f68521f2f803c4f
Barrett17 <b.vitruvio@gmail.com>
2019-03-13 20:24:25 +0100

    Revert "MediaWriter: Private stuff should stay private"
    This reverts commit 59125d3cedbd60772564bd5bedb6d4a2f7315549.

--- headers/os/codec/WriterPlugin.h
+++ headers/os/codec/WriterPlugin.h
@@ -47,6 +47,7 @@ private:
 	virtual void				_ReservedWriter4();
 	virtual void				_ReservedWriter5();
 
+public: // XXX for test programs only
 			void				Setup(BDataIO* target);
 
 			BDataIO*			fTarget;
