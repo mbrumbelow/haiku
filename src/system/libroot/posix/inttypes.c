69ca9eeb35aa1ac472e3cf46212b130c315f0140
Augustin Cavalier <waddlesplash@gmail.com>
2019-05-02 21:39:48 -0400

    libroot: Begin replacing bits with their musl equivalents.
    For now, only our homegrown strictly-POSIX-compliant string/stdlib
    functions, and one glibc file, have been replaced. This should be a
    relatively harmless starting set.

--- src/system/libroot/posix/inttypes.c
+++ src/system/libroot/posix/inttypes.c
@@ -9,30 +9,6 @@
 
 
 intmax_t
-imaxabs(intmax_t number)
-{
-	return number > 0 ? number : -number;
-}
-
-
-imaxdiv_t
-imaxdiv(intmax_t numer, intmax_t denom)
-{
-	imaxdiv_t result;
-
-	result.quot = numer / denom;
-	result.rem = numer % denom;
-
-	if (numer >= 0 && result.rem < 0) {
-		result.quot++;
-		result.rem -= denom;
-	}
-
-	return result;
-}
-
-
-intmax_t
 strtoimax(const char *string, char **_end, int base)
 {
 	return (intmax_t)strtoll(string, _end, base);
