5f6aa043e41fbfcd9e5272670048429b8fa5ba13
brjhaiku <brjhaiku@gmail.com>
2019-06-26 12:28:30 +0000

    Working on btrfs_write

--- src/add-ons/kernel/file_systems/btrfs/kernel_interface.cpp
+++ src/add-ons/kernel/file_systems/btrfs/kernel_interface.cpp
@@ -1,4 +1,5 @@
 /*
+ * Copyright 2019, Bharathi Ramana Joshi, joshibharathiramana@gmail.com
  * Copyright 2019, Les De Ridder, les@lesderid.net
  * Copyright 2017, Chế Vũ Gia Hy, cvghy116@gmail.com.
  * Copyright 2011, Jérôme Duval, korli@users.berlios.de.
@@ -479,6 +480,25 @@ status_t btrfs_create(fs_volume* volume, fs_vnode* dir, const char* name,
 
 	status = btrfs_open(_volume, inode, mode, _cookie);
 	return status;
+}
+
+
+status_t
+btrfs_write(fs_volume* _volume, fs_vnode* _node, void* _cookie, off_t pos,
+		const void* buffer, size_t* _length)
+{
+	Volume* volume = (Volume*)_volume->private_volume;
+	Inode* inode = (Inode*)_node->private_node;
+
+	if (volume->IsReadOnly())
+		return B_READ_ONLY_DEVICE;
+
+	if (pos < 0)
+		return B_BAD_VALUE;
+
+	if (!inode->IsFile())
+		return B_BAD_VALUE;
+
 }
 
 
