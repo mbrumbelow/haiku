eee51dbb29d65394619cd2fbbebc9b5c4f6a110d
brjhaiku <brjhaiku@gmail.com>
2019-07-04 05:57:22 +0530

    btrfs: implemented btrfs_write_fs_info
    Change-Id: I507fc4154f8a11235b6b6f8812c3b38924f533f7

--- src/add-ons/kernel/file_systems/btrfs/Volume.cpp
+++ src/add-ons/kernel/file_systems/btrfs/Volume.cpp
@@ -1,4 +1,5 @@
 /*
+ * Copyright 2019, Bharathi Ramana Joshi, joshibharathiramana@gmail.com
  * Copyright 2019, Les De Ridder, les@lesderid.net
  * Copyright 2017, Chế Vũ Gia Hy, cvghy116@gmail.com.
  * Copyright 2011, Jérôme Duval, korli@users.berlios.de.
@@ -15,6 +16,7 @@
 #include "BTree.h"
 #include "CachedBlock.h"
 #include "Chunk.h"
+#include "CRCTable.h"
 #include "DebugSupport.h"
 #include "ExtentAllocator.h"
 #include "Inode.h"
