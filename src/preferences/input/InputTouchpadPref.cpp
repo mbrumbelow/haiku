348035b1e7c2160f07068b2701ed09a107f984c4
Preetpal Kaur <preetpalok123@gmail.com>
2019-07-13 20:09:38 +0530

    Integrate PadBlocker in touchpad preferences
    - Add a slider to input the padblocking time
    - Import padblocker sources
    - Touchpad preferences generates PadBlocker settings
    
    Fixes https://dev.haiku-os.org/ticket/11673
    
    Change-Id: Ic88416215aabb1ae6aba79ff41cb55a7f0f8008d

--- src/preferences/input/InputTouchpadPref.cpp
+++ src/preferences/input/InputTouchpadPref.cpp
@@ -10,6 +10,7 @@
 #include "InputTouchpadPref.h"
 
 #include <List.h>
+#include <Entry.h>
 #include <FindDirectory.h>
 #include <File.h>
 #include <String.h>
@@ -67,6 +68,7 @@ TouchpadPref::UpdateSettings()
 	msg.AddInt16("scroll_ystepsize", fSettings.scroll_ystepsize);
 	msg.AddInt8("scroll_acceleration", fSettings.scroll_acceleration);
 	msg.AddInt8("tapgesture_sensibility", fSettings.tapgesture_sensibility);
+	msg.AddInt32("padblocker_threshold", fSettings.padblocker_threshold);
 
 	return fTouchPad->Control(MS_SET_TOUCHPAD_SETTINGS, &msg);
 }
