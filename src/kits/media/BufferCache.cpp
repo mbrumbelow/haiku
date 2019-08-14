4d6c3fcd60a885081e0dcff5abf09d0a24aa090a
Ryan Leavengood <leavengood@gmail.com>
2019-08-14 17:59:01 -0400

    Media Kit: Clear the BufferCache of buffers for disconnected clients
    Without this, the BufferCache keeps a reference to these buffers inside the
    media_addon_server until the media_addon_server quits, which is pretty much
    never.
    
    This was the bug no one thought could be fixed.
    
    Should fix #4954 and #14755, and possibly #13614 and #14047, though I think
    they may be something else.
    
    Switched from std::map to our HashMap to get something which works in gcc2 and
    gcc8.
    
    Change-Id: I26463899724b9d1520d97fec785e435f536eaf3d

--- src/kits/media/BufferCache.cpp
+++ src/kits/media/BufferCache.cpp
@@ -1,4 +1,5 @@
 /*
+ * Copyright 2019, Ryan Leavengood.
  * Copyright 2009, Axel Dörfler, axeld@pinc-software.de.
  * Copyright 2002, Marcus Overhagen. All Rights Reserved.
  * Distributed under the terms of the MIT License.
@@ -25,22 +26,23 @@ BufferCache::BufferCache()
 
 BufferCache::~BufferCache()
 {
-	for (BufferMap::iterator iterator = fMap.begin(); iterator != fMap.end();
-			iterator++) {
-		delete iterator->second;
+	BufferMap::Iterator iterator = fMap.GetIterator();
+	while (iterator.HasNext()) {
+		BufferMap::Entry entry = iterator.Next();
+		delete entry.value.buffer;
 	}
 }
 
 
 BBuffer*
-BufferCache::GetBuffer(media_buffer_id id)
+BufferCache::GetBuffer(media_buffer_id id, port_id port)
 {
 	if (id <= 0)
 		return NULL;
 
-	BufferMap::iterator found = fMap.find(id);
-	if (found != fMap.end())
-		return found->second;
+	buffer_cache_entry* entry;
+	if (fMap.Get(id, entry))
+		return entry->buffer;
 
 	buffer_clone_info info;
 	info.buffer = id;
@@ -55,13 +57,32 @@ BufferCache::GetBuffer(media_buffer_id id)
 		debugger("BufferCache::GetBuffer: IDs mismatch");
 
 	try {
-		fMap.insert(std::make_pair(id, buffer));
+		buffer_cache_entry entry;
+		entry.buffer = buffer;
+		entry.port = port;
+		fMap.Put(id, entry);
 	} catch (std::bad_alloc& exception) {
 		delete buffer;
 		return NULL;
 	}
 
 	return buffer;
+}
+
+
+void
+BufferCache::FlushCacheForPort(port_id port)
+{
+	BufferMap::Iterator iterator = fMap.GetIterator();
+	while (iterator.HasNext()) {
+		BufferMap::Entry entry = iterator.Next();
+		if (entry.value.port == port) {
+			// Delete the buffer
+			delete entry.value.buffer;
+			// Then remove it from the map
+			fMap.Remove(iterator);
+		}
+	}
 }
 
 
