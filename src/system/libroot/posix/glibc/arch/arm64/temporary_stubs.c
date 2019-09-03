b83451fed56408b5764694d2fdc8b4d3c03c8017
Jaroslaw Pelczar <jarek@jpelczar.com>
2019-09-03 20:17:14 +0200

    arm64: Add some temporary stubs to get linking done for math code
    Signed-off-by: Jaroslaw Pelczar <jarek@jpelczar.com>
    Change-Id: I2b85f8e6c274018fc8e4b9ee43a8719843416356

---
+++ src/system/libroot/posix/glibc/arch/arm64/temporary_stubs.c
@@ -0,0 +1,15 @@
+// XXX - just temporary stubs to get compilation going
+void __copysignl() {}
+void __ieee754_atan2l() {}
+void __isinfl() {}
+void __ieee754_sqrtl() {}
+void __ieee754_logl() {}
+void __log1pl() {}
+void __ieee754_hypotl() {}
+void __mpn_extract_long_double() {}
+void __hypotl() {}
+void __atan2l() {}
+void __clogl() {}
+void __csqrtl() {}
+void __isnanl() {}
+void __signbitl() {}
