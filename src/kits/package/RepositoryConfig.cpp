9ba758a9c321d100626fd495573815f92d2ce1eb
Stephan Aßmus <superstippi@gmx.de>
2019-11-17 14:55:22 +0100

    Fix fallback for old repository config files (url vs. baseurl)
    From what I understand, BRepositoryConfig::URL() is supposed to
    be a unique identifier (uniquely identifying the repository across
    mirrors), while BRepositoryConfig::BaseURL() specifies the URL
    where the remote server (the repository mirror) can be reached.
    The names for these identifiers seem strange to me. I would have
    kept URL() and introduced a new getter Identifier() or ID() or
    something. In addition, even for the new names, the meaning seems
    to be reverse to intuition.
    In any case, when the new BaseURL() field was introduced, the
    repository config files (which are in driver-settings format)
    have been updated to a new version. When they lack the key
    "cfgversion", then they are version 1 files, otherwise whatever
    cfgversion is specified. The code has a fallback for defining
    BaseURL and URL from the old version of the files which have only
    a "url" key. This commit tries to fix the fallback mechanism by
    simply using the same string for url and baseurl.
    This fixes software which tries to identify a repository by their
    URL(), which was an empty string before this change. An example
    is ticket #14927.

--- src/kits/package/RepositoryConfig.cpp
+++ src/kits/package/RepositoryConfig.cpp
@@ -123,14 +123,11 @@ BRepositoryConfig::SetTo(const BEntry& entry)
 	if (result != B_OK)
 		return result;
 
-	const char* url = NULL;
 	const char* version = driverSettings.GetParameterValue(KEY_CONFIG_VERSION);
 	const char *baseUrlKey = KEY_BASE_URL;
 
 	if (version == NULL || atoi(version) < 2)
 		baseUrlKey = KEY_BASE_URL_LEGACY;
-	else
-		url = driverSettings.GetParameterValue(KEY_URL);
 
 	const char* baseUrl = driverSettings.GetParameterValue(baseUrlKey);
 	const char* priorityString = driverSettings.GetParameterValue(KEY_PRIORITY);
@@ -138,11 +135,13 @@ BRepositoryConfig::SetTo(const BEntry& entry)
 	if (baseUrl == NULL || *baseUrl == '\0')
 		return B_BAD_DATA;
 
+	const char* url = driverSettings.GetParameterValue(KEY_URL);
+
 	fName = entry.Name();
 	fBaseURL = baseUrl;
 	fPriority = priorityString == NULL
 		? kUnsetPriority : atoi(priorityString);
-	fURL = url == NULL ? "" : url;
+	fURL = url == NULL ? fBaseURL.String() : url;
 
 	BPath userSettingsPath;
 	if (find_directory(B_USER_SETTINGS_DIRECTORY, &userSettingsPath) == B_OK) {
