dd093fd880daa76c16f7d500c0a462baee4507a0
Zotyamester <szatmary.zoltan1222@gmail.com>
2020-01-03 13:34:09 +0000

    Fix selection loss on resize (Fixes #13453)
    Change-Id: I634726edef681dc2edbae77e2ce15d248f3f18da

--- src/apps/terminal/TermView.cpp
+++ src/apps/terminal/TermView.cpp
@@ -586,8 +586,9 @@ TermView::SetTermSize(int rows, int columns, bool notifyShell)
 	if (columns > 0)
 		fColumns = columns;
 
-	// To keep things simple, get rid of the selection first.
-	_Deselect();
+	// To keep things simple, only get rid of the selection first if the new size would affect the selection.
+	if (rows < fSelection.End().y || columns < fSelection.End().x)
+		_Deselect();
 
 	{
 		BAutolock _(fTextBuffer);
