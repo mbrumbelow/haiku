16cdcb0254467dc81b6f24db9371458129adb78f
CruxBox <shubhambhagat111@yahoo.com>
2020-01-10 00:35:27 +0530

    Making of xfs file system
    I've copied and edited the kernel_interface and system_dependencies from btrfs so as to get started with xfs_shell. In the tests/add-ons/kernel/file_systems folder you can find the xfs shell which is modeled over btrfs as well. The problem is that btrfs_shell itself wouldn't build. There are multiple redifinition errors. Same errors show up in xfs_shell build output as well (since it is modelled over btrfs).

---
+++ src/add-ons/kernel/file_systems/xfs/system_dependencies.h
@@ -0,0 +1,49 @@
+
+#ifndef _SYSTE_DEPENDENCIES_H
+#define _SYSTEM_DEPENDENCIES_H
+
+
+#ifdef FS_SHELL
+
+// This needs to be included before the fs_shell wrapper
+
+#include <util/AVLTree.h>
+#include <uuid.h>
+
+#include "fssh_api_wrapper.h"
+#include "fssh_auto_deleter.h"
+
+#else	// !FS_SHELL
+
+#include <AutoDeleter.h>
+#include <util/kernel_cpp.h>
+#include <util/AutoLock.h>
+#include <util/SinglyLinkedList.h>
+#include <util/Stack.h>
+#include <util/AVLTree.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <ByteOrder.h>
+#include <driver_settings.h>
+#include <fs_cache.h>
+#include <fs_interface.h>
+#include <fs_info.h>
+#include <fs_volume.h>
+#include <KernelExport.h>
+#include <io_requests.h>
+#include <NodeMonitor.h>
+#include <SupportDefs.h>
+#include <lock.h>
+#include <errno.h>
+#include <new>
+#include <dirent.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <uuid.h>
+
+#endif	// !FS_SHELL
+
+
+#endif	// _SYSTEM_DEPENDENCIES
