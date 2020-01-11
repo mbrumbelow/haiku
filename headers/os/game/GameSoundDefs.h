80eb6df14dd3345b59d6f2539020bae5afe90667
Zotyamester <szatmary.zoltan1222@gmail.com>
2020-01-11 21:28:34 +0000

    Game Kit: rewrite GameSound and GameSoundDefs headers
    Change-Id: I5efee795a73a9637f34ed8ffa4fbeeda09eb7a88

--- headers/os/game/GameSoundDefs.h
+++ headers/os/game/GameSoundDefs.h
@@ -1,9 +1,6 @@
 /*
- *  Copyright 2001-2002, Haiku Inc. All Rights Reserved.
+ *  Copyright 2020, Haiku, Inc. All Rights Reserved.
  *  Distributed under the terms of the MIT License.
- *
- * Author:
- *		Christopher ML Zumwalt May (zummy@users.sf.net)
  */
 #ifndef _GAME_SOUND_DEFS_H
 #define _GAME_SOUND_DEFS_H
@@ -33,21 +30,19 @@ enum {
 };
 
 
-struct gs_audio_format {		
-	// same as media_raw_audio_format
-	enum format {	// for "format"
+struct gs_audio_format {
+	enum format {
 		B_GS_U8 = 0x11,			// 128 == mid, 1 == bottom, 255 == top
 		B_GS_S16 = 0x2,			// 0 == mid, -32767 == bottom, +32767 == top
 		B_GS_F = 0x24,			// 0 == mid, -1.0 == bottom, 1.0 == top
 		B_GS_S32 = 0x4			// 0 == mid, 0x80000001 == bottom,
 								// 0x7fffffff == top
 	};
-	float		frame_rate;
-	uint32		channel_count;	// 1 or 2, mostly
-	uint32		format;			// for compressed formats, go to
-								// media_encoded_audio_format
-	uint32		byte_order;		// 2 for little endian, 1 for big endian
-	size_t		buffer_size;	// size of each buffer -- NOT GUARANTEED
+	float	frame_rate;
+	uint32	channel_count;	// 1 or 2, mostly
+	uint32	format;			// for compressed formats
+	uint32	byte_order;		// 2 for little endian, 1 for big endian
+	size_t	buffer_size;	// size of each buffer -- NOT GUARANTEED
 };
 
 
@@ -55,7 +50,6 @@ enum gs_attributes {
 	B_GS_NO_ATTRIBUTE = 0,		// when there is no attribute
 	B_GS_MAIN_GAIN = 1,			// 0 == 0 dB, -6.0 == -6 dB (gs_id ignored)
 	B_GS_CD_THROUGH_GAIN,		// 0 == 0 dB, -12.0 == -12 dB (gs_id ignored)
-								//	but which CD?
 	B_GS_GAIN = 128,			// 0 == 0 dB, -1.0 == -1 dB, +10.0 == +10 dB
 	B_GS_PAN,					// 0 == middle, -1.0 == left, +1.0 == right
 	B_GS_SAMPLING_RATE,			// 44100.0 == 44.1 kHz
@@ -66,19 +60,19 @@ enum gs_attributes {
 
 
 struct gs_attribute {
-		int32		attribute;	// which attribute
-		bigtime_t	duration;	// how long of time to ramp over for the change
-		float		value;		// where the value stops changing
-		uint32		flags;		// whatever flags are for the attribute
+	int32		attribute;	// which attribute
+	bigtime_t	duration;	// how long of time to ramp over for the change
+	float		value;		// where the value stops changing
+	uint32		flags;		// whatever flags are for the attribute
 };
 
 
 struct gs_attribute_info {
-		int32		attribute;
-		float		granularity;
-		float		minimum;
-		float		maximum;
+	int32	attribute;
+	float	granularity;
+	float	minimum;
+	float	maximum;
 };
 
 
-#endif	// _GAME_SOUND_DEFS_H
+#endif
