a1b167e9df3a989fed8dd5cc0619584d1b44eaa0
CruxBox <shubhambhagat111@yahoo.com>
2020-01-17 01:07:51 +0530

    Completed xfs_shell The xfs_shell builds now and runs as expected. Here is what happens when you try to mount: https://pastebin.com/nr3BW4q3 The result is expected.

--- src/add-ons/kernel/file_systems/xfs/system_dependencies.h
+++ src/add-ons/kernel/file_systems/xfs/system_dependencies.h
@@ -2,48 +2,41 @@
 #ifndef _SYSTE_DEPENDENCIES_H
 #define _SYSTEM_DEPENDENCIES_H
 
-
 #ifdef FS_SHELL
 
 // This needs to be included before the fs_shell wrapper
 
-#include <util/AVLTree.h>
-#include <uuid.h>
+#include <new>
 
 #include "fssh_api_wrapper.h"
 #include "fssh_auto_deleter.h"
+#include "fssh_uuid.h"
 
-#else	// !FS_SHELL
+#else // !FS_SHELL
 
 #include <AutoDeleter.h>
-#include <util/kernel_cpp.h>
 #include <util/AutoLock.h>
+#include <util/DoublyLinkedList.h>
 #include <util/SinglyLinkedList.h>
 #include <util/Stack.h>
-#include <util/AVLTree.h>
-#include <sys/stat.h>
-#include <sys/types.h>
+
 #include <ByteOrder.h>
-#include <driver_settings.h>
-#include <fs_cache.h>
-#include <fs_interface.h>
-#include <fs_info.h>
-#include <fs_volume.h>
-#include <KernelExport.h>
-#include <io_requests.h>
-#include <NodeMonitor.h>
-#include <SupportDefs.h>
-#include <lock.h>
-#include <errno.h>
-#include <new>
-#include <dirent.h>
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <uuid.h>
 
-#endif	// !FS_SHELL
+#include <tracing.h>
+#include <driver_settings.h>
+#include <fs_attr.h>
+#include <fs_cache.h>
+#include <fs_index.h>
+#include <fs_info.h>
+#include <fs_interface.h>
+#include <fs_query.h>
+#include <fs_volume.h>
+#include <Drivers.h>
+#include <KernelExport.h>
+#include <NodeMonitor.h>
+#include <SupportDefs.h>
+#include <TypeConstants.h>
+#endif // !FS_SHELL
 
-
-#endif	// _SYSTEM_DEPENDENCIES
+#endif // _SYSTEM_DEPENDENCIES
