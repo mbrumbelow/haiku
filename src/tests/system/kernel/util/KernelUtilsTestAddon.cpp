83f8f50c34236897e19d9be962d341b192526a87
Kyle Ambroff-Kao <kyle@ambroffkao.com>
2020-01-26 02:11:49 -0800

    tests/kernel: Add tests for BOpenHashTable
    Linking libkernelutilstest.so with libbe to for use of BObjectList in
    the tests.
    
    Change-Id: I1abb991e240dd522821a71ef54d22a1ca7957283

--- src/tests/system/kernel/util/KernelUtilsTestAddon.cpp
+++ src/tests/system/kernel/util/KernelUtilsTestAddon.cpp
@@ -2,16 +2,17 @@
 #include <TestSuiteAddon.h>
 
 //#include "AVLTreeMapTest.h"
-#include "SinglyLinkedListTest.h"
+#include "BOpenHashTableTest.h"
 #include "DoublyLinkedListTest.h"
+#include "SinglyLinkedListTest.h"
 #include "VectorMapTest.h"
 #include "VectorSetTest.h"
 #include "VectorTest.h"
 
-
 BTestSuite* getTestSuite() {
 	BTestSuite *suite = new BTestSuite("KernelUtils");
 //	suite->addTest("AVLTreeMap", AVLTreeMapTest::Suite());
+	suite->addTest("BOpenHashTable", BOpenHashTableTest::Suite());
 	suite->addTest("SinglyLinkedList", SinglyLinkedListTest::Suite());
 	suite->addTest("DoublyLinkedList", DoublyLinkedListTest::Suite());
 	suite->addTest("VectorMap", VectorMapTest::Suite());
