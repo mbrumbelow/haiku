bf5bd9f84b9772016773a09ebc188a09f515c99d
Kyle Ambroff-Kao <kyle@ambroffkao.com>
2020-01-28 16:16:08 -0800

    tcp: Fix KDL when sockets are reused
    This fixes a KDL that is triggered by the following scenario.
    
    1. A socket is created, attempting to establish a session
       between (loopback, ephemeral port) => (remote address, remote
       port).
    2. That socket ends up in the closed state because the remote is not
       accepting connections.
    3. The socket is re-used to connect to a different (remote address,
       remote port).
    
    The problem is that fConnectionHash is a BOpenHashTable<TCPEndpoint>,
    and inserting endpoint multiple times can create a linked list
    cycle (TCPEndpoint is an intrusive linked list node). That means that,
    even though TCPEndpoint's destructor removes itself from
    fConnectionHash, there will still be a pointer to it left behind,
    which means that future accesses within that hash table bucket will
    result in a segfault.
    
    The added fConnectionHash.Remove(endpoint) here prevents the KDL, as
    it ensures that socket reuse doesn't result in a cycle.
    
    Fixes #13927, see that ticket for a detailed explanation of the
    problem.
    
    Also added some regression tests:
    * Added SocketTests::ClientSocketReuseTest to PosixNetTest, which
      reproduces this KDL.
    * BOpenHashTable: Added tests to cover RemoveUnchecked and removal
      of an object that isn't in the table.
    
    Change-Id: If4bcc1e0d94350a5ad9ba8e7ae6f1b783b3f6d34

---
+++ src/tests/system/network/posixnet/SocketTests.cpp
@@ -0,0 +1,57 @@
+#include "SocketTests.h"
+
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <cppunit/TestAssert.h>
+#include <cppunit/TestCaller.h>
+#include <cppunit/TestSuite.h>
+
+
+// This test reproduces a KDL from issue #13927 where a socket is created
+// and an attempt is made to connect to an address, which fails. The socket
+// is closed and then reused to connect to a *different* address.
+void SocketTests::ClientSocketReuseTest()
+{
+	// TODO: Try to find unused ports instead of using these hard-coded ones.
+	const int kFirstPort = 14025;
+	const int kSecondPort = 14026;
+
+	int fd = ::socket(AF_INET, SOCK_STREAM, 0);
+	CPPUNIT_ASSERT(fd > 0);
+
+	sockaddr_in address;
+	memset(&address, 0, sizeof(sockaddr_in));
+	address.sin_family = AF_INET;
+	address.sin_port = htons(kFirstPort);
+	address.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+
+	int connect_result = connect(
+		fd,
+		reinterpret_cast<const sockaddr *>(&address),
+		sizeof(struct sockaddr));
+	CPPUNIT_ASSERT_EQUAL(connect_result, -1);
+
+	close(fd);
+
+	address.sin_port = htons(kSecondPort);
+	connect_result = connect(
+		fd,
+		reinterpret_cast<const sockaddr *>(&address),
+		sizeof(struct sockaddr));
+	CPPUNIT_ASSERT_EQUAL(connect_result, -1);
+}
+
+
+void SocketTests::AddTests(BTestSuite &parent) {
+	CppUnit::TestSuite &suite = *new CppUnit::TestSuite("SocketTests");
+
+	suite.addTest(new CppUnit::TestCaller<SocketTests>(
+		"SocketTests::ClientSocketReuseTest",
+		&SocketTests::ClientSocketReuseTest));
+
+	parent.addTest("SocketTests", &suite);
+}
