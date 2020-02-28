b0365f97e16cbe4d0929a95bd9c360b0d45cc9c5
John Scipione <jscipione@gmail.com>
2020-02-28 18:35:53 -0500

    Tracker: Live updating shiftable menu items
    Create link => Create relative link
    Identify => Force identify
    
    Cut => Cut more
    Copy => Copy more
    Paste => Paste links
    
    Clean up => Clean up all
    Close => Close all
    
    Create TLiveUpdatingMenu and TLiveUpdatingPopUpMenu classes that inherit
    from BMenu and BPopUpMenu respectively. Create TLiveUpdatingFileMenu and
    TLiveUpdatingFilePopUp menu subclasses for the File menu and a file's
    context menu respectively. Create a TFileMenuMixin class to share methods
    between the two file menu classes. Put these classes in their own header
    and cpp files and include it in the Jamfile. Also add some other classes,
    TLiveUpdatingWindowMenu and TLiveUpdatingArrangeByMenu for the other items.
    
    The live updating menu classes listen for B_MODIFIERS_CHANGED messages and
    update the above listed items. SetEventMask(B_KEYBOARD_EVENTS) on
    MenusBeginning() and unset it on MenusEnded(). Lock looper before setting
    and unsetting the EventMask.
    
    Change-Id: I5dfbd4d468fad02894f1f31aa08d1abf630a4b5d

---
+++ src/kits/tracker/LiveUpdatingMenu.cpp
@@ -0,0 +1,400 @@
+/*
+ * Copyright 2020 Haiku, Inc. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ *		John Scipione, jscipione@gmail.com
+ */
+
+
+#include "LiveUpdatingMenu.h"
+
+#include <string.h>
+
+#include <Application.h>
+#include <Catalog.h>
+#include <Locale.h>
+#include <MenuWindow.h>
+#include <MenuItem.h>
+#include <Window.h>
+
+#include "Commands.h"
+
+
+#undef B_TRANSLATION_CONTEXT
+#define B_TRANSLATION_CONTEXT "ContainerWindow"
+
+
+//	#pragma mark - TLiveUpdatingMenu
+
+
+TLiveUpdatingMenu::TLiveUpdatingMenu(const char* label)
+	:
+	BMenu(label)
+{
+}
+
+
+TLiveUpdatingMenu::~TLiveUpdatingMenu()
+{
+}
+
+
+void
+TLiveUpdatingMenu::MessageReceived(BMessage* message)
+{
+	if (message != NULL && message->what == B_MODIFIERS_CHANGED) {
+		UpdateMenuItemsForModifiersChanged();
+	} else
+		BMenu::MessageReceived(message);
+}
+
+
+void
+TLiveUpdatingMenu::UpdateMenuItemsForModifiersChanged()
+{
+	// hook method
+}
+
+
+//	#pragma mark - TLiveUpdatingPopUpMenu
+
+
+TLiveUpdatingPopUpMenu::TLiveUpdatingPopUpMenu(const char* label,
+	bool radioMode = true, bool labelFromMarked, menu_layout layout)
+	:
+	BPopUpMenu(label, radioMode, labelFromMarked, layout)
+{
+}
+
+
+TLiveUpdatingPopUpMenu::~TLiveUpdatingPopUpMenu()
+{
+}
+
+
+void
+TLiveUpdatingPopUpMenu::MessageReceived(BMessage* message)
+{
+	if (message != NULL && message->what == B_MODIFIERS_CHANGED)
+		UpdateMenuItemsForModifiersChanged();
+	else
+		BMenu::MessageReceived(message);
+}
+
+
+void
+TLiveUpdatingPopUpMenu::UpdateMenuItemsForModifiersChanged()
+{
+	// hook method
+}
+
+
+//	#pragma mark - TLiveUpdatingArrangeByMenu
+
+
+TLiveUpdatingArrangeByMenu::TLiveUpdatingArrangeByMenu(const char* label)
+	:
+	TLiveUpdatingMenu(label)
+{
+}
+
+
+TLiveUpdatingArrangeByMenu::~TLiveUpdatingArrangeByMenu()
+{
+}
+
+
+void
+TLiveUpdatingArrangeByMenu::UpdateMenuItemsForModifiersChanged()
+{
+	if (Window()->LockLooper()) {
+		// Clean up/Clean up all
+		BMenuItem* cleanUp = FindItem(kCleanup);
+		if (cleanUp == NULL)
+			cleanUp = FindItem(kCleanupAll);
+		UpdateCleanUpMenuItem(cleanUp);
+
+		Window()->UnlockLooper();
+	}
+}
+
+
+void
+TLiveUpdatingArrangeByMenu::UpdateCleanUpMenuItem(BMenuItem* item)
+{
+	if (item == NULL || item->Menu() == NULL || item->Message() == NULL)
+		return;
+
+	if ((modifiers() & B_SHIFT_KEY) != 0) {
+		item->SetLabel(B_TRANSLATE("Clean up all"));
+		item->SetShortcut('K', B_COMMAND_KEY | B_SHIFT_KEY);
+		item->Message()->what = kCleanupAll;
+	} else {
+		item->SetLabel(B_TRANSLATE("Clean up"));
+		item->SetShortcut('K', B_COMMAND_KEY);
+		item->Message()->what = kCleanup;
+	}
+}
+
+
+//	#pragma mark - TFileMixin
+
+
+void
+TFileMixin::UpdateCreateLinkMenuItem(BMenuItem* item)
+{
+	if (item == NULL || item->Menu() == NULL || item->Message() == NULL)
+		return;
+
+	if ((modifiers() & B_SHIFT_KEY) != 0) {
+		item->SetLabel(B_TRANSLATE("Create relative link"));
+		item->SetMessage(new BMessage(kCreateRelativeLink));
+	} else {
+		item->SetLabel(B_TRANSLATE("Create link"));
+		item->SetMessage(new BMessage(kCreateLink));
+	}
+}
+
+
+void
+TFileMixin::UpdateCreateLinkMenuItemsInMenu(BMenu* menu)
+{
+	for (int32 i = menu->CountItems(); i-- > 0;) {
+		BMenuItem* item = menu->ItemAt(i);
+		if (item == NULL || item->Message() == NULL)
+			continue; // invalid
+
+		uint32 what = item->Message()->what;
+		if (what != kCreateLink && what != kCreateRelativeLink)
+			continue; // not our menu item
+
+		const char* label = item->Label();
+		if (strcmp(label, B_TRANSLATE("Create link")) == 0
+			|| strcmp(label, B_TRANSLATE("Create relative link")) == 0) {
+			// only update label if "Create link" or "Create relative link"
+			UpdateCreateLinkMenuItem(item);
+		} else {
+			// only update the item message command
+			item->Message()->what = ((modifiers() & B_SHIFT_KEY) != 0
+				? kCreateRelativeLink : kCreateLink);
+		}
+	}
+}
+
+
+void
+TFileMixin::UpdateCutMenuItem(BMenuItem* item)
+{
+	if (item == NULL || item->Menu() == NULL || item->Message() == NULL)
+		return;
+
+	if ((modifiers() & B_SHIFT_KEY) != 0) {
+		item->SetLabel(B_TRANSLATE("Cut more"));
+		item->SetShortcut('X', B_COMMAND_KEY | B_SHIFT_KEY);
+		item->SetMessage(new BMessage(kCutMoreSelectionToClipboard));
+	} else {
+		item->SetLabel(B_TRANSLATE("Cut"));
+		item->SetShortcut('X', B_COMMAND_KEY);
+		item->SetMessage(new BMessage(B_CUT));
+	}
+}
+
+
+void
+TFileMixin::UpdateCopyMenuItem(BMenuItem* item)
+{
+	if (item == NULL || item->Menu() == NULL || item->Message() == NULL)
+		return;
+
+	if ((modifiers() & B_SHIFT_KEY) != 0) {
+		item->SetLabel(B_TRANSLATE("Copy more"));
+		item->SetShortcut('C', B_COMMAND_KEY | B_SHIFT_KEY);
+		item->Message()->what = kCopyMoreSelectionToClipboard;
+	} else {
+		item->SetLabel(B_TRANSLATE("Copy"));
+		item->SetShortcut('C', B_COMMAND_KEY);
+		item->Message()->what = B_COPY;
+	}
+}
+
+
+void
+TFileMixin::UpdatePasteMenuItem(BMenuItem* item)
+{
+	if (item == NULL || item->Menu() == NULL || item->Message() == NULL)
+		return;
+
+	if ((modifiers() & B_SHIFT_KEY) != 0) {
+		item->SetLabel(B_TRANSLATE("Paste links"));
+		item->SetShortcut('V', B_COMMAND_KEY | B_SHIFT_KEY);
+		item->Message()->what = kPasteLinksFromClipboard;
+	} else {
+		item->SetLabel(B_TRANSLATE("Paste"));
+		item->SetShortcut('V', B_COMMAND_KEY);
+		item->Message()->what = B_PASTE;
+	}
+}
+
+
+void
+TFileMixin::UpdateIdentifyMenuItem(BMenuItem* item)
+{
+	if (item == NULL || item->Menu() == NULL || item->Message() == NULL)
+		return;
+
+	if ((modifiers() & B_SHIFT_KEY) != 0) {
+		item->SetLabel(B_TRANSLATE("Force identify"));
+		item->Message()->ReplaceBool("force", true);
+	} else {
+		item->SetLabel(B_TRANSLATE("Identify"));
+		item->Message()->ReplaceBool("force", false);
+	}
+}
+
+
+//	#pragma mark - TLiveUpdatingFileMenu
+
+
+TLiveUpdatingFileMenu::TLiveUpdatingFileMenu(const char* label)
+	:
+	TLiveUpdatingMenu(label)
+{
+}
+
+
+TLiveUpdatingFileMenu::~TLiveUpdatingFileMenu()
+{
+}
+
+
+void
+TLiveUpdatingFileMenu::UpdateMenuItemsForModifiersChanged()
+{
+	if (Window()->LockLooper()) {
+		// Create link/Create relative link
+		UpdateCreateLinkMenuItemsInMenu(this);
+
+		// Cut/Cut more
+		BMenuItem* cut = FindItem(B_CUT);
+		if (cut == NULL)
+			cut = FindItem(kCutMoreSelectionToClipboard);
+		UpdateCutMenuItem(cut);
+
+		// Copy/Copy more
+		BMenuItem* copy = FindItem(B_COPY);
+		if (copy == NULL)
+			copy = FindItem(kCopyMoreSelectionToClipboard);
+		UpdateCopyMenuItem(copy);
+
+		// Paste/Paste links
+		BMenuItem* paste = FindItem(B_PASTE);
+		if (paste == NULL)
+			paste = FindItem(kPasteLinksFromClipboard);
+		UpdatePasteMenuItem(paste);
+
+		// Identify/Force identify
+		UpdateIdentifyMenuItem(FindItem(kIdentifyEntry));
+
+		Window()->UnlockLooper();
+	}
+}
+
+
+//	#pragma mark - TLiveUpdatingFilePopUpMenu
+
+
+TLiveUpdatingFilePopUpMenu::TLiveUpdatingFilePopUpMenu(const char* label,
+	bool radioMode = true, bool labelFromMarked, menu_layout layout)
+	:
+	TLiveUpdatingPopUpMenu(label, radioMode, labelFromMarked, layout)
+{
+}
+
+
+TLiveUpdatingFilePopUpMenu::~TLiveUpdatingFilePopUpMenu()
+{
+}
+
+
+void
+TLiveUpdatingFilePopUpMenu::UpdateMenuItemsForModifiersChanged()
+{
+	if (Window()->LockLooper()) {
+		// Create link/Create relative link
+		UpdateCreateLinkMenuItemsInMenu(this);
+
+		// Cut/Cut more
+		BMenuItem* cut = FindItem(B_CUT);
+		if (cut == NULL)
+			cut = FindItem(kCutMoreSelectionToClipboard);
+		UpdateCutMenuItem(cut);
+
+		// Copy/Copy more
+		BMenuItem* copy = FindItem(B_COPY);
+		if (copy == NULL)
+			copy = FindItem(kCopyMoreSelectionToClipboard);
+		UpdateCopyMenuItem(copy);
+
+		// Paste/Paste links
+		BMenuItem* paste = FindItem(B_PASTE);
+		if (paste == NULL)
+			paste = FindItem(kPasteLinksFromClipboard);
+		UpdatePasteMenuItem(paste);
+
+		// Identify/Force identify
+		UpdateIdentifyMenuItem(FindItem(kIdentifyEntry));
+
+		Window()->UnlockLooper();
+	}
+}
+
+
+//	#pragma mark - TLiveUpdatingWindowMenu
+
+
+TLiveUpdatingWindowMenu::TLiveUpdatingWindowMenu(const char* label)
+	:
+	TLiveUpdatingMenu(label)
+{
+}
+
+
+TLiveUpdatingWindowMenu::~TLiveUpdatingWindowMenu()
+{
+}
+
+
+void
+TLiveUpdatingWindowMenu::UpdateMenuItemsForModifiersChanged()
+{
+	if (Window()->LockLooper()) {
+		// Close/Close all
+		BMenuItem* close = FindItem(B_QUIT_REQUESTED);
+		if (close == NULL)
+			close = FindItem(kCloseAllWindows);
+		UpdateCloseMenuItem(close);
+
+		Window()->UnlockLooper();
+	}
+}
+
+
+void
+TLiveUpdatingWindowMenu::UpdateCloseMenuItem(BMenuItem* item)
+{
+	if (item == NULL || item->Menu() == NULL || item->Message() == NULL)
+		return;
+
+	if ((modifiers() & B_SHIFT_KEY) != 0) {
+		item->SetLabel(B_TRANSLATE("Close all"));
+		item->SetShortcut('W', B_COMMAND_KEY | B_SHIFT_KEY);
+		item->SetTarget(be_app);
+		item->SetMessage(new BMessage(kCloseAllWindows));
+	} else {
+		item->SetLabel(B_TRANSLATE("Close"));
+		item->SetShortcut('W', B_COMMAND_KEY);
+		item->SetTarget(Window());
+		item->SetMessage(new BMessage(B_QUIT_REQUESTED));
+	}
+}
