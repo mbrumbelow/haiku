2ffc0feb78561b0c173be69bba9b6a721b51eba7
SuhelMehta9 <suhelmehta@outlook.com>
2020-03-20 23:58:28 +0530

    Adding superblock, ufs2_identify and volume
    Identify function is created.

---
+++ src/add-ons/kernel/file_systems/ufs2/Volume.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2020 Suhel Mehta, mehtasuhel@gmail.com
+ * All rights reserved. Distributed under the terms of the MIT License.
+ */
+#ifndef VOLUME_H
+#define VOLUME_H
+
+#include "ufs2.h"
+
+class Volume {
+    public:
+                                                Volume(fs_volume* volume);
+                                                ~Volume();
+                bool                            IsValidSuperBlock();
+                fs_volume*                      FSVolume() const { return fFSVolume; }
+                ufs2_super_block&               SuperBlock() { return fSuperBlock; }
+                status_t			            LoadSuperBlock();
+        static	status_t			            Identify(int fd, ufs2_super_block* superBlock);
+
+    private:
+                fs_volume*			fFSVolume;
+                ufs2_super_block	        fSuperBlock;
+};
+
+#endif	// VOLUME_H
