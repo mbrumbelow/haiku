098fc5feaee02566a0a9acecca214be78153f0d6
X512 <danger_mail@list.ru>
2020-05-11 16:44:33 +0900

    Tracker: fix debug build
    Change-Id: I8a31802366b3dcbadc9ce1ba903161fa3fcad60d

--- src/kits/tracker/IconCache.cpp
+++ src/kits/tracker/IconCache.cpp
@@ -135,7 +135,6 @@ IconCacheEntry::SetAliasFor(const SharedIconCache* sharedCache,
 	const SharedCacheEntry* entry)
 {
 	sharedCache->SetAliasFor(this, entry);
-	ASSERT(fAliasForIndex >= 0);
 }
 
 
@@ -1457,7 +1456,6 @@ SharedIconCache::IconChanged(SharedCacheEntry* entry)
 {
 	// by now there should be no aliases to entry, just remove entry
 	// itself
-	ASSERT(entry->fAliasForIndex == -1);
 	entry->RetireIcons(&fRetiredBitmaps);
 	fHashTable.Remove(entry);
 }
