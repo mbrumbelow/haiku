cd3d8a7a7b368463de0044ce6843709b3bfbe200
Preetpal Kaur <preetpalok123@gmail.com>
2020-05-17 18:17:48 +0530

    Added the icons in the DeviceList
    Change-Id: I2eaebca0f0e91e3420b7e63a4a568f4e658af253

---
+++ src/preferences/input/InputIcons.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2010, Haiku, Inc. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef __INPUT_ICONS_H
+#define __INPUT_ICONS_H
+
+#include <Bitmap.h>
+
+
+class BResources;
+
+
+struct InputIcons {
+								InputIcons();
+
+	struct IconSet {
+								IconSet();
+
+	};
+
+			BBitmap				mouseIcon;
+			BBitmap				touchpadIcon;
+			BBitmap				keyboardIcon;
+
+	static	BRect				IconRectAt(const BPoint& topLeft);
+
+	static	const BRect			sBounds;
+
+private:
+
+			void				_LoadBitmap(BResources* resources);
+};
+
+#endif
+
