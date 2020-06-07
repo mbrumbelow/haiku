1fa58e14841b0c5b5537bb0730a01097182497a2
X512 <danger_mail@list.ru>
2020-06-07 23:48:29 +0900

    app_server: match behavior when drawing to BPicture
    Change-Id: I7676e73bda0eb02a1905888706aab6bb1db401d5

--- src/servers/app/ServerWindow.cpp
+++ src/servers/app/ServerWindow.cpp
@@ -3245,6 +3245,7 @@ ServerWindow::_DispatchPictureMessage(int32 code, BPrivate::LinkReceiver& link)
 			link.Read<float>(&x);
 			link.Read<float>(&y);
 
+			fCurrentView->SetDrawingOrigin(BPoint(x, y));
 			picture->WriteSetOrigin(BPoint(x, y));
 			break;
 		}
@@ -3259,12 +3260,14 @@ ServerWindow::_DispatchPictureMessage(int32 code, BPrivate::LinkReceiver& link)
 
 		case AS_VIEW_PUSH_STATE:
 		{
+			fCurrentView->PushState();
 			picture->WritePushState();
 			break;
 		}
 
 		case AS_VIEW_POP_STATE:
 		{
+			fCurrentView->PopState();
 			picture->WritePopState();
 			break;
 		}
