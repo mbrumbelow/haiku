6eac1ab84a41f4abc6bcb234f8d79b827af3436a
Suhel Mehta <mehtasuhel@gmail.com>
2020-06-13 00:56:16 +0530

    ufs2: implementing open_dir and read_dir function
    publish_vnode is working correctly and data read from disk is correct.
    
    Change-Id: I8bdc8c1a52edb679a6cb4b5f70e7f42eb2b2c8f1

---
+++ src/add-ons/kernel/file_systems/ufs2/DirectoryIterator.cpp
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2020 Suhel Mehta, mehtasuhel@gmail.com
+ * All rights reserved. Distributed under the terms of the MIT License.
+ */
+
+
+#include "DirectoryIterator.h"
+
+#include <stdlib.h>
+
+#include "Inode.h"
+
+#define TRACE_UFS2
+#ifdef TRACE_UFS2
+#	define TRACE(x...) dprintf("\33[34mufs2:\33[0m " x)
+#else
+#	define TRACE(x...) ;
+#endif
+
+#define ERROR(x...) dprintf("\33[34mufs2:\33[0m " x)
+
+
+DirectoryIterator::DirectoryIterator(Inode* inode)
+	:
+	fInode(inode)
+{
+	TRACE("DirectoryIterator::DirectoryIterator() \n");
+}
+
+
+DirectoryIterator::~DirectoryIterator()
+{
+}
+
+
+status_t
+DirectoryIterator::InitCheck()
+{
+	return B_OK;
+}
+
+
+status_t
+DirectoryIterator::Lookup(const char* name, size_t length, ino_t* _id)
+{
+	if (strcmp(name, ".") == 0) {
+		*_id = fInode->ID();
+		return B_OK;
+	} else if (strcmp(name, "..") == 0) {
+		if (fInode->ID() == 1) {
+			*_id = fInode->ID();
+		}
+	/*	else
+			*_id = fInode->Parent();*/
+
+		return B_NOT_SUPPORTED;
+	}
+}
+
+
+status_t
+DirectoryIterator::GetNext(char* name, size_t* _nameLength, ino_t* _id)
+{
+	TRACE("In GetNext function\n");
+	int64_t offset = fInode->GetBlockPointer() * MINBSIZE;
+	dir* direct = DirectContent();
+	dir_info* direct_info;// = DirectInfo();
+	int fd = fInode->GetVolume()->Device();
+	if (read_pos(fd, offset, direct_info, sizeof(dir_info)) != sizeof(dir_info)) {
+		return B_NOT_SUPPORTED;
+	}
+	TRACE("offset = %ld\n",direct_info->dot_ino);
+	return B_NOT_SUPPORTED;
+}
