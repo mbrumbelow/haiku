2ab6ea75928f15a28d25674bc33c5c57b661f033
CruxBox <shubhambhagat111@yahoo.com>
2020-06-14 01:42:13 +0530

    xfs: An attempt at reading shortform dir
    This patch gets read_dir, lookup and few others to work. Entries cannot
    be listed yet except the root.
    
    Change-Id: Ic4b7b61a794a1b75849e45777a383b3315b15aa8

---
+++ src/add-ons/kernel/file_systems/xfs/ShortDirectory.cpp
@@ -0,0 +1,202 @@
+/*
+ * Copyright 2020, Shubham Bhagat, shubhambhagat111@yahoo.com
+ * All rights reserved. Distributed under the terms of the MIT License.
+ */
+
+
+#include "ShortDirectory.h"
+
+
+ShortDirectory::ShortDirectory(Inode* inode)
+	:
+	fInode(inode),
+	fTrack(0)
+{
+
+	fHeader = (xfs_dir2_sf_hdr_t*)(DIR_DFORK_PTR(fInode->Buffer()));
+}
+
+
+ShortDirectory::~ShortDirectory()
+{
+
+}
+
+
+bool
+ShortDirectory::HasFileTypeField()
+{
+	if (fInode->GetVolume()->SuperBlockFeatures2() & XFS_SB_VERSION2_FTYPE)
+		return true;
+	else
+		return false;
+}
+
+
+uint8
+ShortDirectory::GetFileType(xfs_dir2_sf_entry_t* entry)
+{
+	ASSERT(HasFileTypeField() == true);
+	return entry->name[entry->namelen];
+}
+
+
+xfs_dir2_sf_entry_t*
+ShortDirectory::FirstEntry()
+{
+	return (xfs_dir2_sf_entry_t*) ((char*) fHeader + HeaderSize());
+}
+
+
+size_t
+ShortDirectory::HeaderSize()
+{
+	if (fHeader->i8count)
+		return sizeof(xfs_dir2_sf_hdr_t);
+	else
+		return sizeof(xfs_dir2_sf_hdr_t) - sizeof(uint32);
+}
+
+
+xfs_ino_t
+ShortDirectory::GetIno(xfs_dir2_inou_t* inum)
+{
+	if (fHeader->i8count)
+		return B_BENDIAN_TO_HOST_INT64(inum->i8);
+	else
+		return B_BENDIAN_TO_HOST_INT32(inum->i4);
+}
+
+
+xfs_ino_t
+ShortDirectory::GetEntryIno(xfs_dir2_sf_entry_t* entry)
+{
+	if (HasFileTypeField())
+		return GetIno((xfs_dir2_inou_t*)(entry->name
+				+ entry->namelen + sizeof(uint8)));
+	else
+		return GetIno((xfs_dir2_inou_t*)(entry->name + entry->namelen));
+}
+
+
+size_t
+ShortDirectory::EntrySize(int namelen)
+{
+	return sizeof(xfs_dir2_sf_entry_t) + namelen
+			+ (HasFileTypeField()? sizeof(uint8) : 0)
+			+ (fHeader->i8count? sizeof(uint64):sizeof(uint32));
+}
+
+
+bool
+ShortDirectory::CompareName(const char* name, size_t length,
+	xfs_dir2_sf_entry_t* entry)
+{
+	if (length != entry->namelen)
+		return false;
+
+	bool status = true;
+	for (int i = 0; i < length; i++)
+	{
+		if (name[i] == entry->name[i])
+			continue;
+		status = false;
+	}
+
+	return status;
+}
+
+
+status_t
+ShortDirectory::Lookup(const char* name, size_t length, xfs_ino_t* ino)
+{
+	TRACE("ShortDirectory::Lookup\n");
+
+	if (strcmp(name, ".") == 0 || strcmp(name, "..") == 0) {
+		xfs_ino_t rootIno = fInode->GetVolume()->Root();
+
+		if (strcmp(name, ".") == 0 || (rootIno == fInode->ID())) {
+			*ino = fInode->ID();
+			TRACE("ShortDirectory:Lookup: name: \".\" ino: (%d)\n", *ino);
+			return B_OK;
+		}
+		*ino = GetIno(&fHeader->parent);
+		TRACE("Parent: (%d)\n", *ino);
+		return B_OK;
+	}
+
+	xfs_dir2_sf_entry_t* entry = FirstEntry();
+	TRACE("Length of first entry: (%d),offset of first entry:"
+		"(%d)\n", entry->namelen, B_BENDIAN_TO_HOST_INT16(entry->offset.i));
+
+	for (int i = 0; i < fHeader->count; i++) {
+		bool status = CompareName(name, length, entry);
+		if (status) {
+			*ino = GetEntryIno(entry);
+			return B_OK;
+		}
+		entry = (xfs_dir2_sf_entry_t*)
+			((char*) entry + EntrySize(entry->namelen));
+	}
+
+	return B_ENTRY_NOT_FOUND;
+}
+
+
+status_t
+ShortDirectory::GetNext(char* name, size_t* length, xfs_ino_t* ino)
+{
+	if (fTrack == 0) {
+		// Return '.'
+		if (*length < 2)
+			return B_BUFFER_OVERFLOW;
+
+		*length = 2;
+		strlcpy(name, ".", *length + 1);
+		*ino = fInode->ID();
+		fTrack = 1;
+
+		TRACE("ShortDirectory:GetNext: name: \".\" ino: (%d)\n", *ino);
+		return B_OK;
+	}
+	if (fTrack == 1) {
+		// Return '..'
+		if (*length < 3)
+			return B_BUFFER_OVERFLOW;
+
+		*length = 3;
+		strlcpy(name, "..", *length + 1);
+		*ino = GetIno(&fHeader->parent);
+		fTrack = 2;
+
+		TRACE("ShortDirectory:GetNext: name: \"..\" ino: (%d)\n", *ino);
+		return B_OK;
+	}
+
+	xfs_dir2_sf_entry_t* entry = FirstEntry();
+	TRACE("Length of first entry: (%d), offset of first entry:"
+		"(%d)\n", entry->namelen, B_BENDIAN_TO_HOST_INT16(entry->offset.i));
+
+	for (int i = 0; i < fHeader->count; i++)
+	{
+		uint16 curOffset = B_BENDIAN_TO_HOST_INT16(entry->offset.i);
+		if (curOffset > fLastEntryOffset) {
+
+			if (entry->namelen > *length)
+				return B_BUFFER_OVERFLOW;
+
+			fLastEntryOffset = curOffset;
+			memcpy(name, entry->name, entry->namelen);
+			name[entry->namelen] = '\0';
+			*length = entry->namelen + 1;
+			*ino = GetEntryIno(entry);
+
+			TRACE("Entry found. Name: (%s), Length: (%ld),ino: (%ld)\n", name,
+				*length, *ino);
+			return B_OK;
+		}
+		entry = (xfs_dir2_sf_entry_t*)((char*)entry + EntrySize(entry->namelen));
+	}
+
+	return B_ENTRY_NOT_FOUND;
+}
--- src/add-ons/kernel/file_systems/xfs/xfs.h
+++ src/add-ons/kernel/file_systems/xfs/xfs.h
@@ -1,18 +1,18 @@
 /*
-* Copyright 2020, Shubham Bhagat, shubhambhagat111@yahoo.com
-* All rights reserved. Distributed under the terms of the MIT License.
-*/
+ * Copyright 2020, Shubham Bhagat, shubhambhagat111@yahoo.com
+ * All rights reserved. Distributed under the terms of the MIT License.
+ */
 
 /*
-*Important:
-*All fields in XFS metadata structures are in big-endian byte order
-*except for log items which are formatted in host order.
-*
-*This file contains all global structure definitions.
-*/
-
+ *Important:
+ *All fields in XFS metadata structures are in big-endian byte order
+ *except for log items which are formatted in host order.
+ *
+ *This file contains all global structure definitions.
+ */
 #ifndef _XFS_SB_H_
 #define _XFS_SB_H_
+
 
 #include "system_dependencies.h"
 #include "xfs_types.h"
@@ -22,83 +22,90 @@ extern fs_vnode_ops gxfsVnodeOps;
 extern fs_volume_ops gxfsVolumeOps;
 
 
-#define XFS_SB_MAGIC 0x58465342 /* Identifies XFS. "XFSB" */
+#define XFS_SB_MAGIC 0x58465342
+	// Identifies XFS. "XFSB"
 #define XFS_SB_MAXSIZE 512
-#define BBLOCKLOG 9		/* Log of block size should be 9 */
-#define BBLOCKSIZE 1 << BBLOCKLOG		/* The size of a basic block should be 512 */
+#define BASICBLOCKLOG 9
+	// Log of block size should be 9
+#define BASICBLOCKSIZE 1 << BASICBLOCKLOG
+	// The size of a basic block should be 512
 
 
-/*	Version 4 superblock definition	*/
-class XfsSuperBlock
-{
+/*
+ *Version 4 superblock definition
+ */
+class XfsSuperBlock {
 public:
 
-			bool 			IsValid();
-			char*			Name();
-			uint32			BlockSize();
-			uint8			BlockLog();
-			uint32			DirBlockSize();		// maximum 65536
-			uint8			AgInodeBits();
-			uint8			InodesPerBlkLog();
-			uint8			AgBlocksLog();
-			xfs_rfsblock_t	TotalBlocks();
-			xfs_rfsblock_t	TotalBlocksWithLog();
-			uint64			FreeBlocks();
-			uint64			UsedBlocks();
-			uint32			Size();
-			uint16			InodeSize();
-			void			SwapEndian();
-			xfs_ino_t		Root() const;
-			xfs_agnumber_t	AgCount();
-			xfs_agblock_t	AgBlocks();
-
+			bool 				IsValid() const;
+			const char*			Name() const;
+			uint32				BlockSize() const;
+			uint8				BlockLog() const;
+			uint32				DirBlockSize() const;
+				// maximum 65536
+			uint8				AgInodeBits() const;
+			uint8				InodesPerBlkLog() const;
+			uint8				AgBlocksLog() const;
+			xfs_rfsblock_t		TotalBlocks() const;
+			xfs_rfsblock_t		TotalBlocksWithLog() const;
+			uint64				FreeBlocks() const;
+			uint64				UsedBlocks() const;
+			uint32				Size() const;
+			uint16				InodeSize() const;
+			void				SwapEndian();
+			xfs_ino_t			Root() const;
+			xfs_agnumber_t		AgCount() const;
+			xfs_agblock_t		AgBlocks() const;
+			uint8				Flags() const;
+			uint16				Version() const;
+			uint32				Features2() const;
 private:
 
-			uint32			sb_magicnum;
-			uint32			sb_blocksize;
-			xfs_rfsblock_t	sb_dblocks;
-			xfs_rfsblock_t	sb_rblocks;
-			xfs_rtblock_t	sb_rextents;
-			uuid_t			sb_uuid;
-			xfs_fsblock_t	sb_logstart;
-			xfs_ino_t		sb_rootino;
-			xfs_ino_t		sb_rbmino;
-			xfs_ino_t		sb_rsumino;
-			xfs_agblock_t	sb_rextsize;
-			xfs_agblock_t	sb_agblocks;
-			xfs_agnumber_t	sb_agcount;
-			xfs_extlen_t	sb_rbmblocks;
-			xfs_extlen_t	sb_logblocks;
-			uint16			sb_versionnum;
-			uint16			sb_sectsize;
-			uint16			sb_inodesize;
-			uint16			sb_inopblock;
-			char			sb_fname[12];
-			uint8			sb_blocklog;
-			uint8			sb_sectlog;
-			uint8			sb_inodelog;
-			uint8			sb_inopblog;
-			uint8			sb_agblklog;
-			uint8			sb_rextslog;
-			uint8			sb_inprogress;
-			uint8			sb_imax_pct;
-			uint64			sb_icount;
-			uint64			sb_ifree;
-			uint64			sb_fdblocks;
-			uint64			sb_frextents;
-			xfs_ino_t		sb_uquotino;
-			xfs_ino_t		sb_gquotino;
-			uint16			sb_qflags;
-			uint8			sb_flags;
-			uint8			sb_shared_vn;
-			xfs_extlen_t	sb_inoalignmt;
-			uint32			sb_unit;
-			uint32			sb_width;
-			uint8			sb_dirblklog;
-			uint8			sb_logsectlog;
-			uint16			sb_logsectsize;
-			uint32			sb_logsunit;
-			uint32			sb_features2;
+			uint32				sb_magicnum;
+			uint32				sb_blocksize;
+			xfs_rfsblock_t		sb_dblocks;
+			xfs_rfsblock_t		sb_rblocks;
+			xfs_rtblock_t		sb_rextents;
+			uuid_t				sb_uuid;
+			xfs_fsblock_t		sb_logstart;
+			xfs_ino_t			sb_rootino;
+			xfs_ino_t			sb_rbmino;
+			xfs_ino_t			sb_rsumino;
+			xfs_agblock_t		sb_rextsize;
+			xfs_agblock_t		sb_agblocks;
+			xfs_agnumber_t		sb_agcount;
+			xfs_extlen_t		sb_rbmblocks;
+			xfs_extlen_t		sb_logblocks;
+			uint16				sb_versionnum;
+			uint16				sb_sectsize;
+			uint16				sb_inodesize;
+			uint16				sb_inopblock;
+			char				sb_fname[12];
+			uint8				sb_blocklog;
+			uint8				sb_sectlog;
+			uint8				sb_inodelog;
+			uint8				sb_inopblog;
+			uint8				sb_agblklog;
+			uint8				sb_rextslog;
+			uint8				sb_inprogress;
+			uint8				sb_imax_pct;
+			uint64				sb_icount;
+			uint64				sb_ifree;
+			uint64				sb_fdblocks;
+			uint64				sb_frextents;
+			xfs_ino_t			sb_uquotino;
+			xfs_ino_t			sb_gquotino;
+			uint16				sb_qflags;
+			uint8				sb_flags;
+			uint8				sb_shared_vn;
+			xfs_extlen_t		sb_inoalignmt;
+			uint32				sb_unit;
+			uint32				sb_width;
+			uint8				sb_dirblklog;
+			uint8				sb_logsectlog;
+			uint16				sb_logsectsize;
+			uint32				sb_logsunit;
+			uint32				sb_features2;
 };
 
 
@@ -148,13 +155,13 @@ Superblock quota flags - sb_qflags
 	Extended v4 Superblock flags - sb_features2
 */
 
-#define XFS_SB_VERSION2_LAZYSBCOUNTBIT	0x0001
+#define XFS_SB_VERSION2_LAZYSBCOUNTBIT	0x00000001
 	// update global free space and inode on clean unmount
-#define XFS_SB_VERSION2_ATTR2BIT	0x0002
+#define XFS_SB_VERSION2_ATTR2BIT	0x00000002
 	// optimises the inode layout of ext-attr
-#define XFS_SB_VERSION2_PARENTBIT 0x0004	/* Parent pointers */
-#define XFS_SB_VERSION2_PROJID32BIT 0x0008	/* 32-bit project id */
-#define XFS_SB_VERSION2_CRCBIT 0x0010		/* Metadata checksumming */
-#define XFS_SB_VERSION2_FTYPE 0x0020
+#define XFS_SB_VERSION2_PARENTBIT 0x00000010	/* Parent pointers */
+#define XFS_SB_VERSION2_PROJID32BIT 0x00000080	/* 32-bit project id */
+#define XFS_SB_VERSION2_CRCBIT 0x00000100		/* Metadata checksumming */
+#define XFS_SB_VERSION2_FTYPE 0x00000200
 
 #endif
