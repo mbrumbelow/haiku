79c0da4005747e4e5190ccd1a35575ae2e9e5cf8
CruxBox <shubhambhagat111@yahoo.com>
2020-06-25 17:43:07 +0530

    xfs: Read Short Form Directories
    We can now read short form directories.
    Change-Id: I23eb6ef52e8bb07643213e603ed0185f536c649f

--- src/add-ons/kernel/file_systems/xfs/Volume.h
+++ src/add-ons/kernel/file_systems/xfs/Volume.h
@@ -77,6 +77,9 @@ public:
 			uint8				SuperBlockFlags() const
 									{ return fSuperBlock.Flags(); }
 
+			uint32				SuperBlockFeatures2() const
+									{ return fSuperBlock.Features2(); }
+
 	#if 0
 			off_t				NumBlocks() const
 									{ return fSuperBlock.NumBlocks(); }
