9e80a6b545fd3ba0991ce9f15fb8b05e0fb8fadf
Leorize <leorize+oss@disroot.org>
2020-06-29 01:53:56 -0500

    support: Introduce BMemoryRingIO, a thread-safe ring buffer
    This commit introduces a simple thread-safe ring buffer implementation
    based on top of BDataIO. The main use case for this class will be to
    implement shared buffers between threads for the upcoming refactoring
    of Services Kit.
    
    Change-Id: I526bc044b28c91496ad996fabebe538e75647f2c

---
+++ headers/os/support/MemoryRingIO.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2020 Haiku, Inc. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef _MEMORY_RING_IO_H
+#define _MEMORY_RING_IO_H
+
+
+#include <pthread.h>
+
+#include <DataIO.h>
+#include <Locker.h>
+
+
+class BMemoryRingIO : public BDataIO {
+public:
+								BMemoryRingIO(size_t size);
+	virtual						~BMemoryRingIO();
+
+			status_t			InitCheck();
+
+	virtual	ssize_t				Read(void* buffer, size_t size);
+	virtual	ssize_t				Write(const void* buffer, size_t size);
+
+			status_t			SetSize(size_t size);
+			void				Clear();
+
+			size_t				BytesAvailable();
+			size_t				SpaceAvailable();
+			size_t				GetSize();
+
+			status_t			WaitForRead(
+									bigtime_t timeout = B_INFINITE_TIMEOUT);
+			status_t			WaitForWrite(
+									bigtime_t timeout = B_INFINITE_TIMEOUT);
+
+			void				SetEndReached(bool end);
+private:
+			pthread_mutex_t		fLock;
+			pthread_cond_t		fEvent;
+
+			char*				fBuffer;
+
+			size_t				fBufferSize;
+			size_t				fWriteAtNext;
+			size_t				fReadAtNext;
+
+			bool				fBufferFull;
+			bool				fEnded;
+};
+
+
+#endif	// _MEMORY_RING_IO_H
