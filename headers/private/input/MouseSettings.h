44bab89a557e5e05460d46dd18c41100a689e848
Preetpal Kaur <preetpalok123@gmail.com>
2020-07-17 15:29:24 +0530

    WIP: Update thr Input_Server files to get the settings working
    I have made the different functions in the InterfaceDefs.h and used them in the MouseSettings.h and MouseInputDevice.g files.
    Now, the code runs but there is not effect on the settings. The settings are workings defaults.
    Because the MouseSettings.h files linkes with the src/servers/input/InputServer.h file.
    
    Change-Id: I3d9880935990e671e6f98ff1895edc1e448000e0

--- headers/private/input/MouseSettings.h
+++ headers/private/input/MouseSettings.h
@@ -19,18 +19,16 @@
 #include <InterfaceDefs.h>
 #include <Point.h>
 #include <SupportDefs.h>
-#include <String.h>
 
 #include "kb_mouse_settings.h"
+
 
 class BPath;
 
 class MouseSettings {
 public:
-		// TODO: declare in fname in private and define its method
-		BString fname;
 		MouseSettings();
-		MouseSettings(mouse_settings settings, BString name);
+		MouseSettings(mouse_settings settings);
 		~MouseSettings();
 
 		void Revert();
