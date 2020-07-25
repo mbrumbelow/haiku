3bb88a5dc10bbf9a1d697c40402503c8902f0806
Suhel Mehta <suhelmehta@outlook.com>
2020-07-26 00:36:17 +0530

    ufs2: Implementing read function
    Attempting to read data from direct blocks.
    
    Right now it is reading directly from disk and it is reading very less
    data from disk.
    
    Change-Id: I8156aba53782da8c2bb4481db611ae26d8881b35

--- src/add-ons/kernel/file_systems/ufs2/DirectoryIterator.cpp
+++ src/add-ons/kernel/file_systems/ufs2/DirectoryIterator.cpp
@@ -10,7 +10,7 @@
 
 #include "Inode.h"
 
-//#define TRACE_UFS2
+#define TRACE_UFS2
 #ifdef TRACE_UFS2
 #	define TRACE(x...) dprintf("\33[34mufs2:\33[0m " x)
 #else
@@ -24,7 +24,7 @@ DirectoryIterator::DirectoryIterator(Inode* inode)
 	:
 	fInode(inode)
 {
-	fOffset = fInode->GetBlockPointer() * MINBSIZE;
+	fOffset = fInode->GetBlockPointer(0) * MINBSIZE;
 	TRACE("DirectoryIterator::DirectoryIterator() \n");
 }
 
@@ -81,10 +81,10 @@ DirectoryIterator::GetNext(char* name, size_t* _nameLength, ino_t* _id)
 		remainder = direct.namlen + 4;
 	}
 
+	TRACE("offset is %d\n",fOffset);
 	fOffset = fOffset + 8 + remainder;
 
 	if (direct.next_ino > 0) {
-		TRACE("direct.next_ino %d\n",direct.next_ino);
 
 		strlcpy(name, direct.name, remainder);
 		*_id = direct.next_ino;
