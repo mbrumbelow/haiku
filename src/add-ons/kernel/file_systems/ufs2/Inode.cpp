9bd8bf5774dcb4fc0dfa57c409b9b5405954e2b5
Suhel Mehta <suhelmehta@outlook.com>
2020-07-26 00:36:17 +0530

    ufs2: Implementing read function
    Attempting to read data from direct blocks even if the data stored is not in
    continous blocks.
    
    Change-Id: I8156aba53782da8c2bb4481db611ae26d8881b35

--- src/add-ons/kernel/file_systems/ufs2/Inode.cpp
+++ src/add-ons/kernel/file_systems/ufs2/Inode.cpp
@@ -5,8 +5,11 @@
  */
 
 #include "Inode.h"
+#include <string.h>
 
-#ifdef TRACE_ufs2
+
+#define TRACE_UFS2
+#ifdef TRACE_UFS2
 #define TRACE(x...) dprintf("\33[34mufs2:\33[0m " x)
 #else
 #define TRACE(x...) ;
@@ -36,6 +39,7 @@ Inode::Inode(Volume* volume, ino_t id)
 	int64_t offset_in_block = ino_to_fsbo(superblock, id);
 	int64_t offset = fs_block * MINBSIZE + offset_in_block * 256;
 
+//	ERROR("%ld\n\n",offset);
 	if (read_pos(fd, offset, (void*)&fNode, sizeof(fNode)) != sizeof(fNode))
 		ERROR("Inode::Inode(): IO Error\n");
 
@@ -84,4 +88,69 @@ status_t
 Inode::InitCheck()
 {
 	return fInitStatus;
+}
+
+
+status_t
+Inode::ReadAt(off_t file_offset, uint8* buffer, size_t* _length)
+{
+	int fd = fVolume->Device();
+	ufs2_super_block super_block = fVolume->SuperBlock();
+	int32_t block_size = super_block.fs_bsize;
+	int64_t size = Size();
+	off_t block_number = file_offset / block_size;
+	off_t block_offset = file_offset % block_size;
+	off_t pos = GetBlockPointer(block_number) * 4096 + block_offset;
+
+	ssize_t length = 0;
+	if (size > 0 && block_number < 12) {
+		//data of file is in first twelve direct block only
+		off_t total_block_size = (block_number + 1) * block_size;
+		ssize_t remaining_length = total_block_size - file_offset;
+		if (file_offset + *_length > size) {
+			if (file_offset + *_length > (block_number + 1) * block_size) {
+				while (true) {
+					//code for reading multiple blocks
+					char data[remaining_length] = {0};
+					strcat((char*)buffer, data);
+					if (size == file_offset + length)
+					{
+						*_length = length;
+						block_number = 0;
+						return B_OK;
+					}
+					block_number++;
+					pos = GetBlockPointer(block_number) * 4096;
+					remaining_length = size - file_offset - remaining_length;
+				}
+			}
+			length = read_pos(fd, pos, buffer, size - file_offset);
+			*_length = length;
+			block_number = 0;
+			return B_OK;
+		} else {
+			if (file_offset + *_length > (block_number + 1) * block_size) {
+				while (true) {
+					//code for reading multiple blocks
+					length += read_pos(fd, pos, buffer, remaining_length);
+					if (length == *_length)
+					{
+						return B_OK;
+					}
+					block_number++;
+					pos = GetBlockPointer(block_number) * 4096;
+					remaining_length = *_length - length;
+				}
+				block_number++;
+			}
+			length = read_pos(fd, pos, buffer, *_length);
+			if (length > 0) {
+				*_length = length;
+				return B_OK;
+			}
+		}
+		return length;
+	}
+
+	return B_NOT_SUPPORTED;
 }
