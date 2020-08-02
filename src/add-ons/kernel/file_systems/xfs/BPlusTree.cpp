c5b8526f1ed780d8b996fb1db64777c0e04691e4
CruxBox <shubhambhagat111@yahoo.com>
2020-08-02 13:09:08 +0530

    xfs: B+Tree ExtentMap reading
    We can now read all the extent maps from the leaves. This will be needed
    to implement the GetNext() functionality.
    
    Change-Id: Ie10b453c33bec6e6d3109743d695f86a90de45cd

--- src/add-ons/kernel/file_systems/xfs/BPlusTree.cpp
+++ src/add-ons/kernel/file_systems/xfs/BPlusTree.cpp
@@ -7,122 +7,202 @@
 #include "BPlusTree.h"
 
 
-void
-bplustree_short_block::SwapEndian()
+TreeDirectory::TreeDirectory(Inode* inode)
+	:
+	fInode(inode),
+	fRoot(NULL),
+	fExtents(NULL),
+	fSingleDirBlock(NULL),
+	fCountOfFilledExtents(0)
 {
-	bb_magic = B_BENDIAN_TO_HOST_INT32(bb_magic);
-	bb_level = B_BENDIAN_TO_HOST_INT16(bb_level);
-	bb_numrecs = B_BENDIAN_TO_HOST_INT16(bb_numrecs);
-	bb_leftsib = B_BENDIAN_TO_HOST_INT32(bb_leftsib);
-	bb_rightsib = B_BENDIAN_TO_HOST_INT32(bb_rightsib);
 }
 
 
-void
-bplustree_long_block::SwapEndian()
+TreeDirectory::~TreeDirectory()
 {
-	bb_magic = B_BENDIAN_TO_HOST_INT32(bb_magic);
-	bb_level = B_BENDIAN_TO_HOST_INT16(bb_level);
-	bb_numrecs = B_BENDIAN_TO_HOST_INT16(bb_numrecs);
-	bb_leftsib = B_BENDIAN_TO_HOST_INT64(bb_leftsib);
-	bb_rightsib = B_BENDIAN_TO_HOST_INT64(bb_rightsib);
+	delete fRoot;
+	delete fExtents;
+	delete fSingleDirBlock;
 }
 
 
-void
-xfs_alloc_rec::SwapEndian()
+status_t
+TreeDirectory::Init()
 {
-	ar_startblock = B_BENDIAN_TO_HOST_INT32(ar_startblock);
-	ar_blockcount =  B_BENDIAN_TO_HOST_INT32(ar_blockcount);
-}
+	fRoot = new(std::nothrow) BlockInDataFork;
+	if (fRoot == NULL)
+		return B_NO_MEMORY;
 
+	fSingleDirBlock = new(std::nothrow) char[fInode->DirBlockSize()];
+	if (fSingleDirBlock == NULL)
+		return B_NO_MEMORY;
 
-uint32
-BPlusTree::BlockSize()
-{
-	return fVolume.SuperBlock().BlockSize();
-}
+	memcpy((void*)fRoot,
+		DIR_DFORK_PTR(fInode->Buffer()), sizeof(BlockInDataFork));
 
-
-int
-BPlusTree::RecordSize()
-{
-	if (fRecType == ALLOC_FLAG)
-		return XFS_ALLOC_REC_SIZE;
-
-	return B_BAD_VALUE;
+	return B_OK;
 }
 
 
 int
-BPlusTree::MaxRecords(bool leaf)
+TreeDirectory::BlockLen()
 {
-	int blockLen = BlockSize();
+	return XFS_BTREE_LBLOCK_SIZE;
+}
 
-	if (fPtrType == SHORT_BLOCK_FLAG)
-		blockLen - XFS_BTREE_SBLOCK_SIZE;
 
-	if (leaf) {
-		if (fRecType == ALLOC_FLAG)
-			return blockLen / sizeof(xfs_alloc_rec_t);
-	} else {
-		if (fKeyType == ALLOC_FLAG) {
-			return blockLen / (sizeof(xfs_alloc_key_t)
-							+ sizeof(xfs_alloc_ptr_t));
+TreeKey
+TreeDirectory::GetKey(int pos)
+{
+	return BlockLen() + (pos - 1) * XFS_KEY_SIZE;
+}
+
+
+size_t
+TreeDirectory::KeySize()
+{
+	return XFS_KEY_SIZE;
+}
+
+
+size_t
+TreeDirectory::PtrSize()
+{
+	return XFS_PTR_SIZE;
+}
+
+
+TreePointer*
+TreeDirectory::GetPtr(int pos, LongBlock* curLongBlock)
+{
+	size_t availableSpace = fInode->GetVolume()->BlockSize() - BlockLen();
+	size_t maxRecords = availableSpace / (KeySize() + PtrSize());
+	size_t offsetIntoNode =
+		BlockLen() + maxRecords * KeySize() + (pos - 1) * PtrSize();
+	return (TreePointer*)((char*)curLongBlock + offsetIntoNode);
+}
+
+
+size_t
+TreeDirectory::MaxRecordsPossible(size_t len)
+{
+	len -= sizeof(BlockInDataFork);
+	return len / (KeySize() + PtrSize());
+}
+
+
+status_t
+TreeDirectory::GetAllExtents()
+{
+	xfs_extnum_t noOfExtents = fInode->DataExtentsCount();
+	ExtentMapUnwrap* extentsWrapped
+		= new(std::nothrow) ExtentMapUnwrap[noOfExtents];
+	if (extentsWrapped == NULL)
+		return B_NO_MEMORY;
+
+	Volume* volume = fInode->GetVolume();
+	uint16 levelsInTree = fRoot->Levels();
+
+	size_t lengthOfDataFork;
+	if (fInode->ForkOffset() != 0)
+		lengthOfDataFork = fInode->ForkOffset() << 3;
+	else
+		lengthOfDataFork = volume->InodeSize() - INODE_CORE_UNLINKED_SIZE;
+
+	TRACE("Length Of Data Fork: (%d)\n", lengthOfDataFork);
+	size_t maxRecords = MaxRecordsPossible(lengthOfDataFork);
+	TRACE("Maxrecords: (%d)\n", maxRecords);
+	TreePointer* ptrToNode = (TreePointer*)
+		((char*)DIR_DFORK_PTR(fInode->Buffer())
+			+ sizeof(BlockInDataFork) + maxRecords*KeySize());
+
+	size_t len = volume->BlockSize();
+	char node[len];
+		// This isn't for a directory block but for one of the tree nodes
+
+	TRACE("levels:(%d)\n", levelsInTree);
+	TRACE("Numrecs:(%d)\n", fRoot->NumRecords());
+
+	// Go down the tree by taking the leftest pointer to go to the first leaf
+	uint64 fileSystemBlockNo = B_BENDIAN_TO_HOST_INT64(*ptrToNode);
+	uint64 readPos = fInode->FileSystemBlockToAddr(fileSystemBlockNo);
+	while (levelsInTree !=1) {
+		fileSystemBlockNo = B_BENDIAN_TO_HOST_INT64(*ptrToNode);
+			// The fs block that contains node at next lower level. Now read.
+		readPos = fInode->FileSystemBlockToAddr(fileSystemBlockNo);
+		if (read_pos(volume->Device(), readPos, node, len) != len) {
+			ERROR("Extent::FillBlockBuffer(): IO Error");
+			return B_IO_ERROR;
 		}
+		LongBlock* curLongBlock = (LongBlock*)node;
+		ASSERT(curLongBlock->Magic() == XFS_BMAP_MAGIC);
+		ptrToNode = GetPtr(1, curLongBlock);
+			// Get's the first pointer. This points to next node.
+		levelsInTree--;
 	}
 
-	return B_BAD_VALUE;
+	// Next level wil contain leaf nodes. Now Read Directory Buffer
+	len = fInode->DirBlockSize();
+	if (read_pos(volume->Device(), readPos, fSingleDirBlock, len)
+		!= len) {
+		ERROR("Extent::FillBlockBuffer(): IO Error");
+		return B_IO_ERROR;
+	}
+	levelsInTree--;
+	ASSERT(levelsInTree == 0);
+
+	// We should be at the left most leaf node.
+	// This could be a multilevel node type directory
+	while (1) {
+		// Run till you have leaf blocks to checkout
+		char* leafBuffer = fSingleDirBlock;
+		ASSERT(((LongBlock*)leafBuffer)->Magic() == XFS_BMAP_MAGIC);
+		uint32 offset = sizeof(LongBlock);
+		int numRecs = ((LongBlock*)leafBuffer)->NumRecs();
+
+		for (int i = 0; i < numRecs; i++) {
+			extentsWrapped[fCountOfFilledExtents].first =
+				*(uint64*)(leafBuffer + offset);
+			extentsWrapped[fCountOfFilledExtents].second =
+				*(uint64*)(leafBuffer + offset + sizeof(uint64));
+			offset += sizeof(ExtentMapUnwrap);
+			fCountOfFilledExtents++;
+		}
+
+		fileSystemBlockNo = ((LongBlock*)leafBuffer)->Right();
+		TRACE("Next leaf is at: (%d)\n", fileSystemBlockNo);
+		if (fileSystemBlockNo == -1)
+			break;
+		uint64 readPos = fInode->FileSystemBlockToAddr(fileSystemBlockNo);
+		if (read_pos(volume->Device(), readPos, fSingleDirBlock, len)
+				!= len) {
+				ERROR("Extent::FillBlockBuffer(): IO Error");
+				return B_IO_ERROR;
+		}
+	}
+	TRACE("Total covered: (%d)\n", fCountOfFilledExtents);
+	return UnWrapExtents(extentsWrapped);
 }
 
 
-int
-BPlusTree::KeyLen()
+status_t
+TreeDirectory::UnWrapExtents(ExtentMapUnwrap* extentsWrapped)
 {
-	if (fKeyType == ALLOC_FLAG)
-		return XFS_ALLOC_REC_SIZE;
-	return B_BAD_VALUE;
-}
+	fExtents = new(std::nothrow) ExtentMapEntry[fCountOfFilledExtents];
+	if (fExtents == NULL)
+		return B_NO_MEMORY;
+	uint64 first, second;
 
+	for (int i = 0; i < fCountOfFilledExtents; i++) {
+		first = B_BENDIAN_TO_HOST_INT64(extentsWrapped[i].first);
+		second = B_BENDIAN_TO_HOST_INT64(extentsWrapped[i].second);
+		fExtents[i].br_state = first >> 63;
+		fExtents[i].br_startoff = (first & MASK(63)) >> 9;
+		fExtents[i].br_startblock = ((first & MASK(9)) << 43) | (second >> 21);
+		fExtents[i].br_blockcount = second & MASK(21);
+	}
+	delete extentsWrapped;
+	extentsWrapped = NULL;
 
-int
-BPlusTree::BlockLen()
-{
-	if (fPtrType == LONG_BLOCK_FLAG)
-		return XFS_BTREE_LBLOCK_SIZE;
-	if (fPtrType == SHORT_BLOCK_FLAG)
-		return XFS_BTREE_SBLOCK_SIZE;
-	return B_BAD_VALUE;
-}
-
-
-int
-BPlusTree::PtrLen()
-{
-	if (fPtrType == LONG_BLOCK_FLAG)
-		return sizeof(uint64);
-	else
-		return sizeof(uint32);
-}
-
-
-int
-BPlusTree::RecordOffset(int pos)
-{
-	return BlockLen() + (pos - 1) * RecordSize();
-}
-
-
-int
-BPlusTree::KeyOffset(int pos)
-{
-	return BlockLen() + (pos - 1) * KeyLen();
-}
-
-
-int
-BPlusTree::PtrOffset(int pos, int level)
-{
-	return BlockLen() + MaxRecords(level > 0) * KeyLen()
-		+ (pos - 1) * PtrLen();
+	return B_OK;
 }
