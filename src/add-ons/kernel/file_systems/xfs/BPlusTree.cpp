5e2efaa2a686b8d0d43df4953cfb77ada8404d74
CruxBox <shubhambhagat111@yahoo.com>
2020-08-03 12:08:38 +0530

    xfs: B+Tree GetNext() works
    The GetNext() works well for B+Trees and that wraps up the work needed
    for all kinds of GetNext().
    Change-Id: Ie965d3da273364f8fdbdb8faee5cb3c214881130

--- src/add-ons/kernel/file_systems/xfs/BPlusTree.cpp
+++ src/add-ons/kernel/file_systems/xfs/BPlusTree.cpp
@@ -13,7 +13,10 @@ TreeDirectory::TreeDirectory(Inode* inode)
 	fRoot(NULL),
 	fExtents(NULL),
 	fSingleDirBlock(NULL),
-	fCountOfFilledExtents(0)
+	fCountOfFilledExtents(0),
+	fCurMapIndex(0),
+	fOffset(0),
+	fCurBlockNumber(0)
 {
 }
 
@@ -109,7 +112,6 @@ TreeDirectory::GetAllExtents()
 	else
 		lengthOfDataFork = volume->InodeSize() - INODE_CORE_UNLINKED_SIZE;
 
-	TRACE("Length Of Data Fork: (%d)\n", lengthOfDataFork);
 	size_t maxRecords = MaxRecordsPossible(lengthOfDataFork);
 	TRACE("Maxrecords: (%d)\n", maxRecords);
 	TreePointer* ptrToNode = (TreePointer*)
@@ -182,6 +184,187 @@ TreeDirectory::GetAllExtents()
 	}
 	TRACE("Total covered: (%d)\n", fCountOfFilledExtents);
 	return UnWrapExtents(extentsWrapped);
+}
+
+
+status_t
+TreeDirectory::FillBuffer(int type, char* blockBuffer, int howManyBlocksFurthur)
+{
+	TRACE("FILLBUFFER\n");
+	ExtentMapEntry map;
+	if (type == DATA)
+		map = fExtents[fCurMapIndex];
+	#if 0
+	else if (type == LEAF)
+		map = fLeafMap;
+	else
+		return B_BAD_VALUE;
+	#endif
+
+	if (map.br_state !=0)
+		return B_BAD_VALUE;
+
+	size_t len = fInode->DirBlockSize();
+	if (blockBuffer == NULL) {
+		blockBuffer = new(std::nothrow) char[len];
+		if (blockBuffer == NULL)
+			return B_NO_MEMORY;
+	}
+
+	xfs_daddr_t readPos =
+		fInode->FileSystemBlockToAddr(map.br_startblock + howManyBlocksFurthur);
+
+	if (read_pos(fInode->GetVolume()->Device(), readPos, blockBuffer, len)
+		!= len) {
+		ERROR("Extent::FillBlockBuffer(): IO Error");
+		return B_IO_ERROR;
+	}
+
+	if (type == DATA) {
+		fSingleDirBlock = blockBuffer;
+		ExtentDataHeader* header = (ExtentDataHeader*) fSingleDirBlock;
+		if (B_BENDIAN_TO_HOST_INT32(header->magic) == HEADER_MAGIC) {
+			TRACE("DATA BLOCK VALID\n");
+		} else {
+			TRACE("DATA BLOCK INVALID\n");
+			return B_BAD_VALUE;
+		}
+	} 
+	#if 0
+	else if (type == LEAF) {
+		fLeafBuffer = blockBuffer;
+		ExtentLeafHeader* header = (ExtentLeafHeader*) fLeafBuffer;
+	}
+	#endif
+	return B_OK;
+}
+
+
+int
+TreeDirectory::EntrySize(int len) const
+{
+	int entrySize= sizeof(xfs_ino_t) + sizeof(uint8) + len + sizeof(uint16);
+			// uint16 is for the tag
+	if (fInode->HasFileTypeField())
+		entrySize += sizeof(uint8);
+
+	return (entrySize + 7) & -8;
+			// rounding off to closest multiple of 8
+}
+
+
+/*
+ * Throw in the desired block number and get the index of it
+ */
+int
+TreeDirectory::SearchAndFillMap(int blockNo)
+{
+	ExtentMapEntry map;
+	for (int i = 0; i < fCountOfFilledExtents; i++) {
+		map = fExtents[i];
+		if (map.br_startoff <= blockNo
+			&& (blockNo <= map.br_startoff + map.br_blockcount - 1))
+			// Map found
+			return i;
+	}
+	/* unlikely if this is coming from GetNext()
+	 * since that'd contain allextent maps of the directory.
+	 */
+	return -1;
+}
+
+
+status_t
+TreeDirectory::GetNext(char* name, size_t* length, xfs_ino_t* ino)
+{
+	TRACE("TreeDirectory::GetNext\n");
+	status_t status;
+	if (fExtents == NULL) {
+		status = GetAllExtents();
+		if (status != B_OK)
+			return status;
+	}
+	status = FillBuffer(DATA, fSingleDirBlock, 0);
+	if (status != B_OK)
+		return status;
+
+	Volume* volume = fInode->GetVolume();
+	void* entry = (void*)((ExtentDataHeader*)fSingleDirBlock + 1);
+		// This could be an unused entry so we should check
+
+	uint32 blockNoFromAddress = BLOCKNO_FROM_ADDRESS(fOffset, volume);
+	if (fOffset != 0 && blockNoFromAddress == fCurBlockNumber)
+		entry = (void*)(fSingleDirBlock + BLOCKOFFSET_FROM_ADDRESS(fOffset, fInode));
+		// This gets us a little faster to the next entry
+
+	uint32 curDirectorySize = fInode->Size();
+	ExtentMapEntry& map = fExtents[fCurMapIndex];
+	while (fOffset != curDirectorySize) {
+		blockNoFromAddress = BLOCKNO_FROM_ADDRESS(fOffset, volume);
+
+		TRACE("fOffset:(%d), blockNoFromAddress:(%d)\n",
+			fOffset, blockNoFromAddress);
+		if (fCurBlockNumber != blockNoFromAddress
+			&& blockNoFromAddress > map.br_startoff
+			&& blockNoFromAddress
+				<= map.br_startoff + map.br_blockcount - 1) {
+			// When the block is mapped in the same data
+			// map entry but is not the first block
+			status = FillBuffer(DATA, fSingleDirBlock,
+				blockNoFromAddress - map.br_startoff);
+			if (status != B_OK)
+				return status;
+			entry = (void*)((ExtentDataHeader*)fSingleDirBlock + 1);
+			fOffset = fOffset + sizeof(ExtentDataHeader);
+			fCurBlockNumber = blockNoFromAddress;
+		} else if (fCurBlockNumber != blockNoFromAddress) {
+			// When the block isn't mapped in the current data map entry
+			fCurMapIndex = SearchAndFillMap(blockNoFromAddress);
+			map = fExtents[fCurMapIndex];
+			status = FillBuffer(DATA, fSingleDirBlock,
+				blockNoFromAddress - map.br_startoff);
+			if (status != B_OK)
+				return status;
+			entry = (void*)((ExtentDataHeader*)fSingleDirBlock + 1);
+			fOffset = fOffset + sizeof(ExtentDataHeader);
+			fCurBlockNumber = blockNoFromAddress;
+		}
+
+		ExtentUnusedEntry* unusedEntry = (ExtentUnusedEntry*)entry;
+
+		if (B_BENDIAN_TO_HOST_INT16(unusedEntry->freetag) == DIR2_FREE_TAG) {
+			TRACE("Unused entry found\n");
+			fOffset = fOffset + B_BENDIAN_TO_HOST_INT16(unusedEntry->length);
+			entry = (void*)
+				((char*)entry + B_BENDIAN_TO_HOST_INT16(unusedEntry->length));
+			continue;
+		}
+		ExtentDataEntry* dataEntry = (ExtentDataEntry*) entry;
+
+		uint16 currentOffset = (char*)dataEntry - fSingleDirBlock;
+		TRACE("GetNext: fOffset:(%d), currentOffset:(%d)\n",
+			BLOCKOFFSET_FROM_ADDRESS(fOffset, fInode), currentOffset);
+
+		if (BLOCKOFFSET_FROM_ADDRESS(fOffset, fInode) > currentOffset) {
+			entry = (void*)((char*)entry + EntrySize(dataEntry->namelen));
+			continue;
+		}
+
+		if (dataEntry->namelen + 1 > *length)
+			return B_BUFFER_OVERFLOW;
+
+		fOffset = fOffset + EntrySize(dataEntry->namelen);
+		memcpy(name, dataEntry->name, dataEntry->namelen);
+		name[dataEntry->namelen] = '\0';
+		*length = dataEntry->namelen + 1;
+		*ino = B_BENDIAN_TO_HOST_INT64(dataEntry->inumber);
+
+		TRACE("Entry found. Name: (%s), Length: (%ld),ino: (%ld)\n", name,
+			*length, *ino);
+		return B_OK;
+	}
+
+	return B_ENTRY_NOT_FOUND;
 }
 
 
