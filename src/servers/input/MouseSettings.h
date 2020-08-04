7c278905d66c3489145efe5c3d6df51063419c21
Preetpal Kaur <preetpalok123@gmail.com>
2020-08-04 16:55:07 +0530

    Added the MouseSettings in the InputServer
    Change-Id: I4d96bb3702160c4ab9cf5411573e9609ee1f88a6

---
+++ src/servers/input/MouseSettings.h
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2019, Haiku, Inc.
+ * Distributed under the terms of the MIT License.
+ *
+ * Author:
+ *		Preetpal Kaur <preetpalok123@gmail.com>
+ */
+
+
+#ifndef MOUSE_SETTINGS_H
+#define MOUSE_SETTINGS_H
+
+
+#include <map>
+
+
+#include <Archivable.h>
+#include <Input.h>
+#include <InterfaceDefs.h>
+#include <Path.h> 
+#include <Point.h>
+#include <SupportDefs.h>
+#include <String.h>
+
+#include "kb_mouse_settings.h"
+
+class MouseSettings {
+public:
+		BString fname;
+		MouseSettings(BString name);
+		MouseSettings(mouse_settings settings, BString name);
+		~MouseSettings();
+
+		void Defaults();
+		void Dump();
+
+		int32 MouseType() const { return fSettings.type; }
+		void SetMouseType(int32 type);
+
+		bigtime_t ClickSpeed() const;
+		void SetClickSpeed(bigtime_t click_speed);
+
+		int32 MouseSpeed() const { return fSettings.accel.speed; }
+		void SetMouseSpeed(int32 speed);
+
+		int32 AccelerationFactor() const 
+				{ return fSettings.accel.accel_factor; }
+		void SetAccelerationFactor(int32 factor);
+
+		uint32 Mapping(int32 index) const;
+		void Mapping(mouse_map &map) const;
+		void SetMapping(int32 index, uint32 button);
+		void SetMapping(mouse_map &map);
+
+		mode_mouse MouseMode() const { return fMode; }
+		void SetMouseMode(mode_mouse mode);
+
+		mode_focus_follows_mouse FocusFollowsMouseMode() const {
+			return fFocusFollowsMouseMode;
+		}
+		void SetFocusFollowsMouseMode(mode_focus_follows_mouse mode);
+
+		bool AcceptFirstClick() const { return fAcceptFirstClick; }
+		void SetAcceptFirstClick(bool accept_first_click);
+
+		void RetrieveSettings();
+
+		mouse_settings* GetSettings();
+		mouse_settings	fSettings, fOriginalSettings;
+
+private:
+		static status_t GetSettingsPath(BPath &path);
+
+		mode_mouse		fMode, fOriginalMode;
+		mode_focus_follows_mouse	fFocusFollowsMouseMode;
+		mode_focus_follows_mouse	fOriginalFocusFollowsMouseMode;
+		bool			fAcceptFirstClick;
+		bool			fOriginalAcceptFirstClick;
+
+//		mouse_settings	fSettings, fOriginalSettings;
+};
+
+
+// We want the settings to be stored in a BMessage
+class MultipleMouseSettings: public BArchivable {
+	public:
+		MultipleMouseSettings();
+		~MultipleMouseSettings();
+
+		status_t Archive(BMessage* into, bool deep = false) const;
+
+		void Defaults();
+		void Dump();
+		status_t SaveSettings();
+
+
+		MouseSettings* AddMouseSettings(BString mouse_name);
+		MouseSettings* GetMouseSettings(BString mouse_name);
+
+	private:
+		static status_t GetSettingsPath(BPath &path);
+		void RetrieveSettings();
+
+		MouseSettings*	fDeprecatedMouseSettings;
+
+		typedef std::map<BString, MouseSettings*> mouse_settings_object;
+		mouse_settings_object  fMouseSettingsObject;
+};
+
+#endif	// MOUSE_SETTINGS_H
