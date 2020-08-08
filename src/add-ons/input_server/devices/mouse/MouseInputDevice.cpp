58193ef767c2ca45b5c3d0eb17bf476f29772317
Preetpal Kaur <preetpalok123@gmail.com>
2020-08-08 17:43:56 +0530

    InterfaceDefs and Preferences: Muliple mouse speed
    Still the settings are working only for one mouse and get applied to the
    other mices. Settings are getting stored but not working according to
    it. Please review it!
    
    Change-Id: Iaccf919fe421ea92d2fd0983095c5007cd4ed68c

--- src/add-ons/input_server/devices/mouse/MouseInputDevice.cpp
+++ src/add-ons/input_server/devices/mouse/MouseInputDevice.cpp
@@ -515,7 +515,6 @@ void
 MouseDevice::_UpdateSettings()
 {
 	MD_CALLED();
-
 	// retrieve current values
 
 	if (get_mouse_map(&fSettings.map) != B_OK)
@@ -530,7 +529,7 @@ MouseDevice::_UpdateSettings()
 	else
 		ioctl(fDevice, MS_SET_CLICKSPEED, &fSettings.click_speed);
 
-	if (get_mouse_speed(&fSettings.accel.speed) != B_OK)
+	if (get_mouse_speed_by_name(fDeviceRef.name, &fSettings.accel.speed) != B_OK)
 		LOG_ERR("error when get_mouse_speed\n");
 	else {
 		if (get_mouse_acceleration(&fSettings.accel.accel_factor) != B_OK)
@@ -544,7 +543,7 @@ MouseDevice::_UpdateSettings()
 		}
 	}
 
-	if (get_multiple_mouse_type(fDeviceRef.name, &fSettings.type) != B_OK)
+	if (get_mouse_type_by_name(fDeviceRef.name, &fSettings.type) != B_OK)
 		LOG_ERR("error when get_mouse_type\n");
 	else
 		ioctl(fDevice, MS_SET_TYPE, &fSettings.type);
