c20e19b8d59b35766a60a7c36481c332c3187f2b
X512 <danger_mail@list.ru>
2020-08-27 13:00:10 +0200

    app_server memory management: use ObjectDeleter to mark ownership
    Make object ownership explicit by use of ObjectDeleter where possible.
    
    Change-Id: I499a00aa3390d1510ae284419e73faffa5166430

--- src/servers/app/drawing/AlphaMask.cpp
+++ src/servers/app/drawing/AlphaMask.cpp
@@ -398,7 +398,6 @@ PictureAlphaMask::PictureAlphaMask(AlphaMask* previousMask,
 
 PictureAlphaMask::~PictureAlphaMask()
 {
-	delete fDrawState;
 }
 
 
@@ -413,7 +412,7 @@ BRect
 PictureAlphaMask::DetermineBoundingBox() const
 {
 	BRect boundingBox;
-	PictureBoundingBoxPlayer::Play(fPicture, fDrawState, &boundingBox);
+	PictureBoundingBoxPlayer::Play(fPicture, fDrawState.Get(), &boundingBox);
 
 	if (!boundingBox.IsValid())
 		return boundingBox;
@@ -432,7 +431,7 @@ PictureAlphaMask::DetermineBoundingBox() const
 const DrawState&
 PictureAlphaMask::GetDrawState() const
 {
-	return *fDrawState;
+	return *fDrawState.Get();
 }
 
 
