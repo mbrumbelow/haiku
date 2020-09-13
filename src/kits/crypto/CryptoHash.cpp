3f08eaf5c7820dfce9d4d352ff12454df432b9ad
Alexander von Gluck IV <kallisti5@unixzen.com>
2020-09-13 09:22:44 -0500

    new kit: Crypto Kit.
    This new kit will put together various cryptography related
    algorithms and tools.
    
    For now, the main supported thing is hashing:
    
    * Blake2
    * MD4
    * MD5
    * SHA256
    
    Change-Id: I150556161366277789251da89b8c9e549761333f

---
+++ src/kits/crypto/CryptoHash.cpp
@@ -0,0 +1,168 @@
+/*
+ * Copyright 2015-2022, Haiku, Inc. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ *		Alexander von Gluck IV <kallisti5@unixzen.com>
+ */
+
+#include <DataIO.h>
+#include <Debug.h>
+#include <CryptoHash.h>
+#include <String.h>
+
+#include <new>
+
+// Our supported algorithms
+#include "Blake2.h"
+#include "MD4.h"
+#include "MD5.h"
+#include "SHA256.h"
+
+
+#undef TRACE
+
+// #define TRACE_CRYPTO
+#ifdef TRACE_CRYPTO
+#   define TRACE(x...) _sPrintf("BCryptoHash: " x)
+#else
+#   define TRACE(x...) ;
+#endif
+
+#define ERROR(x...) _sPrintf("BCryptoHash: " x)
+
+
+BCryptoHash::BCryptoHash(algorithm_type algorithm)
+	:
+	fAlgorithm(NULL),
+	fPosition(0)
+{
+	switch (algorithm) {
+		case B_HASH_BLAKE2:
+			fAlgorithm = new BlakeAlgorithm();
+			break;
+		case B_HASH_MD4:
+			fAlgorithm = new MD4Algorithm();
+			break;
+		case B_HASH_MD5:
+			fAlgorithm = new MD5Algorithm();
+			break;
+		case B_HASH_SHA256:
+			fAlgorithm = new SHA256Algorithm();
+			break;
+		case B_HASH_SHA512:
+		default:
+			ERROR("algorithm not yet implemented!\n");
+	}
+}
+
+
+BCryptoHash::~BCryptoHash()
+{
+	if (fAlgorithm != NULL)
+		delete fAlgorithm;
+}
+
+
+BString
+BCryptoHash::Name()
+{
+	if (fAlgorithm != NULL)
+		return fAlgorithm->Name();
+
+	return "UNAVAILABLE";
+}
+
+
+status_t
+BCryptoHash::AddData(const void* input, size_t length)
+{
+	if (input == NULL || length == 0)
+		return B_BAD_VALUE;
+
+	size_t bufferLength = fAlgorithm->MaxBuffer();
+	size_t pos = 0;
+
+	while (pos < length) {
+		char buffer[bufferLength];
+		if ((length - pos) < bufferLength)
+			bufferLength = length - pos;
+		memcpy(buffer, (char*)input + pos, bufferLength);
+		fAlgorithm->Update(buffer, bufferLength);
+		pos += bufferLength;
+	}
+
+	fPosition += length;
+	return B_OK;
+}
+
+
+status_t
+BCryptoHash::AddData(const BString& input)
+{
+	if (input == NULL)
+		return B_BAD_VALUE;
+	return AddData((void*)input.String(), input.Length());
+}
+
+
+status_t
+BCryptoHash::AddData(BFile& file)
+{
+	if (file.InitCheck() != B_OK)
+		return B_BAD_VALUE;
+
+	if (!file.IsReadable())
+		return B_PERMISSION_DENIED;
+
+	size_t bufferLength = fAlgorithm->MaxBuffer();
+	char buffer[bufferLength];
+	off_t length;
+
+	file.Seek(0, SEEK_SET);
+	file.GetSize(&length);
+	while (file.Position() < length) {
+		ssize_t len = file.Read(&buffer, bufferLength);
+		fAlgorithm->Update(buffer, len);
+	}
+
+	fPosition += length;
+	return B_OK;
+}
+
+
+status_t
+BCryptoHash::Flush()
+{
+	fPosition = 0;
+
+	if (fAlgorithm != NULL)
+		fAlgorithm->Flush();
+
+	return B_OK;
+}
+
+
+const size_t
+BCryptoHash::Position()
+{
+	return fPosition;
+}
+
+
+status_t
+BCryptoHash::Result(BString& hash)
+{
+	size_t digestLength = fAlgorithm->DigestLength();
+	uint8 result[digestLength];
+	memset(result, 0, digestLength);
+	fAlgorithm->Finish(result);
+
+	// use 3 character for number and a nul terminator
+	char hex[3];
+	for (size_t j = 0; j < digestLength; j++) {
+		sprintf(hex, "%02x", result[j]);
+		hash.Append(hex, 2);
+	}
+	return B_OK;
+}
