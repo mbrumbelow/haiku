0f97c607f8d25e7e7e6674ec8253e1823092f72f
Jérôme Duval <jerome.duval@gmail.com>
2020-11-07 12:02:58 +0100

    POSIX: lseek: support SEEK_DATA and SEEK_HOLE constants
    this is queued for issue 8: https://www.austingroupbugs.net/view.php?id=415
    this implementation calls the ioctl hook of the filesystem with BSD-like constants
    FIOSEEKDATA and FIOSEEKHOLE. if the hook doesn't know the constants, we use the stat size
    to return the last hole (as proposed in the draft spec).

--- src/system/kernel/fs/vfs.cpp
+++ src/system/kernel/fs/vfs.cpp
@@ -18,6 +18,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/file.h>
+#include <sys/ioctl.h>
 #include <sys/resource.h>
 #include <sys/stat.h>
 #include <unistd.h>
@@ -5856,6 +5857,39 @@ file_seek(struct file_descriptor* descriptor, off_t pos, int seekType)
 				}
 			}
 
+			break;
+		}
+		case SEEK_DATA:
+		case SEEK_HOLE:
+		{
+			status_t status = B_BAD_VALUE;
+			if (HAS_FS_CALL(vnode, ioctl)) {
+				offset = pos;
+				status_t status = FS_CALL(vnode, ioctl, descriptor->cookie,
+					seekType == SEEK_DATA ? FIOSEEKDATA : FIOSEEKHOLE,
+					&offset, sizeof(offset));
+				if (status == B_OK && offset > pos)
+					offset -= pos;
+			}
+			if (status != B_OK) {
+				if (status != B_BAD_VALUE)
+					return status;
+
+				// basic implementation with stat() the node
+				if (!HAS_FS_CALL(vnode, read_stat) || isDevice)
+					return B_BAD_VALUE;
+
+				struct stat stat;
+				status_t status = FS_CALL(vnode, read_stat, &stat);
+				if (status != B_OK)
+					return status;
+
+				off_t end = stat.st_size;
+				if (pos < end) {
+					offset = seekType == SEEK_HOLE ? end - pos : 0;
+				} else
+					return ENXIO;
+			}
 			break;
 		}
 		default:
