5a7f1e8f2c75681bec09515373ccab024d3ac1e7
Pascal Abresch <nep@packageloss.eu>
2020-11-21 18:12:16 +0100

    tracker kit: enable background image in list mode

--- src/kits/tracker/BackgroundImage.cpp
+++ src/kits/tracker/BackgroundImage.cpp
@@ -384,7 +384,7 @@ BackgroundImage::Refresh(BackgroundImage* oldBackgroundImage,
 	}
 
 	BackgroundImage* backgroundImage = GetBackgroundImage(fromNode, desktop);
-	if (backgroundImage != NULL && poseView->ViewMode() != kListMode)
+	if (backgroundImage != NULL)
 		backgroundImage->Show(poseView, current_workspace());
 
 	return backgroundImage;
