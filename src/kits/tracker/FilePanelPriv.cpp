244839abcb45c10c5f1437b1ae42afb04725e648
John Scipione <jscipione@gmail.com>
2020-11-28 21:09:06 -0500

    Tracker: Live updating shiftable menu items
    Live updating menu classes listen for B_MODIFIERS_CHANGED
    messages and update the item label and shortcut on Shift.
    
    -- File --
    Create link => Create relative link
    Move to Trash => Delete
    Cut => Cut more
    Copy => Copy more
    Paste => Paste links
    Identify => Force identify
    
    -- Window --
    Close => Close all
    Open parent Enable/Disable
    
    -- Window => Arrange by/Desktop --
    Clean up => Clean up all
    
    Create TLiveMenu and TLivePopUpMenu classes that inherit from
    BMenu and BPopUpMenu respectively.
    
    Create TLiveFileMenu, TLivePosePopUpMenu, TLiveWindowMenu and
    TLiveWindowPopUpMenu subclasses for the File and Window menu
    and context menus respectively.
    
    Create TLiveMixin to share methods between menus. Pass in parent
    window and use window shortcuts to enable/disable "Open parent".
    
    Built on top of Shortcuts refactor.
    
    Change-Id: I5dfbd4d468fad02894f1f31aa08d1abf630a4b5d

--- src/kits/tracker/FilePanelPriv.cpp
+++ src/kits/tracker/FilePanelPriv.cpp
@@ -75,6 +75,7 @@ All rights reserved.
 #include "FSUtils.h"
 #include "FavoritesMenu.h"
 #include "IconMenuItem.h"
+#include "LiveMenu.h"
 #include "MimeTypes.h"
 #include "NavMenu.h"
 #include "Shortcuts.h"
@@ -854,7 +855,7 @@ TFilePanel::AddMenus()
 {
 	// File
 
-	fFileMenu = new BMenu(B_TRANSLATE("File"));
+	fFileMenu = new TLiveFileMenu(B_TRANSLATE("File"), this);
 	AddFileMenu(fFileMenu);
 	fMenuBar->AddItem(fFileMenu);
 
@@ -1104,7 +1105,7 @@ TFilePanel::RepopulateMenus()
 		fMenuBar->RemoveItem(fFileMenu);
 		delete fFileMenu;
 		if (ShouldAddMenus()) {
-			fFileMenu = new BMenu(B_TRANSLATE("File"));
+			fFileMenu = new TLiveFileMenu(B_TRANSLATE("File"), this);
 			AddFileMenu(fFileMenu);
 			fMenuBar->AddItem(fFileMenu, 0);
 			UpdateMenu(fFileMenu, kFileMenuContext);
@@ -1112,12 +1113,12 @@ TFilePanel::RepopulateMenus()
 	}
 
 	delete fPoseContextMenu;
-	fPoseContextMenu = new BPopUpMenu("PoseContext", false, false);
+	fPoseContextMenu = new TLivePosePopUpMenu("PoseContext", this, false, false);
 	fPoseContextMenu->SetFont(be_plain_font);
 	AddPoseContextMenu(fPoseContextMenu);
 
 	delete fWindowContextMenu;
-	fWindowContextMenu = new BPopUpMenu("WindowContext", false, false);
+	fWindowContextMenu = new TLiveWindowPopUpMenu("WindowContext", this, false, false);
 	fWindowContextMenu->SetFont(be_plain_font);
 	AddWindowContextMenu(fWindowContextMenu);
 }
