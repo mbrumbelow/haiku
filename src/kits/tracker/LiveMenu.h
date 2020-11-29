4cce10d5ff5e04ca243f4f0a8255de9d57e8de0e
John Scipione <jscipione@gmail.com>
2020-11-28 21:09:06 -0500

    Tracker: Live updating shiftable menu items
    Live updating menu classes listen for B_MODIFIERS_CHANGED
    messages and update the item label and shortcut on Shift.
    
    -- File --
    Create link => Create relative link
    Move to Trash => Delete
    Cut => Cut more
    Copy => Copy more
    Paste => Paste links
    Identify => Force identify
    
    -- Window --
    Close => Close all
    
    -- Window => Arrange by/Desktop --
    Clean up => Clean up all
    
    Create TLiveMenu and TLivePopUpMenu classes that inherit from
    BMenu and BPopUpMenu respectively.
    
    Create TLiveFileMenu, TLivePosePopUpMenu, TLiveWindowMenu and
    TLiveWindowPopUpMenu subclasses for the File and Window menu
    and context menus respectively.
    
    Create TLiveMixin to share methods between menu and pop up menu.
    
    Built on top of Shortcuts refactor.
    
    Change-Id: I5dfbd4d468fad02894f1f31aa08d1abf630a4b5d

---
+++ src/kits/tracker/LiveMenu.h
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2020-2023 Haiku, Inc. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ *		John Scipione, jscipione@gmail.com
+ */
+#ifndef _LIVE_MENU_H
+#define _LIVE_MENU_H
+
+
+#include <Menu.h>
+#include <PopUpMenu.h>
+
+
+namespace BPrivate {
+
+// mixin class for sharing virtual methods
+struct TLiveMixin {
+	virtual	void			UpdateFileMenu(BMenu* menu);
+	virtual	void			UpdateWindowMenu(BMenu* menu);
+};
+
+
+class TLiveMenu : public BMenu {
+public:
+							TLiveMenu(const char* label);
+	virtual 				~TLiveMenu();
+
+	virtual	void			MessageReceived(BMessage* message);
+
+protected:
+	virtual	void			Update();
+};
+
+
+class TLivePopUpMenu : public BPopUpMenu {
+public:
+							TLivePopUpMenu(const char* label,
+								bool radioMode = true,
+								bool labelFromMarked = true,
+								menu_layout layout = B_ITEMS_IN_COLUMN);
+	virtual					~TLivePopUpMenu();
+
+	virtual	void			MessageReceived(BMessage* message);
+
+protected:
+	virtual	void			Update();
+};
+
+
+class TLiveArrangeByMenu: public TLiveMenu, public TLiveMixin {
+public:
+							TLiveArrangeByMenu(const char* label);
+	virtual					~TLiveArrangeByMenu();
+
+protected:
+	virtual	void			Update();
+};
+
+
+class TLiveFileMenu : public TLiveMenu, public TLiveMixin {
+public:
+							TLiveFileMenu(const char* label);
+	virtual					~TLiveFileMenu();
+
+protected:
+	virtual	void			Update();
+};
+
+class TLivePosePopUpMenu : public TLivePopUpMenu, public TLiveMixin {
+public:
+							TLivePosePopUpMenu(const char* label,
+								bool radioMode = true,
+								bool labelFromMarked = true,
+								menu_layout layout = B_ITEMS_IN_COLUMN);
+	virtual					~TLivePosePopUpMenu();
+
+protected:
+	virtual	void			Update();
+};
+
+
+class TLiveWindowMenu : public TLiveMenu, public TLiveMixin {
+public:
+							TLiveWindowMenu(const char* label);
+	virtual					~TLiveWindowMenu();
+
+protected:
+	virtual	void			Update();
+};
+
+class TLiveWindowPopUpMenu : public TLivePopUpMenu, public TLiveMixin {
+public:
+							TLiveWindowPopUpMenu(const char* label,
+								bool radioMode = true,
+								bool labelFromMarked = true,
+								menu_layout layout = B_ITEMS_IN_COLUMN);
+	virtual					~TLiveWindowPopUpMenu();
+
+protected:
+	virtual	void			Update();
+};
+
+} // namespace BPrivate
+
+using namespace BPrivate;
+
+
+#endif	// _LIVE_MENU_H
