26cf85b64d6146e79a882efbd4a4548c3ba531b6
Alexander von Gluck IV <kallisti5@unixzen.com>
2021-01-15 08:50:14 -0600

    libroot/riscv64: fix fenv implementation
    Change-Id: I9897cdadf2a1c50bb04601118dd801d75fa86263

--- src/system/libroot/posix/arch/riscv64/fenv.c
+++ src/system/libroot/posix/arch/riscv64/fenv.c
@@ -26,8 +26,30 @@
  * $FreeBSD$
  */
 
-#if 0 // disabled for now
-#include <posix/fenv.h>
+#define	__fenv_static
+#include "fenv.h"
 
-const fenv_t __fe_dfl_env = 0x00000000;
+#ifdef __GNUC_GNU_INLINE__
+#error "This file must be compiled with C99 'inline' semantics"
 #endif
+
+/*
+ * Hopefully the system ID byte is immutable, so it's valid to use
+ * this as a default environment.
+ */
+const fenv_t __fe_dfl_env = 0;
+
+extern inline int feclearexcept(int __excepts);
+extern inline int fegetexceptflag(fexcept_t *__flagp, int __excepts);
+extern inline int fesetexceptflag(const fexcept_t *__flagp, int __excepts);
+extern inline int feraiseexcept(int __excepts);
+extern inline int fetestexcept(int __excepts);
+extern inline int fegetround(void);
+extern inline int fesetround(int __round);
+extern inline int fegetenv(fenv_t *__envp);
+extern inline int feholdexcept(fenv_t *__envp);
+extern inline int fesetenv(const fenv_t *__envp);
+extern inline int feupdateenv(const fenv_t *__envp);
+extern inline int feenableexcept(int __mask);
+extern inline int fedisableexcept(int __mask);
+extern inline int fegetexcept(void);
