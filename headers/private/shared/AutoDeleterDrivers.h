47aa6bf15b2759281808865c0d9429c8efb0e315
X512 <danger_mail@list.ru>
2021-05-26 22:23:38 +0900

    AutoDeleter: add VMAddressSpacePutter, DeviceNodePutter
    Change-Id: I705de24f6a87287943814e748d5a02e43b029e60

--- headers/private/shared/AutoDeleterDrivers.h
+++ headers/private/shared/AutoDeleterDrivers.h
@@ -11,6 +11,8 @@
 #if defined(_KERNEL_MODE) && !defined(_BOOT_MODE)
 #include <vfs.h>
 #include <fs/fd.h>
+#include <vm/VMAddressSpace.h>
+#include <device_manager.h>
 #endif
 
 
@@ -23,6 +25,14 @@ typedef CObjectDeleter<void, status_t, unload_driver_settings> DriverSettingsUnl
 
 typedef CObjectDeleter<struct vnode, void, vfs_put_vnode> VnodePutter;
 typedef CObjectDeleter<file_descriptor, void, put_fd> DescriptorPutter;
+typedef MethodDeleter<VMAddressSpace, void, &VMAddressSpace::Put> VMAddressSpacePutter;
+
+#if __GNUC__ >= 4
+
+template <device_manager_info **deviceManager>
+using DeviceNodePutter = FieldFunctionDeleter<device_node, device_manager_info, deviceManager, void, &device_manager_info::put_node>;
+
+#endif
 
 #endif
 
@@ -36,6 +46,10 @@ using ::BPrivate::DriverSettingsUnloader;
 
 using ::BPrivate::VnodePutter;
 using ::BPrivate::DescriptorPutter;
+using ::BPrivate::VMAddressSpacePutter;
+#if __GNUC__ >= 4
+using ::BPrivate::DeviceNodePutter;
+#endif
 
 #endif
 
