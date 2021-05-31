eac1e581808831622d09ee4257124934c30c7836
Panagiotis Vasilopoulos <hello@alwayslivid.com>
2021-05-31 11:39:56 +0000

    AboutSystem: Cleaned up AboutSystem.cpp, added header file

--- src/apps/aboutsystem/AboutSystem.cpp
+++ src/apps/aboutsystem/AboutSystem.cpp
@@ -1,14 +1,16 @@
 /*
- * Copyright 2005-2018, Haiku, Inc.
+ * Copyright 2005-2021, Haiku, Inc.
  * Distributed under the terms of the MIT license.
  *
  * Authors:
  *		Augustin Cavalier <waddlesplash>
  *		DarkWyrm <bpmagic@columbus.rr.com>
+ *      Panagiotis Vasilopoulos <hello@alwayslivid.com>
  *		René Gollent
  *		Wim van der Meer <WPJvanderMeer@gmail.com>
  */
 
+#include "AboutSystem.h"
 
 #include <ctype.h>
 #include <stdio.h>
@@ -67,15 +69,8 @@
 
 #include "Credits.h"
 
-#ifndef LINE_MAX
-#define LINE_MAX 2048
-#endif
-
-#define SCROLL_CREDITS_VIEW 'mviv'
-
 #undef B_TRANSLATION_CONTEXT
 #define B_TRANSLATION_CONTEXT "AboutWindow"
-
 
 
 static const char* UptimeToString(char string[], size_t size);
@@ -98,15 +93,18 @@ static const char* kGPLv2 = B_TRANSLATE_MARK("GNU GPL v2");
 static const char* kGPLv3 = B_TRANSLATE_MARK("GNU GPL v3");
 static const char* kLGPLv2 = B_TRANSLATE_MARK("GNU LGPL v2");
 static const char* kLGPLv21 = B_TRANSLATE_MARK("GNU LGPL v2.1");
+
 #if 0
 static const char* kPublicDomain = B_TRANSLATE_MARK("Public Domain");
 #endif
+
 #ifdef __i386__
 static const char* kIntel2xxxFirmware = B_TRANSLATE_MARK("Intel (2xxx firmware)");
 static const char* kIntelFirmware = B_TRANSLATE_MARK("Intel (firmware)");
 static const char* kMarvellFirmware = B_TRANSLATE_MARK("Marvell (firmware)");
 static const char* kRalinkFirmware = B_TRANSLATE_MARK("Ralink (firmware)");
 #endif
+
 
 
 static int
@@ -136,123 +134,6 @@ TranslationComparator(const void* left, const void* right)
 	BLocale::Default()->GetCollator(&collator);
 	return collator.Compare(leftName.String(), rightName.String());
 }
-
-
-class AboutApp : public BApplication {
-public:
-							AboutApp();
-			void			MessageReceived(BMessage* message);
-};
-
-
-class AboutView;
-
-class AboutWindow : public BWindow {
-public:
-							AboutWindow();
-
-	virtual	bool			QuitRequested();
-
-			AboutView*		fAboutView;
-};
-
-
-class LogoView : public BView {
-public:
-							LogoView();
-	virtual					~LogoView();
-
-	virtual	BSize			MinSize();
-	virtual	BSize			MaxSize();
-
-	virtual void			Draw(BRect updateRect);
-
-private:
-			BBitmap*		fLogo;
-};
-
-
-class CropView : public BView {
-public:
-							CropView(BView* target, int32 left, int32 top,
-								int32 right, int32 bottom);
-	virtual					~CropView();
-
-	virtual	BSize			MinSize();
-	virtual	BSize			MaxSize();
-
-	virtual void			DoLayout();
-
-private:
-			BView*			fTarget;
-			int32			fCropLeft;
-			int32			fCropTop;
-			int32			fCropRight;
-			int32			fCropBottom;
-};
-
-
-class AboutView : public BView {
-public:
-							AboutView();
-							~AboutView();
-
-	virtual void			AttachedToWindow();
-	virtual	void			AllAttached();
-	virtual void			Pulse();
-
-	virtual void			MessageReceived(BMessage* msg);
-	virtual void			MouseDown(BPoint point);
-
-			void			AddCopyrightEntry(const char* name,
-								const char* text,
-								const StringVector& licenses,
-								const StringVector& sources,
-								const char* url);
-			void			AddCopyrightEntry(const char* name,
-								const char* text, const char* url = NULL);
-			void			PickRandomHaiku();
-
-
-			void			_AdjustTextColors();
-private:
-	typedef std::map<std::string, PackageCredit*> PackageCreditMap;
-
-private:
-			BView*			_CreateLabel(const char* name, const char* label);
-			BView*			_CreateCreditsView();
-			status_t		_GetLicensePath(const char* license,
-								BPath& path);
-			void			_AddCopyrightsFromAttribute();
-			void			_AddPackageCredit(const PackageCredit& package);
-			void			_AddPackageCreditEntries();
-
-			BStringView*	fMemView;
-			BStringView*	fUptimeView;
-			BView*			fInfoView;
-			HyperTextView*	fCreditsView;
-
-			BObjectList<BView> fTextViews;
-			BObjectList<BView> fSubTextViews;
-
-			BBitmap*		fLogo;
-
-			bigtime_t		fLastActionTime;
-			BMessageRunner*	fScrollRunner;
-			PackageCreditMap fPackageCredits;
-
-private:
-			rgb_color		fTextColor;
-			rgb_color		fLinkColor;
-			rgb_color		fHaikuOrangeColor;
-			rgb_color		fHaikuGreenColor;
-			rgb_color		fHaikuYellowColor;
-			rgb_color		fBeOSRedColor;
-			rgb_color		fBeOSBlueColor;
-};
-
-
-//	#pragma mark -
 
 
 AboutApp::AboutApp()
@@ -307,9 +188,6 @@ AboutWindow::QuitRequested()
 }
 
 
-//	#pragma mark - LogoView
-
-
 LogoView::LogoView()
 	: BView("logo", B_WILL_DRAW)
 {
@@ -352,9 +230,6 @@ LogoView::Draw(BRect updateRect)
 			BPoint((Bounds().Width() - fLogo->Bounds().Width()) / 2, 0));
 	}
 }
-
-
-//	#pragma mark - CropView
 
 
 CropView::CropView(BView* target, int32 left, int32 top, int32 right,
@@ -421,8 +296,6 @@ CropView::DoLayout()
 }
 
 
-//	#pragma mark - AboutView
-
 #undef B_TRANSLATION_CONTEXT
 #define B_TRANSLATION_CONTEXT "AboutView"
 
@@ -447,7 +320,6 @@ AboutView::AboutView()
 	// Create all the various labels for system infomation
 
 	// OS Version
-
 	char string[1024];
 	strlcpy(string, B_TRANSLATE("Unknown"), sizeof(string));
 
@@ -654,21 +526,6 @@ AboutView::AllAttached()
 
 
 void
-AboutView::MouseDown(BPoint point)
-{
-	BRect r(92, 26, 105, 31);
-	if (r.Contains(point))
-		BMessenger(this).SendMessage('eegg');
-
-	if (Bounds().Contains(point)) {
-		fLastActionTime = system_time();
-		delete fScrollRunner;
-		fScrollRunner = NULL;
-	}
-}
-
-
-void
 AboutView::Pulse()
 {
 	char string[255];
@@ -681,6 +538,21 @@ AboutView::Pulse()
 		&& system_time() > fLastActionTime + 10000000) {
 		BMessage message(SCROLL_CREDITS_VIEW);
 		//fScrollRunner = new BMessageRunner(this, &message, 25000, -1);
+	}
+}
+
+
+void
+AboutView::MouseDown(BPoint point)
+{
+	BRect r(92, 26, 105, 31);
+	if (r.Contains(point))
+		BMessenger(this).SendMessage('eegg');
+
+	if (Bounds().Contains(point)) {
+		fLastActionTime = system_time();
+		delete fScrollRunner;
+		fScrollRunner = NULL;
 	}
 }
 
@@ -1649,4 +1521,3 @@ main()
 	app.Run();
 	return 0;
 }
-
