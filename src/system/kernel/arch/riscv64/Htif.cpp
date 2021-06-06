aa48032efa1acfab2d2b93e25e548deb501db519
X512 <danger_mail@list.ru>
2021-06-06 22:37:10 +0900

    kernel: add RISC-V HTIF code

---
+++ src/system/kernel/arch/riscv64/Htif.cpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2021, Haiku, Inc.
+ * Distributed under the terms of the MIT License.
+ */
+
+
+#include <Htif.h>
+
+
+uint64_t HtifCmd(uint32_t device, uint8_t cmd, uint32_t arg)
+{
+	uint64_t htifTohost = ((uint64_t)device << 56) + ((uint64_t)cmd << 48) + arg;
+	gHtifRegs->toHostLo = htifTohost % ((uint64_t)1 << 32);
+	gHtifRegs->toHostHi = htifTohost / ((uint64_t)1 << 32);
+	return (uint64_t)gHtifRegs->fromHostLo + ((uint64_t)gHtifRegs->fromHostHi << 32);
+}
+
+void HtifShutdown()
+{
+	HtifCmd(0, 0, 1);
+}
+
+void HtifOutChar(char ch)
+{
+	HtifCmd(1, 1, ch);
+}
+
+void HtifOutString(const char *str)
+{
+	for (; *str != '\0'; str++) HtifOutChar(*str);
+}
+
+void HtifOutString(const char *str, size_t len)
+{
+	for (; len > 0; str++, len--) HtifOutChar(*str);
+}
