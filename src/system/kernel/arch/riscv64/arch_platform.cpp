21ea6b7e07beac6223c9e8e693ece073dfb5631f
X512 <danger_mail@list.ru>
2021-06-06 22:57:41 +0900

    kernel/arch/platform: implement for riscv64

--- src/system/kernel/arch/riscv64/arch_platform.cpp
+++ src/system/kernel/arch/riscv64/arch_platform.cpp
@@ -4,11 +4,16 @@
 
 
 #include <arch/platform.h>
+#include <boot/kernel_args.h>
+
+
+void* gFDT = NULL;
 
 
 status_t
 arch_platform_init(struct kernel_args *kernelArgs)
 {
+	gFDT = kernelArgs->arch_args.fdt;
 	return B_OK;
 }
 
