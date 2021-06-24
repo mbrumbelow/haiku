0982e2707659357d15b6c0cf33468590103d8f03
CodeforEvolution <secundaja@gmail.com>
2021-06-24 12:22:06 -0500

    StyledEdit: Fix Window Draw Freeze On Quit & Add Multi-File Save Window
    - Previously, BWindow's UpdateIfNeeded() function was being invoked directly by BApplication's built-in QuitRequested() method, thus failing to work and causing
    StyledEdit to stop redrawing its windows while trying to quit. This is due to UpdateIfNeeded() needing to be called directly from an application's window thread.
    
    - Additionally, I have added a save window (using code from KapiX's Koder source code editor)
    that allows the user to choose which open windows should be saved in a single dialog.
    
    Fixes #16580
    
    Change-Id: Iea1e1811d43155b4453b870f0c6ed9f738394b86

---
+++ src/apps/stylededit/SaveWindow.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2016-2018 Kacper Kasper, <kacperkasper@gmail.com>
+ * Copyright 2021 Haiku, Inc.
+ * All rights reserved. Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ *		Kacper Kasper
+ *		Jacob Secunda
+ */
+#ifndef SAVE_WINDOW_H
+#define SAVE_WINDOW_H
+
+
+#include <StringList.h>
+#include <Window.h>
+
+#include <vector>
+
+
+class BButton;
+class BCheckBox;
+class BScrollView;
+class BStringView;
+
+
+class SaveWindow : public BWindow {
+public:
+								SaveWindow(const BStringList& unsavedFiles);
+								~SaveWindow();
+
+			void				MessageReceived(BMessage* message);
+	virtual	void				Show();
+			std::vector<bool>	Go();
+
+private:
+			void 				_InitInterface();
+
+private:
+	enum Actions {
+		SAVE_ALL		= 'sval',
+		SAVE_SELECTED	= 'svsl',
+		DONT_SAVE		= 'dnsv'
+	};
+
+	const	BStringList			fUnsavedFiles;
+			std::vector<bool>	fAlertValue;
+			sem_id				fAlertSem;
+			BScrollView*		fScrollView;
+			BStringView*		fMessageString;
+			BButton*			fSaveAll;
+			BButton*			fSaveSelected;
+			BButton*			fDontSave;
+			BButton*			fCancel;
+			std::vector<BCheckBox*>	fCheckboxes;
+};
+
+
+#endif // SAVE_WINDOW_H
