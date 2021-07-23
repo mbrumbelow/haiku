2453f5aed9c9ca9ef0ac340604ee5221073d446c
Adrien Destugues <adrien.destugues@opensource.viveris.fr>
2021-07-23 20:07:44 +0200

    XSI semaphores: Do not insert invalid sem_keys in key map.
    Semaphore keys were added to the key map a bit early, before all error
    checking had been done. As a result, we could have keys in the map that
    would not point to a valid semaphore.
    
    Fixes the root issue in #16741. A previous patch in hrev54878 had
    already fixed the panic when trying to access such a key, but it was
    still possible to generate one.
    
    Change-Id: I7449e295fdbe2a48b554cbc0508683d042f6ca48

--- src/system/kernel/posix/xsi_semaphore.cpp
+++ src/system/kernel/posix/xsi_semaphore.cpp
@@ -737,9 +737,9 @@ _user_xsi_semget(key_t key, int numberOfSemaphores, int flags)
 		(int)key, numberOfSemaphores, flags));
 	XsiSemaphoreSet *semaphoreSet = NULL;
 	Ipc *ipcKey = NULL;
-	// Default assumptions
+
+	// Default assumption
 	bool isPrivate = true;
-	bool create = true;
 
 	MutexLocker _(sIpcLock);
 	if (key != IPC_PRIVATE) {
@@ -760,7 +760,6 @@ _user_xsi_semget(key_t key, int numberOfSemaphores, int flags)
 					"for key %d\n",	(int)key));
 				return ENOMEM;
 			}
-			sIpcHashTable.Insert(ipcKey);
 		} else {
 			// The IPC key exist and it already has a semaphore
 			if ((flags & IPC_CREAT) && (flags & IPC_EXCL)) {
@@ -789,47 +788,51 @@ _user_xsi_semget(key_t key, int numberOfSemaphores, int flags)
 					semaphoreSet->ID(), (int)key));
 				return EINVAL;
 			}
-			create = false;
+
+			return semaphoreSet->ID();
 		}
 	}
 
-	if (create) {
-		// Create a new sempahore set for this key
-		if (numberOfSemaphores <= 0
-			|| numberOfSemaphores >= MAX_XSI_SEMS_PER_TEAM) {
-			TRACE_ERROR(("xsi_semget: numberOfSemaphores out of range\n"));
-			return EINVAL;
-		}
-		if (sXsiSemaphoreCount >= MAX_XSI_SEMAPHORE
-			|| sXsiSemaphoreSetCount >= MAX_XSI_SEMAPHORE_SET) {
-			TRACE_ERROR(("xsi_semget: reached limit of maximum number of "
-				"semaphores allowed\n"));
-			return ENOSPC;
-		}
-
-		semaphoreSet = new(std::nothrow) XsiSemaphoreSet(numberOfSemaphores,
-			flags);
-		if (semaphoreSet == NULL || !semaphoreSet->InitOK()) {
-			TRACE_ERROR(("xsi_semget: failed to allocate a new xsi "
-				"semaphore set\n"));
-			delete semaphoreSet;
-			return ENOMEM;
-		}
-		atomic_add(&sXsiSemaphoreCount, numberOfSemaphores);
-		atomic_add(&sXsiSemaphoreSetCount, 1);
-
-		MutexLocker _(sXsiSemaphoreSetLock);
-		semaphoreSet->SetID();
-		if (isPrivate) {
-			semaphoreSet->SetIpcKey((key_t)-1);
-		} else {
-			semaphoreSet->SetIpcKey(key);
-			ipcKey->SetSemaphoreSetID(semaphoreSet);
-		}
-		sSemaphoreHashTable.Insert(semaphoreSet);
-		TRACE(("semget: new set = %d created, sequence = %ld\n",
-			semaphoreSet->ID(), semaphoreSet->SequenceNumber()));
+	// Create a new sempahore set for this key
+	if (numberOfSemaphores <= 0
+		|| numberOfSemaphores >= MAX_XSI_SEMS_PER_TEAM) {
+		TRACE_ERROR(("xsi_semget: numberOfSemaphores out of range\n"));
+		delete ipcKey;
+		return EINVAL;
 	}
+	if (sXsiSemaphoreCount >= MAX_XSI_SEMAPHORE
+		|| sXsiSemaphoreSetCount >= MAX_XSI_SEMAPHORE_SET) {
+		TRACE_ERROR(("xsi_semget: reached limit of maximum number of "
+			"semaphores allowed\n"));
+		delete ipcKey;
+		return ENOSPC;
+	}
+
+	semaphoreSet = new(std::nothrow) XsiSemaphoreSet(numberOfSemaphores,
+		flags);
+	if (semaphoreSet == NULL || !semaphoreSet->InitOK()) {
+		TRACE_ERROR(("xsi_semget: failed to allocate a new xsi "
+			"semaphore set\n"));
+		delete semaphoreSet;
+		delete ipcKey;
+		return ENOMEM;
+	}
+
+	atomic_add(&sXsiSemaphoreCount, numberOfSemaphores);
+	atomic_add(&sXsiSemaphoreSetCount, 1);
+
+	MutexLocker _2(sXsiSemaphoreSetLock);
+	semaphoreSet->SetID();
+	if (isPrivate) {
+		semaphoreSet->SetIpcKey((key_t)-1);
+	} else {
+		sIpcHashTable.Insert(ipcKey);
+		semaphoreSet->SetIpcKey(key);
+		ipcKey->SetSemaphoreSetID(semaphoreSet);
+	}
+	sSemaphoreHashTable.Insert(semaphoreSet);
+	TRACE(("semget: new set = %d created, sequence = %ld\n",
+		semaphoreSet->ID(), semaphoreSet->SequenceNumber()));
 
 	return semaphoreSet->ID();
 }
