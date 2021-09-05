da2c787e1d46ebb791f42e4f289d0798acc64b93
Adrien Destugues <pulkomandy@pulkomandy.tk>
2021-09-05 16:35:28 +0200

    ps2 synaptics: try to implement multitouch support.
    This should allow to do left and right clicks on multitouch clickpads.
    
    The details were extracted from the Linux driver (Linux developers
    reverse engineered the protocol, twice, but did not otherwise document
    it).
    
    Unfortunately my laptop apparently has a non-multitouch clickpad, so
    I can't test this. I hope someone else can confirm if it works.
    
    Change-Id: If334392f4eb2a146955f6c8c897f0ab64d79b8d9

--- src/add-ons/input_server/devices/mouse/movement_maker.cpp
+++ src/add-ons/input_server/devices/mouse/movement_maker.cpp
@@ -237,7 +237,7 @@ TouchpadMovement::EventToMovement(const touchpad_movement* event, mouse_movement
 		}
 	}
 
-	if (event->buttons & kLeftButton) {
+	if (event->wValue != 2 && (event->buttons & kLeftButton) != 0) {
 		fTapClicks = 0;
 		fTapdragStarted = false;
 		fTapStarted = false;
@@ -252,7 +252,9 @@ TouchpadMovement::EventToMovement(const touchpad_movement* event, mouse_movement
 		// The touch pad is in touch with at least one finger
 		if (!_CheckScrollingToMovement(event, movement))
 			_MoveToMovement(event, movement);
-	} else
+	} else if (event->wValue == 2)
+		_MultiTouchMovement(event, movement);
+	else
 		_NoTouchToMovement(event, movement);
 
 
@@ -348,6 +350,25 @@ TouchpadMovement::_UpdateButtons(mouse_movement *movement)
 		movement->clicks = fClickCount;
 
 	fButtonsState = movement->buttons;
+}
+
+
+TouchpadMovement::_MultiTouchMovement(cosnt touchpad_movement *event,
+	mouse_movement *movement)
+{
+	uint32 buttons = event->buttons;
+
+	TRACE("TouchpadMovement: multitouch event (position %" B_PRId32 " width %d)\n",
+		event->xPosition, fAreaWidth);
+
+	if (buttons != 0) {
+		if (event->xPosition < (uint32)fAreaWidth / 2)
+			movement->buttons = kLeftButton;
+		else
+			movement->buttons = kRightButton;
+	}
+
+	UpdateButtons(movement);
 }
 
 
