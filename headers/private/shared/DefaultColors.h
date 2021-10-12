b59eedf14fb122b37cfcabf6cfe5deaf7877691f
Pascal Abresch <nep@packageloss.eu>
2021-10-12 12:31:58 +0200

    Interface Kit: add default dark appearence color constants
    Change-Id: I8added6ada702eb09e95f5f2d6e34b748067c6bc

---
+++ headers/private/shared/DefaultColors.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2022, Pascal Abresch, nep@packageloss.eu.
+ * Distributed under the terms of the MIT License.
+ */
+#include <InterfaceDefs.h>
+
+namespace BPrivate {
+
+rgb_color GetSystemColor(color_which, bool darkVariant);
+
+} // namespace BPrivate
