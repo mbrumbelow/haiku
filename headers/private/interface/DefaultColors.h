5c0fa9eddafcf9a01e9878c241bbe1b2c31ca799
Pascal Abresch <nep@packageloss.eu>
2021-10-12 12:31:58 +0200

    Interface Kit: add default dark appearence color constants
    This is mainly required for webkit.
    A website can explicitly for controls request to draw them
    with either a light or a dark mode. It is not required that
    this matches the system color scheme, that means a website
    can declare specific controls to always render in a light theme
    even if the OS is in a dark mode or vice versa
    
    Change-Id: I8added6ada702eb09e95f5f2d6e34b748067c6bc

--- headers/private/interface/DefaultColors.h
+++ headers/private/interface/DefaultColors.h
@@ -7,12 +7,15 @@
 
 
 #include <GraphicsDefs.h>
+#include <InterfaceDefs.h>
 
 
 namespace BPrivate {
 
 extern const rgb_color* kDefaultColors;
+extern const rgb_color* kDefaultColorsDark;
 
+rgb_color GetSystemColor(color_which, bool darkVariant);
 }	// namespace BPrivate
 
 #endif // _DEFAULT_COLORS_H
---
+++ src/kits/shared/DefaultColors.cpp
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2022, Pascal Abresch, nep@packageloss.eu.
+ * Distributed under the terms of the MIT License.
+ */
+
+#include "DefaultColors.h"
+#include <private/interface/DefaultColors.h>
+#include <private/app/ServerReadOnlyMemory.h>
+
+namespace BPrivate {
+
+rgb_color GetSystemColor(color_which colorConstant, bool darkVariant) {
+	if (darkVariant) {
+		return BPrivate::kDefaultColorsDark[color_which_to_index(colorConstant)]
+	} else {
+		return BPrivate::kDefaultColors[color_which_to_index(colorConstant)]
+	}
+}
+
+} // namespace BPrivate
