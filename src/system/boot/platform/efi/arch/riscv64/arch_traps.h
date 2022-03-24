1cab8152420db0d8b12a45b06cc17db7db9ac9b9
Alexander von Gluck IV <kallisti5@unixzen.com>
2022-03-23 19:44:28 -0500

    boot/efi/riscv64: fix duplicate iframe definition
    Change-Id: I92d9719e13516fba2540631dadfad7e1fc0d0a53

--- src/system/boot/platform/efi/arch/riscv64/arch_traps.h
+++ src/system/boot/platform/efi/arch/riscv64/arch_traps.h
@@ -1,44 +1,13 @@
+/*
+ * Copyright 2021-2022, Haiku, Inc. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ */
 #ifndef _ARCH_TRAPS_H_
 #define _ARCH_TRAPS_H_
 
 
 #include <SupportDefs.h>
-
-
-struct iframe {
-	uint64 ra;
-	uint64 t6;
-	uint64 sp;
-	uint64 gp;
-	uint64 tp;
-	uint64 t0;
-	uint64 t1;
-	uint64 t2;
-	uint64 t5;
-	uint64 s1;
-	uint64 a0;
-	uint64 a1;
-	uint64 a2;
-	uint64 a3;
-	uint64 a4;
-	uint64 a5;
-	uint64 a6;
-	uint64 a7;
-	uint64 s2;
-	uint64 s3;
-	uint64 s4;
-	uint64 s5;
-	uint64 s6;
-	uint64 s7;
-	uint64 s8;
-	uint64 s9;
-	uint64 s10;
-	uint64 s11;
-	uint64 t3;
-	uint64 t4;
-	uint64 fp;
-	uint64 epc;
-};
+#include <arch_thread_types.h>
 
 
 extern "C" {
