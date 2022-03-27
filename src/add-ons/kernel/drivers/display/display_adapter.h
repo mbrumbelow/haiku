c51ffd708c2af02a05d2af592f91320534bc0c49
PulkoMandy <pulkomandy@pulkomandy.tk>
2022-03-27 17:00:48 +0200

    [WIP] acpi display driver
    Supposedly this should allow us to set the display brightness in non
    platfor specific way, and even enable or disable specific displays.
    
    However, on my machine, I did not get the _DOD method to report valid
    device IDs, and while setting the backlight level and reading it back
    seems to work, no change is visible on the display.
    
    This does not implement actual ioctl for now, so you can test in a very
    simple way:
    
    - cat /dev/display/display_adapter/0/* will try to read the backlight
      level for all displays. Results are printed to syslog.
    - echo xxxxxxx > /dev/display/display_adapter/0/DD00 will try to set the
      backlight level on a given display. A longer string in the echo will
      make a brighter display.
    
    Testing welcome.
    
    Change-Id: I797dd7d69df704ee036ccdbe9a40e12b14ff2c4a

--- src/add-ons/kernel/drivers/display/display_adapter.h
+++ src/add-ons/kernel/drivers/display/display_adapter.h
@@ -15,12 +15,12 @@
 
 #include <ACPI.h>
 
-#define DISPLAYADAPTER_MODULE_NAME "drivers/display_adapter/display_adapter/driver_v1"
-#define DISPLAY_DEVICE_MODULE_NAME "drivers/display_adapter/display_adapter/display/device_v1"
-#define DISPLAYADAPTER_DEVICE_MODULE_NAME "drivers/display_adapter/display_adapter/device_v1"
+#define DISPLAYADAPTER_MODULE_NAME "drivers/display/display_adapter/driver_v1"
+#define DISPLAY_DEVICE_MODULE_NAME "drivers/display/display_adapter/display/device_v1"
+#define DISPLAYADAPTER_DEVICE_MODULE_NAME "drivers/display/display_adapter/device_v1"
 
-#define DISPLAYADAPTER_BASENAME "display_adapter/%d"
-#define DISPLAYADAPTER_PATHID_GENERATOR "display_adapter/path_id"
+#define DISPLAYADAPTER_BASENAME "display/display_adapter/%d"
+#define DISPLAYADAPTER_PATHID_GENERATOR "display/display_adapter/path_id"
 
 
 #define OS_DISPLAY_SWITCH 0
