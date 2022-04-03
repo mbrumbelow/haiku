76903c21e2f31e4300148b15ace8b644d6d820d1
PulkoMandy <pulkomandy@pulkomandy.tk>
2022-04-03 14:02:02 +0200

    PCI: Publish bus under its ACPI parent on x86_64
    This results in the PCI root device being published at the right place in
    the device manager tree (under the corresponding ACPI node) instead of
    at the root. This will allow to access the ACPI node properties.
    
    TODO: test this on machines that predate PCI express, where the PCI bus
    may not be discovered through ACPI. Possibly we need to handle both
    cases for x86_64 (attach to ACPI if found there, attach to root if found
    using the legacy mechanisms). It's unclear if ACPI entries will be
    present for classic PCI or not. It's also unclear what happens if
    booting with ACPI disabled.
    
    Change-Id: Ib44939359724779945e46446c0b24a8bd7de6be3

--- src/add-ons/kernel/bus_managers/pci/pci_root.cpp
+++ src/add-ons/kernel/bus_managers/pci/pci_root.cpp
@@ -6,6 +6,7 @@
  */
 
 
+#include <ACPI.h>
 #include <device_manager.h>
 #include <PCI.h>
 #include <drivers/bus/FDT.h>
@@ -32,34 +33,31 @@ pci_root_supports_device(device_node* parent)
 
 #if defined(__riscv)
 	const char* compatible;
-	if (gDeviceManager->get_attr_string(parent, "fdt/compatible", &compatible,
-		false) < B_OK)
+	if (gDeviceManager->get_attr_string(parent, "fdt/compatible", &compatible, false) < B_OK)
 		return -1.0f;
 
-	if (strcmp(bus, "fdt") != 0)
-		return 0.0f;
-
-	if (strcmp(compatible, "pci-host-ecam-generic") != 0
-		&& strcmp(compatible, "sifive,fu740-pcie") != 0) {
+		if (strcmp(compatible, "pci-host-ecam-generic") == 0
+			|| strcmp(compatible, "sifive,fu740-pcie") == 0) {
+			return 1.0f;
+		}
 		return 0.0f;
 	}
-#elif defined(__aarch64__)
+#elif defined(__aarch64__) || defined(__x86_64__)
 	if (strcmp(bus, "acpi") != 0)
 		return -1.0f;
 
 	const char* hid;
-	if (gDeviceManager->get_attr_string(parent, ACPI_DEVICE_HID_ITEM, &hid,
-		false) < B_OK)
+	if (gDeviceManager->get_attr_string(parent, ACPI_DEVICE_HID_ITEM, &hid, false) < B_OK)
 		return -1.0f;
 
 	if (strcmp(hid, "PNP0A03") != 0 && strcmp(hid, "PNP0A08") != 0)
 		return -1.0f;
 #else
-	if (strcmp(bus, "root") != 0)
-		return 0.0f;
+	if (strcmp(bus, "root") == 0)
+		return 1.0f;
 #endif
 
-	return 1.0;
+	return 0.0f;
 }
 
 
