3051b0a0bd1ad44b95741d42e4c6f5eb682b24a5
Jim906 <jim_l@fastmail.com>
2022-04-12 15:55:29 -0400

    MediaPlayer: add .pls playlist support
    * Add PlaylistFileReader class and derived M3uReader and PlsReader classes.
    * Move most of the code from Playlist::AppendM3uToPlaylist to PlaylistFileReader::_AppendItemToPlaylist
    * For each File line in the .pls file, a PlaylistItem is added to the MediaPlayer playlist.
    * For each Title/Length line, the data is applied to the most recently added PlaylistItem.
    * The NumberOfEntries and Version lines are read to make them available for future use, but currently they have no effect.
    * Fixes #6813
    
    Change-Id: Ifa23d0df2e4d5b466aa7b85649a78276cff986ef

---
+++ src/apps/mediaplayer/playlist/PlaylistFileReader.h
@@ -0,0 +1,52 @@
+/*
+ * PlaylistFileReader.h - Media Player for the Haiku Operating System
+ *
+ * Copyright (C) 2006 Marcus Overhagen 	<marcus@overhagen.de>
+ * Copyright (C) 2007-2009 Stephan Aßmus <superstippi@gmx.de> (MIT ok)
+ * Copyright (C) 2008-2009 Fredrik Modéen <[FirstName]@[LastName].se> (MIT ok)
+ *
+ * Released under the terms of the MIT license.
+ */
+#ifndef __PLAYLIST_READER_H
+#define __PLAYLIST_READER_H
+
+#include <SupportDefs.h>
+
+class BString;
+struct entry_ref;
+class Playlist;
+
+enum class PlaylistFileType {m3u, pls, unknown};
+
+class PlaylistFileReader {
+public:
+	virtual	void				AppendToPlaylist(const entry_ref& ref,
+									Playlist* playlist) = 0;
+	static	PlaylistFileType	IdentifyType(const entry_ref& ref);
+
+protected:
+			int32				_AppendItemToPlaylist(const BString& entry, Playlist* playlist);
+			// Returns the track's playlist index if it was successfully added, else returns -1.
+			// BString& entry is a (absolute or relative) file path or URL
+};
+
+
+class M3uReader : public PlaylistFileReader {
+public:
+	virtual	void				AppendToPlaylist(const entry_ref& ref, Playlist* playlist);
+};
+
+
+class PlsReader : public PlaylistFileReader {
+public:
+	virtual void				AppendToPlaylist(const entry_ref& ref,
+									Playlist* playlist);
+private:
+			status_t			_ParseTitleLine(const BString& title, Playlist* playlist,
+									const int32 lastAssignedIndex);
+			status_t			_ParseLengthLine(const BString& length, Playlist* playlist,
+									const int32 lastAssignedIndex);
+};
+
+
+#endif
