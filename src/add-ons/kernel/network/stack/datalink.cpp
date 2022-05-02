ba774906168db525ce0ffebe104febd1b2d2d2a3
Alexander von Gluck IV <kallisti5@unixzen.com>
2022-05-02 18:43:27 -0500

    net/stack: Allow maximum MTU for Jumbo Frames; allow updates to fail
    * Working towards fixing #17728
    * The previous behaviour was a bit odd since it only allowed
      the MTU to shrink from the default.
    * Now we allow to set the MTU to the maximum sane value, while
      also allowing the interface to "fail" the adjustment by
      allowing the notify_interface_changed to fail
    
    Change-Id: I73c442ceffc7d3bedbf726c48e8866d8a9b189d2

--- src/add-ons/kernel/network/stack/datalink.cpp
+++ src/add-ons/kernel/network/stack/datalink.cpp
@@ -888,13 +888,16 @@ interface_protocol_control(net_datalink_protocol* _protocol, int32 option,
 				return B_BAD_ADDRESS;
 
 			// check for valid bounds
-			if (request.ifr_mtu < 100
-				|| (uint32)request.ifr_mtu > interface->device->mtu)
+			if (request.ifr_mtu < 500 || request.ifr_mtu > 16110)
 				return B_BAD_VALUE;
 
+			// notify. If the notification fails, revert
+			int old_mtu = interface->mtu;
 			interface->mtu = request.ifr_mtu;
-			notify_interface_changed(interface);
-			return B_OK;
+			status_t result = notify_interface_changed(interface);
+			if (result != B_OK)
+				interface->mtu = old_mtu;
+			return result;
 		}
 
 		case SIOCSIFMEDIA:
@@ -957,8 +960,7 @@ interface_protocol_control(net_datalink_protocol* _protocol, int32 option,
 				return B_BAD_ADDRESS;
 
 			interface->metric = request.ifr_metric;
-			notify_interface_changed(interface);
-			return B_OK;
+			return notify_interface_changed(interface);
 		}
 
 		case SIOCADDRT:
