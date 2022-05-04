a64f2c341280aa473b062ac8eda590b5ba78e177
X512 <danger_mail@list.ru>
2022-05-04 21:29:42 +0900

    shared: move ScopeExit from riscv64 arch kernel code
    Change-Id: Ibff7acec00337a9f56f9b8e29ea262c8d64c2446

---
+++ headers/private/shared/ScopeExit.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2022, Haiku, Inc. All Rights Reserved.
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef _SCOPE_EXIT_H
+#define _SCOPE_EXIT_H
+
+#include <utility>
+
+
+template<typename F>
+class ScopeExit
+{
+public:
+	explicit ScopeExit(F&& fn) : fFn(fn)
+	{
+	}
+
+	~ScopeExit()
+	{
+		fFn();
+	}
+
+	ScopeExit(ScopeExit&& other) : fFn(std::move(other.fFn))
+	{
+	}
+
+private:
+	ScopeExit(const ScopeExit&);
+	ScopeExit& operator=(const ScopeExit&);
+
+private:
+	F fFn;
+};
+
+
+#endif // _SCOPE_EXIT_H
