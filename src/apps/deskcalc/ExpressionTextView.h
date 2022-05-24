f40c5c53037baa3b3029afabcb29e0160611f753
Dale Cieslak <dcieslak@yahoo.com>
2022-05-23 22:21:24 -0700

    DeskCalc: Fix Locale-aware display
    Expression parser was made aware of Locale-specific separators in
    hrev56067. However, the Expression view was still using only '.' as
    separator. This change passes the Locale-specific decimal separator
    to the ExpressionTextView class and uses that to parse the value for
    display.
    
    Fixes #17754

--- src/apps/deskcalc/ExpressionTextView.h
+++ src/apps/deskcalc/ExpressionTextView.h
@@ -43,7 +43,8 @@ class ExpressionTextView : public InputTextView {
 			void				AddKeypadLabel(const char* label);
 
 			void				SetExpression(const char* expression);
-			void				SetValue(BString value);
+			void				SetValue(BString value,
+									BString decSeparator = ".");
 
 			void				BackSpace();
 			void				Clear();
