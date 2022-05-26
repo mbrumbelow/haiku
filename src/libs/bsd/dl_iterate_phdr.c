e47b066e2ff185cfd467b0d212e2ffec1c52b311
Trung Nguyen <trungnt282910@gmail.com>
2022-05-26 20:48:56 +0700

    libbsd: implemented dl_iterate_phdr
    dl_iterate_phdr now fills the first four fields of
    struct dl_phdr_info. The last four fields remain
    unimplemented, as it requires help from the
    runtime_loader.
    
    Change-Id: Id96a7c7ac05633a71b9fb62c98b3a40f7d4f255b

---
+++ src/libs/bsd/dl_iterate_phdr.c
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2022, Haiku, Inc. All Rights Reserved.
+ * Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ *		Trung Nguyen, trungnt282910@gmail.com
+ */
+
+#include <kernel/image.h>
+
+#define _DEFAULT_SOURCE
+#include <link.h>
+#include <stddef.h>
+
+#if B_HAIKU_32_BIT
+	#define Elf_Ehdr Elf32_Ehdr
+#elif B_HAIKU_64_BIT
+	#define Elf_Ehdr Elf64_Ehdr
+#endif
+
+int
+dl_iterate_phdr(int (*callback)(struct dl_phdr_info* info, size_t size, void* data), void* data)
+{
+	image_info info;
+	int32 cookie = 0;
+
+	struct dl_phdr_info phdr_info;
+
+	while (get_next_image_info(0, &cookie, &info) == B_OK) {
+		const Elf_Ehdr* header = (const Elf_Ehdr*)info.text;
+
+		if (!IS_ELF(*header))
+			continue;
+
+		phdr_info.dlpi_addr = (Elf_Addr)info.text;
+		phdr_info.dlpi_name = info.name;
+		phdr_info.dlpi_phnum = header->e_phnum;
+		phdr_info.dlpi_phdr = (const Elf_Phdr*)((const char*)info.text + header->e_phoff);
+
+		int status = callback(&phdr_info, sizeof(phdr_info), data);
+
+		if (status != 0)
+			return status;
+	}
+
+	return 0;
+}
