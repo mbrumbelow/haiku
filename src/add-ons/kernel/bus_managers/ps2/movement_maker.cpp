1825311278fdeb375d124bfc5c5bf1db96914850
Fredrik Holmqvist <fredrik.holmqvist@gmail.com>
2022-06-21 21:29:01 +0200

    movement_maker: use real math
    Not sure if linking is correct.
    It compiles and boots but untested as I use USB input

--- src/add-ons/kernel/bus_managers/ps2/movement_maker.cpp
+++ src/add-ons/kernel/bus_managers/ps2/movement_maker.cpp
@@ -1,5 +1,6 @@
 #include "movement_maker.h"
 
+#include <math.h>
 #include <stdlib.h>
 
 #include <KernelExport.h>
@@ -13,162 +14,6 @@
 #endif
 
 
-typedef union {
-  float value;
-  /* FIXME: Assumes 32 bit int.  */
-  unsigned int word;
-} ieee_float_shape_type;
-
-/* Get a 32 bit int from a float.  */
-
-#define GET_FLOAT_WORD(i,d)					\
-do {								\
-  ieee_float_shape_type gf_u;					\
-  gf_u.value = (d);						\
-  (i) = gf_u.word;						\
-} while (0)
-
-/* Set a float from a 32 bit int.  */
-
-#define SET_FLOAT_WORD(d,i)					\
-do {								\
-  ieee_float_shape_type sf_u;					\
-  sf_u.word = (i);						\
-  (d) = sf_u.value;						\
-} while (0)
-
-static const float huge = 1.0e30;
-
-float
-floorf(float x)
-{
-	int32 i0,j0;
-	uint32 i;
-	GET_FLOAT_WORD(i0,x);
-	j0 = ((i0>>23)&0xff)-0x7f;
-	if (j0<23) {
-	    if (j0<0) { 	/* raise inexact if x != 0 */
-		if (huge+x>(float)0.0) {/* return 0*sign(x) if |x|<1 */
-		    if (i0>=0) {i0=0;}
-		    else if ((i0&0x7fffffff)!=0)
-			{ i0=0xbf800000;}
-		}
-	    } else {
-		i = (0x007fffff)>>j0;
-		if ((i0&i)==0) return x; /* x is integral */
-		if (huge+x>(float)0.0) {	/* raise inexact flag */
-		    if (i0<0) i0 += (0x00800000)>>j0;
-		    i0 &= (~i);
-		}
-	    }
-	} else {
-	    if (j0==0x80) return x+x;	/* inf or NaN */
-	    else return x;		/* x is integral */
-	}
-	SET_FLOAT_WORD(x,i0);
-	return x;
-}
-
-
-float
-ceilf(float x)
-{
-	int32 i0,j0;
-	uint32 i;
-
-	GET_FLOAT_WORD(i0,x);
-	j0 = ((i0>>23)&0xff)-0x7f;
-	if (j0<23) {
-	    if (j0<0) { 	/* raise inexact if x != 0 */
-		if (huge+x>(float)0.0) {/* return 0*sign(x) if |x|<1 */
-		    if (i0<0) {i0=0x80000000;}
-		    else if (i0!=0) { i0=0x3f800000;}
-		}
-	    } else {
-		i = (0x007fffff)>>j0;
-		if ((i0&i)==0) return x; /* x is integral */
-		if (huge+x>(float)0.0) {	/* raise inexact flag */
-		    if (i0>0) i0 += (0x00800000)>>j0;
-		    i0 &= (~i);
-		}
-	    }
-	} else {
-	    if (j0==0x80) return x+x;	/* inf or NaN */
-	    else return x;		/* x is integral */
-	}
-	SET_FLOAT_WORD(x,i0);
-	return x;
-}
-
-static  const float        one        = 1.0, tiny=1.0e-30;
-
-float
-sqrtf(float x)
-{
-        float z;
-        int32 sign = (int)0x80000000;
-        int32 ix,s,q,m,t,i;
-        uint32 r;
-
-        GET_FLOAT_WORD(ix,x);
-
-    /* take care of Inf and NaN */
-        if ((ix&0x7f800000)==0x7f800000) {
-            return x*x+x;                /* sqrt(NaN)=NaN, sqrt(+inf)=+inf
-                                           sqrt(-inf)=sNaN */
-        }
-    /* take care of zero */
-        if (ix<=0) {
-            if ((ix&(~sign))==0) return x;/* sqrt(+-0) = +-0 */
-            else if (ix<0)
-                return (x-x)/(x-x);                /* sqrt(-ve) = sNaN */
-        }
-    /* normalize x */
-        m = (ix>>23);
-        if (m==0) {                                /* subnormal x */
-            for(i=0;(ix&0x00800000)==0;i++) ix<<=1;
-            m -= i-1;
-        }
-        m -= 127;        /* unbias exponent */
-        ix = (ix&0x007fffff)|0x00800000;
-        if (m&1)        /* odd m, double x to make it even */
-            ix += ix;
-        m >>= 1;        /* m = [m/2] */
-
-    /* generate sqrt(x) bit by bit */
-        ix += ix;
-        q = s = 0;                /* q = sqrt(x) */
-        r = 0x01000000;                /* r = moving bit from right to left */
-
-        while(r!=0) {
-            t = s+r;
-            if (t<=ix) {
-                s    = t+r;
-                ix  -= t;
-                q   += r;
-            }
-            ix += ix;
-            r>>=1;
-        }
-
-    /* use floating add to find out rounding direction */
-        if (ix!=0) {
-            z = one-tiny; /* trigger inexact flag */
-            if (z>=one) {
-                z = one+tiny;
-                if (z>one)
-                    q += 2;
-                else
-                    q += (q&1);
-            }
-        }
-        ix = (q>>1)+0x3f000000;
-        ix += (m <<23);
-        SET_FLOAT_WORD(z,ix);
-        return z;
-}
-
-
 static int32
 make_small(float value)
 {
@@ -177,7 +22,6 @@ make_small(float value)
 	else
 		return (int32)ceilf(value);
 }
-
 
 
 void
