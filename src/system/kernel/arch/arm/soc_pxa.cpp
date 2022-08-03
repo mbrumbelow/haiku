d1047ff691a7aa760305db2b1a466ac0d5636750
David Karoly <karolyd577@gmail.com>
2022-08-03 21:19:58 +0200

    arm: detect SoC timer from FDT for OMAP3 and PXA
    Change-Id: Ib03a11f016cb937d748d2696a17158e17c86e317

--- src/system/kernel/arch/arm/soc_pxa.cpp
+++ src/system/kernel/arch/arm/soc_pxa.cpp
@@ -143,11 +143,11 @@ PXATimer::HandleInterrupt()
 }
 
 
-#if 0
-PXATimer::PXATimer(fdt_module_info *fdt, fdt_device_node node)
-	: HardwareTimer(fdt, node)
+PXATimer::PXATimer(uint32_t reg_base)
 {
-	fRegArea = fFDT->map_reg_range(node, 0, (void**)&fRegBase);
+	fRegArea = vm_map_physical_memory(B_SYSTEM_TEAM, "pxa-timer", (void**)&fRegBase,
+		B_ANY_KERNEL_ADDRESS, B_PAGE_SIZE, B_KERNEL_READ_AREA | B_KERNEL_WRITE_AREA,
+		reg_base, false);
 	if (fRegArea < 0)
 		panic("Cannot map PXATimer registers!");
 
@@ -158,4 +158,3 @@ PXATimer::PXATimer(fdt_module_info *fdt, fdt_device_node node)
 
 	install_io_interrupt_handler(PXA_TIMERS_INTERRUPT, &PXATimer::_InterruptWrapper, NULL, 0);
 }
-#endif
