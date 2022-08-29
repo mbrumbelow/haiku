08e67867563c0c68029cc549bd072e8d6043ad41
Mashijams <raghavself28@gmail.com>
2022-08-29 19:58:44 +0530

    xfs: leaf xattrs
    - Implemented leaf based extended attributes on-disk headers
    
    - Both version 4 and version 5 attributes can be read now
    
    - Implemented all leaf headers check to ensure metadata integrity
    
    Change-Id: Ifc3f2e433e991776bb02d1d38f621d5bd1554a4e

--- src/add-ons/kernel/file_systems/xfs/Attribute.cpp
+++ src/add-ons/kernel/file_systems/xfs/Attribute.cpp
@@ -12,8 +12,9 @@
 Attribute::Attribute(Inode* inode)
 	:
 	fInode(inode),
+	fName(NULL),
 	fShortAttr(NULL),
-	fName(NULL)
+	fLeafAttr(NULL)
 {
 }
 
@@ -21,7 +22,9 @@ Attribute::Attribute(Inode* inode)
 Attribute::Attribute(Inode* inode, attr_cookie* cookie)
 	:
 	fInode(inode),
-	fName(cookie->name)
+	fName(cookie->name),
+	fShortAttr(NULL),
+	fLeafAttr(NULL)
 {
 }
 
@@ -29,15 +32,15 @@ Attribute::Attribute(Inode* inode, attr_cookie* cookie)
 Attribute::~Attribute()
 {
 	delete fShortAttr;
+	delete fLeafAttr;
 }
 
 
 status_t
 Attribute::Init()
 {
-	if (fInode->AFormat() == XFS_DINODE_FMT_LOCAL)
-	{
-		TRACE("Attribute:Init: LOCAL\n");
+	if (fInode->AFormat() == XFS_DINODE_FMT_LOCAL) {
+		TRACE("Attribute::Init: LOCAL\n");
 		if(fName == NULL)
 			fShortAttr = new(std::nothrow) ShortAttribute(fInode);
 		else
@@ -45,6 +48,26 @@ Attribute::Init()
 		if (fShortAttr == NULL)
 			return B_NO_MEMORY;
 		return B_OK;
+	}
+	if (fInode->AFormat() == XFS_DINODE_FMT_EXTENTS) {
+		TRACE("Attribute::Init: EXTENTS\n");
+		if (fName == NULL)
+			fLeafAttr = new(std::nothrow) LeafAttribute(fInode);
+		else
+			fLeafAttr = new(std::nothrow) LeafAttribute(fInode, fName);
+		if (fLeafAttr == NULL)
+			return B_NO_MEMORY;
+
+		status_t status = fLeafAttr->Init();
+
+		if (status == B_OK)
+			return status;
+
+		delete fLeafAttr;
+		fLeafAttr = NULL;
+
+		// Currently node attributes are not supported return B_BAD_VALUE
+		return B_BAD_VALUE;
 	}
 
 	return B_BAD_VALUE;
@@ -91,8 +114,10 @@ Attribute::Open(const char* name, int openMode, attr_cookie** _cookie)
 status_t
 Attribute::Stat(struct stat& stat)
 {
-	if(fShortAttr != NULL)
+	if (fShortAttr != NULL)
 		return fShortAttr->Stat(stat);
+	else if (fLeafAttr != NULL)
+		return fLeafAttr->Stat(stat);
 	else
 		return B_BAD_VALUE;
 }
@@ -101,8 +126,10 @@ Attribute::Stat(struct stat& stat)
 status_t
 Attribute::Read(attr_cookie* cookie, off_t pos, uint8* buffer, size_t* length)
 {
-	if(fShortAttr != NULL)
+	if (fShortAttr != NULL)
 		return fShortAttr->Read(cookie, pos, buffer, length);
+	else if (fLeafAttr != NULL)
+		return fLeafAttr->Read(cookie, pos, buffer, length);
 	else
 		return B_BAD_VALUE;
 }
@@ -111,8 +138,10 @@ Attribute::Read(attr_cookie* cookie, off_t pos, uint8* buffer, size_t* length)
 status_t
 Attribute::GetNext(char* name, size_t* nameLength)
 {
-	if(fShortAttr != NULL)
+	if (fShortAttr != NULL)
 		return fShortAttr->GetNext(name, nameLength);
+	else if (fLeafAttr != NULL)
+		return fLeafAttr->GetNext(name, nameLength);
 	else
 		return B_BAD_VALUE;
 }
@@ -121,8 +150,10 @@ Attribute::GetNext(char* name, size_t* nameLength)
 status_t
 Attribute::Lookup(const char* name, size_t* nameLength)
 {
-	if(fShortAttr != NULL)
+	if (fShortAttr != NULL)
 		return fShortAttr->Lookup(name, nameLength);
+	else if (fLeafAttr != NULL)
+		return fLeafAttr->Lookup(name, nameLength);
 	else
 		return B_BAD_VALUE;
 }
