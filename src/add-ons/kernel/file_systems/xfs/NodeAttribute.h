2e4ce7a62c640bf406114c5755d5bf0eab0d0faf
Mashijams <raghavself28@gmail.com>
2022-09-02 01:26:11 +0530

    xfs: Node xattrs
    - Implemented node based extended attributes support
    
    - Both Versions of xfs node based extended attributes can be read now
    
    Change-Id: I2d2915029435e7722cf463b1e2b5579b361dd592

---
+++ src/add-ons/kernel/file_systems/xfs/NodeAttribute.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2022, Raghav Sharma, raghavself28@gmail.com
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef NODE_ATTRIBUTE_H
+#define NODE_ATTRIBUTE_H
+
+
+#include "LeafAttribute.h"
+#include "Node.h"
+
+
+class NodeAttribute {
+public:
+								NodeAttribute(Inode* inode);
+								NodeAttribute(Inode* inode, const char* name);
+								~NodeAttribute();
+
+			xfs_fsblock_t		LogicalToFileSystemBlock(uint32 LogicalBlock);
+
+			status_t			FillMapEntry(xfs_extnum_t num);
+
+			status_t			FillBuffer(char* buffer, xfs_fsblock_t block);
+
+			status_t			Init();
+
+			status_t			Stat(struct stat& stat);
+
+			status_t			Read(attr_cookie* cookie, off_t pos,
+									uint8* buffer, size_t* length);
+
+			status_t			GetNext(char* name, size_t* nameLength);
+
+			status_t			Lookup(const char* name, size_t* nameLength);
+private:
+			Inode*				fInode;
+			const char*			fName;
+			ExtentMapEntry*		fMap;
+			char*				fLeafBuffer;
+			char*				fNodeBuffer;
+			uint16				fLastEntryOffset;
+			AttrLeafNameLocal*	fLocalEntry;
+			AttrLeafNameRemote*	fRemoteEntry;
+};
+
+#endif
