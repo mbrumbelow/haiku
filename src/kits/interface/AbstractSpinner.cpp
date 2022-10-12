c956d0f4917dbed97e70906b90623fa7b634368c
MaK <manishkl543@gmail.com>
2022-10-12 07:07:17 +0530

    BSpinner: increment/decrement twice on plus minus button click.

--- src/kits/interface/AbstractSpinner.cpp
+++ src/kits/interface/AbstractSpinner.cpp
@@ -446,9 +446,9 @@ SpinnerButton::MouseDown(BPoint where)
 {
 	if (fIsEnabled) {
 		fIsMouseDown = true;
-		fSpinnerDirection == SPINNER_INCREMENT
+		/*fSpinnerDirection == SPINNER_INCREMENT
 			? fParent->Increment()
-			: fParent->Decrement();
+			: fParent->Decrement();*/
 		Invalidate();
 		BMessage repeatMessage('rept');
 		SetMouseEventMask(B_POINTER_EVENTS, B_NO_POINTER_HISTORY);
