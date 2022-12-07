177081ecd0e41205d54723b22bc25d6c9f42cd0f
X512 <danger_mail@list.ru>
2022-12-08 06:05:07 +0900

    plic: move to separate add-on
    Change-Id: Iba0c94ada48b3ce259288489aa7dd8a8c50876af

--- headers/private/kernel/arch/riscv64/Plic.h
+++ headers/private/kernel/arch/riscv64/Plic.h
@@ -12,7 +12,6 @@
 // platform-level interrupt controller
 struct PlicRegs
 {
-	// context = hart * 2 + mode, mode: 0 - machine, 1 - supervisor
 	uint32 priority[1024];           // 0x000000
 	uint32 pending[1024 / 32];       // 0x001000, bitfield
 	uint8  unused1[0x2000 - 0x1080];
@@ -24,8 +23,6 @@ struct PlicRegs
 		uint8  unused[0xff8];
 	} contexts[15872];               // 0x200000
 };
-
-extern PlicRegs* volatile gPlicRegs;
 
 
 #endif	// _PLIC_H_
