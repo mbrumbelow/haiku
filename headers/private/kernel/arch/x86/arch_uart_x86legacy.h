fcbdb8337160f8bf963b237e171d9113f6e48b14
Alexander von Gluck IV <kallisti5@unixzen.com>
2023-01-04 11:18:28 -0600

    efi/serial: Refactor EFI bios serial services
    * Convert x86 direct port I/O to a gUART.
    * sSerial is plainly EFI Serial or EFI Stdout
    * gUART is plainly hardware serial now
    
    Change-Id: I5730fbc0ce3427851e4e2d1d0aa8c55f7765e444

---
+++ headers/private/kernel/arch/x86/arch_uart_x86legacy.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2011-2021 Haiku, Inc. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ *		Alexander von Gluck, kallisti5@unixzen.com
+ */
+#ifndef __DEV_UART_X86_LEGACY_H
+#define __DEV_UART_X86_LEGACY_H
+
+
+#include <sys/types.h>
+
+#include <SupportDefs.h>
+
+#include <arch/generic/debug_uart.h>
+
+
+#define UART_KIND_X86LEGACY "x86 Legacy"
+
+
+class ArchUARTx86Legacy : public DebugUART {
+public:
+							ArchUARTx86Legacy(addr_t base, int64 clock);
+							~ArchUARTx86Legacy();
+
+			void			InitEarly();
+			void			InitPort(uint32 baud);
+
+			void			Enable();
+			void			Disable();
+
+			int				PutChar(char c);
+			int				GetChar(bool wait);
+
+			void			FlushTx();
+			void			FlushRx();
+};
+
+
+ArchUARTx86Legacy *arch_get_uart_x86legacy(addr_t base, int64 clock);
+
+
+#endif
