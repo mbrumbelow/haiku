cc27ad8e6cd289e921dd01a7eee037b2a580e261
vaibhavg20comp <vaibhavg20.comp@coep.ac.in>
2023-02-07 09:55:12 +0530

    Updated termios.c and termios.h
    termios.h: Added cfsetspeed function signature.
    
    Available only if _BSD_SOURCE is defined.
    Fixes #18220.
    
    Change-Id: Ic1592317ec8b934a75dad01b4e838c3315387783
    
    termios.c: Added cfsetspeed function.
    
    * It sets both input and output baud rate in the termios
      structure referenced by termios to speed.
    * Available only if _BSD_SOURCE is defined.
    
    Fixes #18220.
    
    Change-Id: I446c955dbc4332aa1be864746ca843a8668a9378
    
    termios.c Made necessary formatting Changes
    
    Fixes #18220.
    
    Change-Id: I8c4a06b4be4aa55b8ce35cb7f62552fc47a8e8d0

--- src/system/libroot/posix/termios.c
+++ src/system/libroot/posix/termios.c
@@ -157,6 +157,23 @@ cfsetospeed(struct termios *termios, speed_t speed)
 }
 
 
+int
+cfsetspeed(struct termios *termios,speed_t speed)
+{ 
+	/* Check for custom speed values (see above) */
+	if (speed > B31250) {
+		termios->c_cflag |= CBAUD;
+		termios->c_ispeed = speed;
+		termios->c_ospeed = speed;
+		return 0;
+	}
+
+	termios->c_cflag &= ~CBAUD;
+	termios->c_cflag |= speed;
+	return 0;
+} 
+
+
 void
 cfmakeraw(struct termios *termios)
 {
