7062596d3248673662e4445c7e98943b0a4bf328
Panagiotis "Ivory" Vasilopoulos <git@n0toose.net>
2023-02-25 13:42:01 +0100

    Installer: Add partition setup instructions
    Even if it may seem obvious to experienced users that they have to
    click on the 'Set up partitions...' button, this is unfortunately not
    always the case. Despite the pop-up that appears if there are no suitable
    partitions to install Haiku on, some contradicting instructions remain on
    the screen after closing it.
    
    The Installer should also provide some feedback to the user as to whether
    they used DriveSetup correctly, instead of inferring it from the fact that
    they cannot select any drives after having used DriveSetup.
    
    For this purpose, the Installer window was made a bit wider.
    
    Change-Id: If561f628e2768a01d85176359f07a187e002f51b

--- src/apps/installer/InstallerWindow.cpp
+++ src/apps/installer/InstallerWindow.cpp
@@ -157,7 +157,7 @@ layout_item_for(BView* view)
 
 InstallerWindow::InstallerWindow()
 	:
-	BWindow(BRect(-2000, -2000, -1800, -1800),
+	BWindow(BRect(-2100, -2000, -1700, -1800),
 		B_TRANSLATE_SYSTEM_NAME("Installer"), B_TITLED_WINDOW,
 		B_NOT_ZOOMABLE | B_AUTO_UPDATE_SIZE_LIMITS),
 	fEncouragedToSetupPartitions(false),
@@ -806,8 +806,18 @@ InstallerWindow::_UpdateControls()
 		statusText.SetToFormat(B_TRANSLATE("Press the Begin button to install "
 			"from '%1s' onto '%2s'."), srcItem->Name(), dstItem->Name());
 	} else if (srcItem != NULL) {
-		statusText = B_TRANSLATE("Choose the disk you want to install "
-			"onto from the pop-up menu. Then click \"Begin\".");
+		if (!foundOneSuitableTarget) {
+			statusText = B_TRANSLATE(
+				"Haiku has to be installed on a partition that uses "
+				"the Be File System, but there are currently no such "
+				"partitions on your system.\n\n"
+				"Click on 'Set up partitions" B_UTF8_ELLIPSIS
+				"' to create one.");
+		} else {
+			statusText = B_TRANSLATE(
+				"Choose the disk you want to install "
+				"onto from the pop-up menu. Then click \"Begin\".");
+		}
 	} else if (dstItem != NULL) {
 		statusText = B_TRANSLATE("Choose the source disk from the "
 			"pop-up menu. Then click \"Begin\".");
@@ -830,6 +840,7 @@ InstallerWindow::_UpdateControls()
 		label = buffer;
 	} else
 		label = B_TRANSLATE("Write boot sector");
+
 	fMakeBootableItem->SetEnabled(dstItem != NULL);
 	fMakeBootableItem->SetLabel(label.String());
 // TODO: Once bootman support writing to specific disks, enable this, since
