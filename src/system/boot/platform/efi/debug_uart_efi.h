3d79e54b7badbdae639a1f2296bea63b6b6fd507
Alexander von Gluck IV <kallisti5@unixzen.com>
2023-03-12 12:43:01 -0500

    kernel/uart: Add EFI SerialIO based DebugUART
    Change-Id: Ic00b1783b175a15c0819ce6f536dc4b991cf0181

---
+++ src/system/boot/platform/efi/debug_uart_efi.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2012-2023 Haiku, Inc. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ *		Alexander von Gluck IV, kallisti5@unixzen.com
+ */
+#ifndef _KERNEL_ARCH_DEBUG_UART_EFI_H
+#define _KERNEL_ARCH_DEBUG_UART_EFI_H
+
+
+#include <arch/generic/debug_uart.h>
+#include <efi/protocol/serial-io.h>
+#include <sys/types.h>
+
+#include <SupportDefs.h>
+
+#define UART_KIND_EFI "EFI"
+
+
+class DebugUARTEFI : public DebugUART {
+public:
+							DebugUARTEFI(addr_t base, int64 clock);
+							~DebugUARTEFI();
+
+			void			InitEarly();
+			void			Init();
+			void			InitPort(uint32 baud);
+
+			int				PutChar(char c);
+			int				GetChar(bool wait);
+
+			void			FlushTx();
+			void			FlushRx();
+private:
+			efi_serial_io_protocol*	fEFISerialIO;
+};
+
+
+DebugUARTEFI* arch_get_uart_EFI(addr_t base, int64 clock);
+
+
+#endif /* _KERNEL_ARCH_DEBUG_UART_EFI_H */
