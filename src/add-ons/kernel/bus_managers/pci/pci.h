bc22a316fd7692a81b5403404d5f7c7c9fb8d91d
X512 <danger_mail@list.ru>
2023-03-20 03:30:05 +0900

    bus_managers/pci: split PCI controller to separate add-on
    Add-ons are in following commits.

--- src/add-ons/kernel/bus_managers/pci/pci.h
+++ src/add-ons/kernel/bus_managers/pci/pci.h
@@ -9,16 +9,10 @@
 
 #include <PCI.h>
 
-#ifdef __cplusplus
-  #include <VectorMap.h>
-#endif
+#include <VectorMap.h>
 
 #include "pci_controller.h"
 #include "pci_msi.h"
-
-#if defined(__i386__) || defined(__x86_64__)
-#include "pci_arch_info.h"
-#endif
 
 #define TRACE_PCI
 #ifndef TRACE_PCI
@@ -26,8 +20,6 @@
 #else
 #	define TRACE(x) dprintf x
 #endif
-
-#ifdef __cplusplus
 
 struct PCIDev;
 
@@ -57,11 +49,18 @@ struct PCIDev {
 
 struct domain_data {
 	// These two are set in PCI::AddController:
-	pci_controller *	controller;
+	pci_controller_module_info *controller;
 	void *				controller_cookie;
+	device_node *		root_node;
 
 	// All the rest is set in PCI::InitDomainData
 	int					max_bus_devices;
+	pci_resource_range 	ranges[kPciRangeEnd];
+
+#if !(defined(__i386__) || defined(__x86_64__))
+	area_id				io_port_area;
+	uint8 *				io_port_adr;
+#endif
 };
 
 
@@ -72,9 +71,13 @@ public:
 
 			void			InitDomainData();
 			void			InitBus();
+			status_t	Finalize();
 
-			status_t		AddController(pci_controller *controller,
-								void *controller_cookie);
+			status_t		AddController(pci_controller_module_info *controller,
+								void *controller_cookie, device_node *root_node);
+
+			status_t		LookupRange(uint32 type, phys_addr_t pciAddr,
+								uint8 &domain, pci_resource_range &range, uint8 **mappedAdr = NULL);
 
 			status_t		GetNthInfo(long index, pci_info *outInfo);
 
@@ -172,8 +175,10 @@ private:
 								uint32 &address, uint32 *size = NULL,
 								uint8 *flags = NULL);
 
+public:
 			domain_data *	_GetDomainData(uint8 domain);
 
+private:
 			status_t		_CreateVirtualBus(uint8 domain, uint8 bus,
 								uint8 *virtualBus);
 
@@ -206,12 +211,8 @@ private:
 
 extern PCI *gPCI;
 
-#endif // __cplusplus
 
-
-#ifdef __cplusplus
 extern "C" {
-#endif
 
 status_t	pci_init(void);
 status_t	pci_init_deferred(void);
@@ -226,8 +227,6 @@ void		pci_write_config(uint8 virtualBus, uint8 device, uint8 function,
 
 void		__pci_resolve_virtual_bus(uint8 virtualBus, uint8 *domain, uint8 *bus);
 
-#ifdef __cplusplus
 }
-#endif
 
 #endif	/* __PCI_H__ */
