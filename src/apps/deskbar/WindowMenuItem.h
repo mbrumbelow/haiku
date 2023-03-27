544b33279ba4896181b051652a7daa87c898925f
John Scipione <jscipione@gmail.com>
2023-03-26 20:13:14 -0400

    Deskbar: Use hvif window icons & scale with font
    Add vector rdefs for shown, hidden, shown switch and
    hidden switch icons. Remove unused bitmap resources.
    Add window switch vector icons to artwork.
    
    Create window icon cache in TBarApp and cache the window
    icons based on font size. Fixes memory leak in #18357.
    
    Don't draw off-workspace lines in Switcher, use icon for
    that instead. Position window name a bit better in Switcher.
    Fixes crash reported in #18359.
    
    Fixes #14694 #17918 #18357 #18359
    
    Change-Id: Ib63cc307f14cda397ffb66ea74091be59e6e5535

--- src/apps/deskbar/WindowMenuItem.h
+++ src/apps/deskbar/WindowMenuItem.h
@@ -50,16 +50,16 @@ public:
 								TWindowMenuItem(const char* name, int32 id,
 									bool mini, bool currentWorkSpace,
 									bool dragging = false);
+	virtual						~TWindowMenuItem();
 
-			void				SetTo(const char* name, int32 id, bool mini,
-									bool currentWorkSpace,
-									bool dragging = false);
+			void				SetTo(const char* name, int32 id, bool minimized,
+									bool local, bool dragging = false);
 
 			bool				Expanded() const { return fExpanded; };
 			void				SetExpanded(bool expand) { fExpanded = expand; };
 
 			int32				ID() const { return fID; };
-			bool				Modified() const { return fModified; };
+			bool				Modified() const { return fIsModified; };
 
 			bool				RequiresUpdate() { return fRequireUpdate; };
 			void				SetRequireUpdate(bool update)
@@ -77,17 +77,21 @@ protected:
 private:
 			void				_Init(const char* name);
 
+private:
+	const	BBitmap*			fBitmap;
+
 			int32				fID;
-			bool				fMini;
-			bool				fCurrentWorkSpace;
-			const BBitmap*		fBitmap;
+
 			float				fLabelWidth;
 			float				fLabelAscent;
 			float				fLabelDescent;
+
+			bool				fIsModified;
+			bool				fIsMinimized;
+			bool				fIsLocal;
 			bool				fDragging;
 			bool				fExpanded;
 			bool				fRequireUpdate;
-			bool				fModified;
 };
 
 
