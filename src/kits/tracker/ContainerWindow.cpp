7ae823072bbd0fab439cdace7d0b546b56700935
John Scipione <jscipione@gmail.com>
2023-04-14 20:35:06 -0400

    Tracker: Add Cut/Copy/Paste items to context menus
    Remove CUT_COPY_PASTE_IN_CONTEXT_MENU define entirely.
    
    Change-Id: I457ee140c2bde7e5ab750528b3196adfe9eb7018

--- src/kits/tracker/ContainerWindow.cpp
+++ src/kits/tracker/ContainerWindow.cpp
@@ -2949,7 +2949,6 @@ BContainerWindow::AddFileContextMenus(BMenu* menu)
 			new BMessage(kRestoreFromTrash), 0));
 	}
 
-#ifdef CUT_COPY_PASTE_IN_CONTEXT_MENU
 	menu->AddSeparatorItem();
 	BMenuItem* cutItem = new BMenuItem(B_TRANSLATE("Cut"),
 		new BMessage(B_CUT), 'X');
@@ -2963,7 +2962,6 @@ BContainerWindow::AddFileContextMenus(BMenu* menu)
 		new BMessage(B_PASTE), 'V');
 	pasteItem->SetEnabled(FSClipboardHasRefs() && !TargetVolumeIsReadOnly());
 	menu->AddItem(pasteItem);
-#endif
 
 	menu->AddSeparatorItem();
 
@@ -2977,11 +2975,9 @@ BContainerWindow::AddFileContextMenus(BMenu* menu)
 
 	// set targets as needed
 	menu->SetTargetForItems(PoseView());
-#ifdef CUT_COPY_PASTE_IN_CONTEXT_MENU
 	cutItem->SetTarget(this);
 	copyItem->SetTarget(this);
 	pasteItem->SetTarget(this);
-#endif
 }
 
 
@@ -3042,13 +3038,11 @@ BContainerWindow::AddWindowContextMenus(BMenu* menu)
 	if (needSeparator)
 		menu->AddSeparatorItem();
 
-#ifdef CUT_COPY_PASTE_IN_CONTEXT_MENU
 	BMenuItem* pasteItem = new BMenuItem(B_TRANSLATE("Paste"),
 		new BMessage(B_PASTE), 'V');
 	pasteItem->SetEnabled(FSClipboardHasRefs() && !TargetVolumeIsReadOnly());
 	menu->AddItem(pasteItem);
 	menu->AddSeparatorItem();
-#endif
 
 	BMenu* arrangeBy = new BMenu(B_TRANSLATE("Arrange by"));
 	PopulateArrangeByMenu(arrangeBy);
@@ -3082,9 +3076,7 @@ BContainerWindow::AddWindowContextMenus(BMenu* menu)
 
 	// target items as needed
 	menu->SetTargetForItems(PoseView());
-#ifdef CUT_COPY_PASTE_IN_CONTEXT_MENU
 	pasteItem->SetTarget(this);
-#endif
 }
 
 
