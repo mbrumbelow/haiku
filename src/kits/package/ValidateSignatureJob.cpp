98ea77df8c30e7eaaa5683a9758b4c2e8f344f19
Alexander von Gluck IV <kallisti5@unixzen.com>
2023-04-18 10:22:10 -0500

    WIP: package/repository: Fetch repo signaturs and verify
    Change-Id: I1a231ebae19cfc748d977abee55347ecb6a72e6f

---
+++ src/kits/package/ValidateSignatureJob.cpp
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2011-2023, Haiku, Inc. All Rights Reserved.
+ * Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ *		Oliver Tappe <zooey@hirschkaefer.de>
+ *		Alexander von Gluck IV <kallisti5@unixzen.com>
+ */
+
+
+#include <package/ValidateSignatureJob.h>
+
+#include "Minisign.h"
+#include <File.h>
+
+#include <package/Context.h>
+
+
+namespace BPackageKit {
+
+namespace BPrivate {
+
+
+ValidateSignatureJob::ValidateSignatureJob(const BContext& context,
+	const BString& title, BEntry* signatureFile, BEntry* targetFile,
+	bool failIfSignatureUntrusted)
+	:
+	inherited(context, title),
+	fSignatureFile(signatureFile),
+	fTargetFile(targetFile),
+	fFailIfSignatureUntrusted(failIfSignatureUntrusted),
+	fTargetTrusted(false)
+{
+}
+
+
+ValidateSignatureJob::~ValidateSignatureJob()
+{
+}
+
+
+status_t
+ValidateSignatureJob::Execute()
+{
+	if (fSignatureFile == NULL || fTargetFile == NULL)
+		return B_BAD_VALUE;
+
+	/*
+	status_t result = fExpectedChecksumAccessor->GetChecksum(expectedChecksum);
+	if (result != B_OK)
+		return result;
+
+	result = fRealChecksumAccessor->GetChecksum(realChecksum);
+	if (result != B_OK)
+		return result;
+
+	fChecksumsMatch = expectedChecksum.ICompare(realChecksum) == 0;
+
+	if (fFailIfChecksumsDontMatch && !fChecksumsMatch) {
+		BString error = BString("Signature error:\n")
+			<< "expected '"	<< expectedChecksum << "'\n"
+			<< "got      '" << realChecksum << "'";
+		SetErrorString(error);
+		return B_BAD_DATA;
+	}
+	*/
+
+	return B_OK;
+}
+
+
+bool
+ValidateSignatureJob::TrustedFile() const
+{
+	return fTargetTrusted;
+}
+
+
+}	// namespace BPrivate
+
+}	// namespace BPackageKit
