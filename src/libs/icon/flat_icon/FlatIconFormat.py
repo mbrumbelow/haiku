78e12e9e74813b84039e499e287f9d6a443f0523
Zardshard <0azrune6@zard.anonaddy.com>
2023-05-18 10:27:33 -0400

    libicon: Add tool to decode custom float format
    I've decoded the HVIF format's custom three byte float format by hand
    several times by now. This adds a python tool to automatically decode
    these.
    
    Change-Id: I92cc1697a0b4fc3b55bb20a5bbcf6e93e3c0738b

---
+++ src/libs/icon/flat_icon/FlatIconFormat.py
@@ -0,0 +1,10 @@
+# Utility functions to aid in converting from the special data types the flat icon format has
+
+# Example usage:
+# >>> from FlatIconFormat import *
+# >>> read_float_24(0x3ffffd)
+def read_float_24(value):
+	sign = (-1) ** ((value & 0x800000) >> 23 + 1)
+	exponent = 2 ** (((value & 0x7e0000) >> 17) - 32)
+	mantissa = (value & 0x01ffff) * 2 ** -17
+	return sign * exponent * (mantissa + 1)
