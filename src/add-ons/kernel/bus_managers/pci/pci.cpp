74f568fca6a01c3a1df436d3f3ce3cf51a5db79c
Alexander von Gluck IV <kallisti5@unixzen.com>
2023-08-04 11:05:58 -0500

    bus_managers/pci: Add device Thunderbolt detection
    * Detect Thunderbolt bridging related devices.
    
    Change-Id: Ie86c2dc75f1caae409d0511571f9b1d56a397604

--- src/add-ons/kernel/bus_managers/pci/pci.cpp
+++ src/add-ons/kernel/bus_managers/pci/pci.cpp
@@ -1927,6 +1927,24 @@ PCI::UpdateInterruptLine(uint8 domain, uint8 bus, uint8 _device, uint8 function,
 }
 
 
+bool
+PCI::IsThunderbolt(PCIDev *device)
+{
+	uint16 extendedCapOffset;
+	status_t res = FindExtendedCapability(device, PCI_extcap_id_vendor, &extendedCapOffset);
+	if (res == B_OK) {
+		uint32 tb = ReadConfig(device, extendedCapOffset, 2);
+		if (PCI_vndr_header_id(tb) == PCI_vsec_id_intel_tbt)
+			return true;
+	}
+
+	// TODO: Walk up each bridge device to confirm if device is downstream
+	// a Thunderbolt Controller
+
+	return false;
+}
+
+
 uint8
 PCI::GetPowerstate(PCIDev *device)
 {
