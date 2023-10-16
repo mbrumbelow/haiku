c985bdfe7d3a66f51cd16f0f9f812233489b8564
thaflo <thaflo@thaler-online.com>
2023-10-16 08:23:54 +0200

    code style cleanups and fix gcc2 build

--- src/apps/screenshot/SelectionWindow.h
+++ src/apps/screenshot/SelectionWindow.h
@@ -1,7 +1,3 @@
-/*
- * Copyright 2013-2021, Stefano Ceccherini <stefano.ceccherini@gmail.com>
- * All rights reserved. Distributed under the terms of the MIT license.
- */
 #ifndef __SELECTIONWINDOW_H
 #define __SELECTIONWINDOW_H
 
@@ -14,12 +10,12 @@ class SelectionWindow : public BWindow {
 public:
 
 	SelectionWindow(BMessenger& target, uint32 command);
-	
+
 	virtual void Show();
 	virtual bool QuitRequested();
-	
-private:	
-	SelectionView *fView;	
+
+private:
+	SelectionView *fView;
 	BMessenger fTarget;
 	uint32 fCommand;
 };
