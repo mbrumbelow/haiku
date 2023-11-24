3fb543efd100caede7e1fc628f961594cc0b9de5
PulkoMandy <pulkomandy@pulkomandy.tk>
2023-11-24 20:57:02 +0100

    Update ACPICA to 20230628
    Not working on my machine yet.
    
    Change-Id: I5cbe5fa52bcca13ef8308ae1d2a1b1c19afcfe7e

--- src/add-ons/kernel/bus_managers/acpi/acpica/include/platform/achaiku.h
+++ src/add-ons/kernel/bus_managers/acpi/acpica/include/platform/achaiku.h
@@ -8,7 +8,7 @@
  *
  * 1. Copyright Notice
  *
- * Some or all of this work - Copyright (c) 1999 - 2021, Intel Corp.
+ * Some or all of this work - Copyright (c) 1999 - 2023, Intel Corp.
  * All rights reserved.
  *
  * 2. License
@@ -197,8 +197,8 @@ struct mutex;
 #endif
 
 /* Based on FreeBSD's due to lack of documentation */
-extern int AcpiOsAcquireGlobalLock(uint32 *lock);
-extern int AcpiOsReleaseGlobalLock(uint32 *lock);
+int AcpiOsAcquireGlobalLock(uint32 *lock);
+int AcpiOsReleaseGlobalLock(uint32 *lock);
 
 #define ACPI_ACQUIRE_GLOBAL_LOCK(GLptr, Acq)    do {                \
         (Acq) = AcpiOsAcquireGlobalLock((uint32 *)&((GLptr)->GlobalLock));    \
