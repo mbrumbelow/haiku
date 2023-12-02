67a816c9f9006e04fed2f7f9c8e8467a2731052d
John Scipione <jscipione@gmail.com>
2023-12-01 21:18:18 -0500

    Tracker: Treat root (aka Disks) like a volume
    This yields the volume context menu on the Disks icon. Grab root
    (Disks) icon before getting volume icon so Disks doesn't get the
    generic volume icon. Don't add free space bar to Disks.
    
    Add Mount menu to the Disks File menu and context menu. Mount
    settings option has been removed as you can get to it from inside
    the Mount menu instead.
    
    SetupMountMenu() method is used to insert the Mount menu similar
    to SetupOpenWith() and SetupNavMenus(). Add DetachMountMenu()
    convenience method.
    
    No "Unmount" on Desktop or root volume context menu as before.
    
    Built on top of Shortcuts refactor.
    
    Fixes #5584
    
    Change-Id: Ic9e272807a0a36b89cba7258b9937b09b2da32dc

--- src/kits/tracker/ContainerWindow.h
+++ src/kits/tracker/ContainerWindow.h
@@ -257,6 +257,10 @@ protected:
 	virtual void SetupEditQueryItem(BMenu*);
 	virtual void SetupEditQueryItem(BMenu*, const entry_ref* ref);
 	virtual void SetupDiskMenu(BMenu*);
+	virtual void SetupMountMenu(BMenu*, MenuContext context);
+	virtual void SetupMountMenu(BMenu*, MenuContext context,
+		const entry_ref* ref);
+	BMenuItem* DetachMountMenu();
 
 	virtual void BuildAddOnsMenu(BMenu*);
 	void BuildMimeTypeList(BStringList& mimeTypes);
@@ -318,6 +322,7 @@ protected:
 	BMenuItem* fCreateLinkItem;
 	BMenuItem* fOpenWithItem;
 	BMenuItem* fEditQueryItem;
+	BMenuItem* fMountItem;
 	ModelMenuItem* fNavigationItem;
 	BMenuItem* fNewTemplatesItem;
 	BMenuBar* fMenuBar;
