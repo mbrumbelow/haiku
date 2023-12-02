571923cb659ecb3353f0e2bdf4bf7acbf00fbbb2
John Scipione <jscipione@gmail.com>
2023-12-01 21:18:18 -0500

    Tracker: Treat root (aka Disks) like a volume
    This yields the volume context menu on the Disks icon. Grab root
    (Disks) icon before getting volume icon so Disks doesn't get the
    generic volume icon. Don't add free space bar to Disks.
    
    Add Mount menu to the Disks File menu and context menu. Mount
    settings option has been removed as you can get to it from inside
    the Mount menu instead.
    
    SetupMountMenu() method is used to insert the Mount menu in the
    menu similar SetupOpenWith() and SetupNavMenus().
    Add DetachMountMenu() convenience method.
    
    No "Unmount" on Desktop or root volume context menu as before.
    
    Built on top of Shortcuts refactor.
    
    Fixes #17943
    
    Change-Id: Ic9e272807a0a36b89cba7258b9937b09b2da32dc

--- src/kits/tracker/Pose.cpp
+++ src/kits/tracker/Pose.cpp
@@ -88,17 +88,15 @@ BPose::BPose(Model* model, BPoseView* view, uint32 clipboardMode,
 {
 	CreateWidgets(view);
 
-	if (model->IsVolume()) {
+	if (model->IsVolume() && !model->IsRoot()) {
 		fs_info info;
 		dev_t device = model->NodeRef()->device;
 		BVolume* volume = new BVolume(device);
-		if (volume->InitCheck() == B_OK
-			&& fs_stat_dev(device, &info) == B_OK) {
+		if (volume->InitCheck() == B_OK && fs_stat_dev(device, &info) == B_OK) {
 			// Philosophy here:
 			// Bars go on all read/write volumes
-			// Exceptions: Not on CDDA
-			if (strcmp(info.fsh_name,"cdda") != 0
-				&& !volume->IsReadOnly()) {
+			// Exceptions: Not on CDDA or Disks
+			if (strcmp(info.fsh_name, "cdda") != 0 && !volume->IsReadOnly()) {
 				// The volume is ok and we want space bars on it
 				gPeriodicUpdatePoses.AddPose(this, view,
 					_PeriodicUpdateCallback, volume);
@@ -114,7 +112,7 @@ BPose::BPose(Model* model, BPoseView* view, uint32 clipboardMode,
 
 BPose::~BPose()
 {
-	if (fModel->IsVolume()) {
+	if (fModel->IsVolume() && !fModel->IsRoot()) {
 		// we might be registered for periodic updates
 		BVolume* volume = NULL;
 		if (gPeriodicUpdatePoses.RemovePose(this, (void**)&volume))
