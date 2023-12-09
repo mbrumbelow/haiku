3f1cd3decbf763cd5ad23854d4b73e75369b430d
John Scipione <jscipione@gmail.com>
2023-12-09 02:02:10 -0500

    Tracker: Shortcuts refactor
    Create Shortcuts class that creates shortcut menu items and performs
    manipulations to label and modifiers. Not every Tracker shortcut is
    present but the ones used multiple times and the tricky ones are.
    
    Add SetMoveToTrashItem() method to update "Move to Trash"/"Delete".
    
    Create AddIconSizeMenu() in BContainerWindow to eliminate duplicate
    shortcut handling code in DeskWindow.
    
    Update shortcuts in ContainerWindow, DeskWindow, FilePanelPriv,
    QueryContainerWindow, VirtualDirectoryWindow and Info Header.
    
    Change-Id: Ic41b148cb6bcf14288cb5588038c13ea12426dc2

---
+++ src/kits/tracker/Shortcuts.cpp
@@ -0,0 +1,683 @@
+/*
+ * Copyright 2020-2023 Haiku, Inc. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ *		John Scipione, jscipione@gmail.com
+ */
+
+
+#include "Shortcuts.h"
+
+#include <Catalog.h>
+#include <InterfaceDefs.h>
+#include <Menu.h>
+#include <MenuItem.h>
+#include <Message.h>
+
+#include "Commands.h"
+#include "TrackerSettings.h"
+
+
+#undef B_TRANSLATION_CONTEXT
+#define B_TRANSLATION_CONTEXT "ContainerWindow"
+
+
+Shortcuts::Shortcuts()
+{
+}
+
+
+BMenuItem*
+Shortcuts::AddPrinterItem()
+{
+	return new BMenuItem(B_TRANSLATE("Add printer" B_UTF8_ELLIPSIS), new BMessage(kAddPrinter));
+}
+
+
+const char*
+Shortcuts::AddPrinterLabel()
+{
+	return B_TRANSLATE("Add printer" B_UTF8_ELLIPSIS);
+}
+
+
+BMenuItem*
+Shortcuts::CleanupItem()
+{
+	return new BMenuItem(B_TRANSLATE("Clean up"), new BMessage(kCleanup), 'K');
+}
+
+
+const char*
+Shortcuts::CleanupLabel()
+{
+	return (modifiers() & B_SHIFT_KEY) != 0
+		? B_TRANSLATE("Clean up all")
+		: B_TRANSLATE("Clean up");
+}
+
+
+int32
+Shortcuts::CleanupCommand()
+{
+	return (modifiers() & B_SHIFT_KEY) != 0
+		? kCleanupAll
+		: kCleanup;
+}
+
+
+BMenuItem*
+Shortcuts::CloseItem()
+{
+	return new BMenuItem(B_TRANSLATE("Close"), new BMessage(B_QUIT_REQUESTED), 'W');
+}
+
+
+const char*
+Shortcuts::CloseLabel()
+{
+	return (modifiers() & B_SHIFT_KEY) != 0
+		? B_TRANSLATE("Close all")
+		: B_TRANSLATE("Close");
+}
+
+
+int32
+Shortcuts::CloseCommand()
+{
+	return (modifiers() & B_SHIFT_KEY) != 0
+		? kCloseAllWindows
+		: B_QUIT_REQUESTED;
+}
+
+
+BMenuItem*
+Shortcuts::CloseAllInWorkspaceItem()
+{
+	return new BMenuItem(B_TRANSLATE("Close all in workspace"), new BMessage(kCloseAllInWorkspace),
+		'Q');
+}
+
+
+const char*
+Shortcuts::CloseAllInWorkspaceLabel()
+{
+	return B_TRANSLATE("Close all in workspace");
+}
+
+
+BMenuItem*
+Shortcuts::CopyItem()
+{
+	return new BMenuItem(B_TRANSLATE("Copy"), new BMessage(B_COPY), 'C');
+}
+
+
+const char*
+Shortcuts::CopyLabel()
+{
+	return (modifiers() & B_SHIFT_KEY) != 0
+		? B_TRANSLATE("Copy more")
+		: B_TRANSLATE("Copy");
+}
+
+
+int32
+Shortcuts::CopyCommand()
+{
+	return (modifiers() & B_SHIFT_KEY) != 0
+		? kCopyMoreSelectionToClipboard
+		: B_COPY;
+}
+
+
+BMenuItem*
+Shortcuts::CopyToItem(BMenu* menu)
+{
+	return new BMenuItem(menu, new BMessage(kCopySelectionTo));
+}
+
+
+const char*
+Shortcuts::CopyToLabel()
+{
+	return B_TRANSLATE("Copy to");
+}
+
+
+BMenuItem*
+Shortcuts::CreateLinkItem(BMenu* menu)
+{
+	return new BMenuItem(menu, new BMessage(kCreateLink));
+}
+
+
+const char*
+Shortcuts::CreateLinkLabel()
+{
+	return (modifiers() & B_SHIFT_KEY) != 0
+		? B_TRANSLATE("Create relative link")
+		: B_TRANSLATE("Create link");
+}
+
+
+int32
+Shortcuts::CreateLinkCommand()
+{
+	return (modifiers() & B_SHIFT_KEY) != 0
+		? kCreateRelativeLink
+		: kCreateLink;
+}
+
+
+BMenuItem*
+Shortcuts::CreateLinkHereItem()
+{
+	return new BMenuItem(B_TRANSLATE("Create link here"), new BMessage(kCreateLink));
+}
+
+
+const char*
+Shortcuts::CreateLinkHereLabel()
+{
+	return (modifiers() & B_SHIFT_KEY) != 0
+		? B_TRANSLATE("Create relative link here")
+		: B_TRANSLATE("Create link here");
+}
+
+
+int32
+Shortcuts::CreateLinkHereCommand()
+{
+	return (modifiers() & B_SHIFT_KEY) != 0
+		? kCreateRelativeLink
+		: kCreateLink;
+}
+
+
+BMenuItem*
+Shortcuts::CutItem()
+{
+	return new BMenuItem(B_TRANSLATE("Cut"), new BMessage(B_CUT), 'X');
+}
+
+
+const char*
+Shortcuts::CutLabel()
+{
+	return (modifiers() & B_SHIFT_KEY) != 0
+		? B_TRANSLATE("Cut more")
+		: B_TRANSLATE("Cut");
+}
+
+
+int32
+Shortcuts::CutCommand()
+{
+	return (modifiers() & B_SHIFT_KEY) != 0
+		? kCutMoreSelectionToClipboard
+		: B_CUT;
+}
+
+
+BMenuItem*
+Shortcuts::DeleteItem()
+{
+	return new BMenuItem(B_TRANSLATE("Delete"), new BMessage(kDelete));
+}
+
+
+const char*
+Shortcuts::DeleteLabel()
+{
+	return B_TRANSLATE("Delete");
+}
+
+
+BMenuItem*
+Shortcuts::DuplicateItem()
+{
+	return new BMenuItem(B_TRANSLATE("Duplicate"), new BMessage(kDuplicateSelection), 'D');
+}
+
+
+const char*
+Shortcuts::DuplicateLabel()
+{
+	return B_TRANSLATE("Duplicate");
+}
+
+
+BMenuItem*
+Shortcuts::EditNameItem()
+{
+	return new BMenuItem(B_TRANSLATE("Edit name"), new BMessage(kEditItem), 'E');
+}
+
+
+const char*
+Shortcuts::EditNameLabel()
+{
+	return B_TRANSLATE("Edit name");
+}
+
+
+BMenuItem*
+Shortcuts::EditQueryItem()
+{
+	return new BMenuItem(B_TRANSLATE("Edit query"), new BMessage(kEditQuery), 'G');
+}
+
+
+const char*
+Shortcuts::EditQueryLabel()
+{
+	return B_TRANSLATE("Edit query");
+}
+
+
+BMenuItem*
+Shortcuts::EmptyTrashItem()
+{
+	return new BMenuItem(B_TRANSLATE("Empty Trash"), new BMessage(kEmptyTrash));
+}
+
+
+const char*
+Shortcuts::EmptyTrashLabel()
+{
+	return B_TRANSLATE("Empty Trash");
+}
+
+
+BMenuItem*
+Shortcuts::FindItem()
+{
+	return new BMenuItem(B_TRANSLATE("Find" B_UTF8_ELLIPSIS), new BMessage(kFindButton), 'F');
+}
+
+
+const char*
+Shortcuts::FindLabel()
+{
+	return B_TRANSLATE("Find" B_UTF8_ELLIPSIS);
+}
+
+
+BMenuItem*
+Shortcuts::GetInfoItem()
+{
+	return new BMenuItem(B_TRANSLATE("Get info"), new BMessage(kGetInfo), 'I');
+}
+
+
+const char*
+Shortcuts::GetInfoLabel()
+{
+	return B_TRANSLATE("Get info");
+}
+
+
+BMenuItem*
+Shortcuts::IdentifyItem()
+{
+	BMessage* message = new BMessage(kIdentifyEntry);
+	BMenuItem* item = new BMenuItem(B_TRANSLATE("Identify"), message);
+	message->AddBool("force", false);
+	return item;
+}
+
+
+const char*
+Shortcuts::IdentifyLabel()
+{
+	return (modifiers() & B_SHIFT_KEY) != 0
+		? B_TRANSLATE("Force identify")
+		: B_TRANSLATE("Identify");
+}
+
+
+BMenuItem*
+Shortcuts::InvertSelectionItem()
+{
+	return new BMenuItem(B_TRANSLATE("Invert selection"), new BMessage(kInvertSelection), 'S');
+}
+
+
+const char*
+Shortcuts::InvertSelectionLabel()
+{
+	return B_TRANSLATE("Invert selection");
+}
+
+
+BMenuItem*
+Shortcuts::MakeActivePrinterItem()
+{
+	return new BMenuItem(B_TRANSLATE("Make active printer"), new BMessage(kMakeActivePrinter));
+}
+
+
+const char*
+Shortcuts::MakeActivePrinterLabel()
+{
+	return B_TRANSLATE("Make active printer");
+}
+
+
+const char*
+Shortcuts::MountLabel()
+{
+	return B_TRANSLATE("Mount");
+}
+
+
+BMenuItem*
+Shortcuts::MoveToItem(BMenu* menu)
+{
+	return new BMenuItem(menu, new BMessage(kMoveSelectionTo));
+}
+
+
+const char*
+Shortcuts::MoveToLabel()
+{
+	return B_TRANSLATE("Move to");
+}
+
+
+BMenuItem*
+Shortcuts::MoveToTrashItem()
+{
+	return new BMenuItem(B_TRANSLATE("Move to Trash"), new BMessage(kMoveToTrash), 'T');
+}
+
+
+const char*
+Shortcuts::MoveToTrashLabel()
+{
+	return TrackerSettings().DontMoveFilesToTrash() || (modifiers() & B_SHIFT_KEY) != 0
+		? B_TRANSLATE("Delete")
+		: B_TRANSLATE("Move to Trash");
+}
+
+
+int32
+Shortcuts::MoveToTrashCommand()
+{
+	return TrackerSettings().DontMoveFilesToTrash() || (modifiers() & B_SHIFT_KEY) != 0
+		? kDelete
+		: kMoveToTrash;
+}
+
+
+BMenuItem*
+Shortcuts::NewFolderItem()
+{
+	return new BMenuItem(B_TRANSLATE("New folder"), new BMessage(kNewFolder), 'N');
+}
+
+
+const char*
+Shortcuts::NewFolderLabel()
+{
+	return B_TRANSLATE("New folder");
+}
+
+
+BMenuItem*
+Shortcuts::OpenItem()
+{
+	return new BMenuItem(B_TRANSLATE("Open"), new BMessage(kOpenSelection), 'O');
+}
+
+
+const char*
+Shortcuts::OpenLabel()
+{
+	return B_TRANSLATE("Open");
+}
+
+
+BMenuItem*
+Shortcuts::OpenParentItem()
+{
+	return new BMenuItem(B_TRANSLATE("Open parent"), new BMessage(kOpenParentDir), B_UP_ARROW);
+}
+
+
+const char*
+Shortcuts::OpenParentLabel()
+{
+	return B_TRANSLATE("Open parent");
+}
+
+
+BMenuItem*
+Shortcuts::OpenWithItem()
+{
+	return new BMenuItem(B_TRANSLATE("Open with" B_UTF8_ELLIPSIS),
+		new BMessage(kOpenSelectionWith));
+}
+
+
+BMenuItem*
+Shortcuts::OpenWithItem(BMenu* menu)
+{
+	return new BMenuItem(menu, new BMessage(kOpenSelectionWith));
+}
+
+
+const char*
+Shortcuts::OpenWithhLabel()
+{
+	return B_TRANSLATE("Open with" B_UTF8_ELLIPSIS);
+}
+
+
+BMenuItem*
+Shortcuts::PasteItem()
+{
+	return new BMenuItem(B_TRANSLATE("Paste"), new BMessage(B_PASTE), 'V');
+}
+
+
+const char*
+Shortcuts::PasteLabel()
+{
+	return (modifiers() & B_SHIFT_KEY) != 0
+		? B_TRANSLATE("Paste links")
+		: B_TRANSLATE("Paste");
+}
+
+
+int32
+Shortcuts::PasteCommand()
+{
+	return (modifiers() & B_SHIFT_KEY) != 0
+		? kPasteLinksFromClipboard
+		: B_PASTE;
+}
+
+
+BMenuItem*
+Shortcuts::RestoreItem()
+{
+	return new BMenuItem(B_TRANSLATE("Restore"), new BMessage(kRestoreFromTrash));
+}
+
+
+const char*
+Shortcuts::RestoreLabel()
+{
+	return B_TRANSLATE("Restore");
+}
+
+
+BMenuItem*
+Shortcuts::ReverseOrderItem()
+{
+	return new BMenuItem(B_TRANSLATE("Reverse order"), new BMessage(kArrangeReverseOrder));
+}
+
+
+const char*
+Shortcuts::ReverseOrderLabel()
+{
+	return B_TRANSLATE("Reverse order");
+}
+
+
+BMenuItem*
+Shortcuts::ResizeToFitItem()
+{
+	return new BMenuItem(B_TRANSLATE("Resize to fit"), new BMessage(kResizeToFit), 'Y');
+}
+
+
+const char*
+Shortcuts::ResizeToFitLabel()
+{
+	return B_TRANSLATE("Resize to fit");
+}
+
+
+
+BMenuItem*
+Shortcuts::SelectItem()
+{
+	return new BMenuItem(B_TRANSLATE("Select" B_UTF8_ELLIPSIS), new BMessage(kShowSelectionWindow),
+		'A', B_SHIFT_KEY);
+}
+
+
+const char*
+Shortcuts::SelectLabel()
+{
+	return B_TRANSLATE("Select" B_UTF8_ELLIPSIS);
+}
+
+
+BMenuItem*
+Shortcuts::SelectAllItem()
+{
+	return new BMenuItem(B_TRANSLATE("Select all"), new BMessage(B_SELECT_ALL), 'A', B_SHIFT_KEY);
+}
+
+
+const char*
+Shortcuts::SelectAllLabel()
+{
+	return B_TRANSLATE("Select all");
+}
+
+
+BMenuItem*
+Shortcuts::UnmountItem()
+{
+	return new BMenuItem(B_TRANSLATE("Unmount"), new BMessage(kUnmountVolume), 'U');
+}
+
+
+const char*
+Shortcuts::UnmountLabel()
+{
+	return B_TRANSLATE("Unmount");
+}
+
+
+void
+Shortcuts::UpdateCleanupItem(BMenuItem* item)
+{
+	item->SetLabel(CleanupLabel());
+	item->Message()->what = CleanupCommand();
+	item->SetShortcut(item->Shortcut(), B_COMMAND_KEY | (modifiers() & B_SHIFT_KEY));
+}
+
+
+void
+Shortcuts::UpdateCloseItem(BMenuItem* item)
+{
+	item->SetLabel(CloseLabel());
+	item->Message()->what = CloseCommand();
+	item->SetShortcut(item->Shortcut(), B_COMMAND_KEY | (modifiers() & B_SHIFT_KEY));
+}
+
+
+void
+Shortcuts::UpdateCopyItem(BMenuItem* item)
+{
+	item->SetLabel(CopyLabel());
+	item->Message()->what = CopyCommand();
+	item->SetShortcut(item->Shortcut(), B_COMMAND_KEY | (modifiers() & B_SHIFT_KEY));
+}
+
+
+void
+Shortcuts::UpdateCreateLinkItem(BMenuItem* item)
+{
+	item->SetLabel(CreateLinkLabel());
+	item->Message()->what = CreateLinkCommand();
+}
+
+
+void
+Shortcuts::UpdateCreateLinkHereItem(BMenuItem* item)
+{
+	item->SetLabel(CreateLinkHereLabel());
+	item->Message()->what = CreateLinkHereCommand();
+}
+
+
+void
+Shortcuts::UpdateCreateLinkInMenu(BMenu* menu)
+{
+	BMenuItem* link = menu->FindItem(kCreateLink);
+	BMenuItem* relL = menu->FindItem(kCreateRelativeLink);
+	if (link == NULL && relL == NULL)
+		return;
+
+	if (relL != NULL)
+		link = relL;
+	if (link != NULL)
+		UpdateCreateLinkItem(link);
+}
+
+
+void
+Shortcuts::UpdateCutItem(BMenuItem* item)
+{
+	item->SetLabel(CutLabel());
+	item->Message()->what = CutCommand();
+	item->SetShortcut(item->Shortcut(), B_COMMAND_KEY | (modifiers() & B_SHIFT_KEY));
+}
+
+
+void
+Shortcuts::UpdateIdentifyItem(BMenuItem* item)
+{
+	item->SetLabel(IdentifyLabel());
+	item->Message()->ReplaceBool("force", (modifiers() & B_SHIFT_KEY) != 0);
+}
+
+
+void
+Shortcuts::UpdateMoveToTrashItem(BMenuItem* item)
+{
+	item->SetLabel(MoveToTrashLabel());
+	item->Message()->what = MoveToTrashCommand();
+	if (TrackerSettings().DontMoveFilesToTrash())
+		item->SetShortcut(item->Shortcut(), B_COMMAND_KEY);
+	else
+		item->SetShortcut(item->Shortcut(), B_COMMAND_KEY | (modifiers() & B_SHIFT_KEY));
+}
+
+
+void
+Shortcuts::UpdatePasteItem(BMenuItem* item)
+{
+	item->SetLabel(PasteLabel());
+	item->Message()->what = PasteCommand();
+	item->SetShortcut(item->Shortcut(), B_COMMAND_KEY | (modifiers() & B_SHIFT_KEY));
+}
---
+++ src/kits/tracker/Shortcuts.h
@@ -0,0 +1,150 @@
+/*
+ * Copyright 2020-2023 Haiku, Inc. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ *		John Scipione, jscipione@gmail.com
+ */
+#ifndef _SHORTCUTS_H
+#define _SHORTCUTS_H
+
+
+#include <SupportDefs.h>
+
+
+class BMenu;
+class BMenuItem;
+
+
+namespace BPrivate {
+
+class Shortcuts {
+public:
+								Shortcuts();
+
+			BMenuItem*			AddPrinterItem();
+			const char*			AddPrinterLabel();
+
+			BMenuItem*			CleanupItem();
+			const char*			CleanupLabel();
+			int32				CleanupCommand();
+
+			BMenuItem*			CloseItem();
+			const char*			CloseLabel();
+			int32				CloseCommand();
+
+			BMenuItem*			CloseAllInWorkspaceItem();
+			const char*			CloseAllInWorkspaceLabel();
+
+			BMenuItem*			CopyItem();
+			const char*			CopyLabel();
+			int32				CopyCommand();
+
+			BMenuItem*			CopyToItem(BMenu* menu);
+			const char*			CopyToLabel();
+
+			BMenuItem*			CreateLinkItem(BMenu* menu);
+			const char*			CreateLinkLabel();
+			int32				CreateLinkCommand();
+
+			BMenuItem*			CreateLinkHereItem();
+			const char*			CreateLinkHereLabel();
+			int32				CreateLinkHereCommand();
+
+			BMenuItem*			CutItem();
+			const char*			CutLabel();
+			int32				CutCommand();
+
+			BMenuItem*			DeleteItem();
+			const char*			DeleteLabel();
+
+			BMenuItem*			DuplicateItem();
+			const char*			DuplicateLabel();
+
+			BMenuItem*			EditNameItem();
+			const char*			EditNameLabel();
+
+			BMenuItem*			EditQueryItem();
+			const char*			EditQueryLabel();
+
+			BMenuItem*			EmptyTrashItem();
+			const char*			EmptyTrashLabel();
+
+			BMenuItem*			FindItem();
+			const char*			FindLabel();
+
+			BMenuItem*			GetInfoItem();
+			const char*			GetInfoLabel();
+
+			BMenuItem*			IdentifyItem();
+			const char*			IdentifyLabel();
+
+			BMenuItem*			InvertSelectionItem();
+			const char*			InvertSelectionLabel();
+
+			BMenuItem*			MakeActivePrinterItem();
+			const char*			MakeActivePrinterLabel();
+
+			const char*			MountLabel();
+
+			BMenuItem*			MoveToItem(BMenu* menu);
+			const char*			MoveToLabel();
+
+			BMenuItem*			MoveToTrashItem();
+			const char*			MoveToTrashLabel();
+			int32				MoveToTrashCommand();
+
+			BMenuItem*			NewFolderItem();
+			const char*			NewFolderLabel();
+
+			BMenuItem*			OpenItem();
+			const char*			OpenLabel();
+
+			BMenuItem*			OpenParentItem();
+			const char*			OpenParentLabel();
+
+			BMenuItem*			OpenWithItem();
+			BMenuItem*			OpenWithItem(BMenu* menu);
+			const char*			OpenWithhLabel();
+
+			BMenuItem*			PasteItem();
+			const char*			PasteLabel();
+			int32				PasteCommand();
+
+			BMenuItem*			RestoreItem();
+			const char*			RestoreLabel();
+
+			BMenuItem*			ReverseOrderItem();
+			const char*			ReverseOrderLabel();
+
+			BMenuItem*			ResizeToFitItem();
+			const char*			ResizeToFitLabel();
+
+			BMenuItem*			SelectItem();
+			const char*			SelectLabel();
+
+			BMenuItem*			SelectAllItem();
+			const char*			SelectAllLabel();
+
+			BMenuItem*			UnmountItem();
+			const char*			UnmountLabel();
+
+			void				UpdateCleanupItem(BMenuItem* item);
+			void				UpdateCloseItem(BMenuItem* item);
+			void				UpdateCreateLinkItem(BMenuItem* item);
+			void				UpdateCreateLinkHereItem(BMenuItem* item);
+			void				UpdateCreateLinkInMenu(BMenu* menu);
+			void				UpdateCutItem(BMenuItem* item);
+			void				UpdateCopyItem(BMenuItem* item);
+			void				UpdateDeleteItem(BMenuItem* item);
+			void				UpdateIdentifyItem(BMenuItem* item);
+			void				UpdateMoveToTrashItem(BMenuItem* item);
+			void				UpdatePasteItem(BMenuItem* item);
+};
+
+}
+
+using namespace BPrivate;
+
+
+#endif	// _SHORTCUTS_H
