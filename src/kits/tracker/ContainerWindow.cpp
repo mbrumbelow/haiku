9978aedffebe85bb28b38d0b3c408dbaf020ec3b
John Scipione <jscipione@gmail.com>
2023-12-17 13:31:20 -0500

    Tracker: Add Cut/Copy/Paste items to context menus
    Remove CUT_COPY_PASTE_IN_CONTEXT_MENU define entirely.
    
    Built on top of Shortcuts refactor.
    
    Change-Id: I457ee140c2bde7e5ab750528b3196adfe9eb7018

--- src/kits/tracker/ContainerWindow.cpp
+++ src/kits/tracker/ContainerWindow.cpp
@@ -2622,14 +2622,12 @@ BContainerWindow::AddPoseContextMenu(BMenu* menu)
 	if (ShouldHaveMoveCopyMenus())
 		menu->AddSeparatorItem();
 
-#ifdef CUT_COPY_PASTE_IN_CONTEXT_MENU
 	if (!IsPrintersDir() && !IsRoot() && !IsTrash() && !InTrash()) {
 		menu->AddItem(Shortcuts()->CutItem());
 		menu->AddItem(Shortcuts()->CopyItem());
 		menu->AddItem(Shortcuts()->PasteItem());
 		menu->AddSeparatorItem();
 	}
-#endif
 
 	menu->AddItem(Shortcuts()->IdentifyItem());
 	menu->AddItem(Shortcuts()->AddOnsItem(
@@ -2674,13 +2672,11 @@ BContainerWindow::AddWindowContextMenu(BMenu* menu)
 		menu->AddSeparatorItem();
 	}
 
-#ifdef CUT_COPY_PASTE_IN_CONTEXT_MENU
 	if (!(IsPrintersDir() || IsVolume() || IsRoot()
 		|| IsTrash() || InTrash())) {
 		menu->AddItem(Shortcuts()->PasteItem());
 		menu->AddSeparatorItem();
 	}
-#endif
 
 	if (IsDesktop()) // "Clean up" on Desktop
 		menu->AddItem(Shortcuts()->CleanupItem());
