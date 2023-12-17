ea00a3362c892184420bac3a2cd018c68c8e9ecd
John Scipione <jscipione@gmail.com>
2023-12-17 13:31:20 -0500

    Tracker: Add Cut/Copy/Paste items to context menus
    Remove CUT_COPY_PASTE_IN_CONTEXT_MENU define entirely.
    
    Built on top of Shortcuts refactor.
    
    Change-Id: I457ee140c2bde7e5ab750528b3196adfe9eb7018

--- src/kits/tracker/FilePanelPriv.cpp
+++ src/kits/tracker/FilePanelPriv.cpp
@@ -999,9 +999,7 @@ TFilePanel::AddPoseContextMenu(BMenu* menu)
 
 	menu->AddItem(Shortcuts()->CutItem());
 	menu->AddItem(Shortcuts()->CopyItem());
-#ifdef CUT_COPY_PASTE_IN_CONTEXT_MENU
 	menu->AddItem(Shortcuts()->PasteItem());
-#endif
 }
 
 
@@ -1012,10 +1010,8 @@ TFilePanel::AddVolumeContextMenu(BMenu* menu)
 	menu->AddItem(Shortcuts()->GetInfoItem());
 	menu->AddItem(Shortcuts()->EditNameItem());
 
-#ifdef CUT_COPY_PASTE_IN_CONTEXT_MENU
 	menu->AddSeparatorItem();
 	menu->AddItem(Shortcuts()->PasteItem());
-#endif
 }
 
 
@@ -1025,10 +1021,8 @@ TFilePanel::AddWindowContextMenu(BMenu* menu)
 	menu->AddItem(Shortcuts()->NewFolderItem());
 	menu->AddItem(new BSeparatorItem());
 
-#ifdef CUT_COPY_PASTE_IN_CONTEXT_MENU
 	menu->AddItem(Shortcuts()->PasteItem());
 	menu->AddSeparatorItem();
-#endif
 
 	menu->AddItem(Shortcuts()->SelectItem());
 	menu->AddItem(Shortcuts()->SelectAllItem());
