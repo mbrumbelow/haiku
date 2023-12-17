be138ff99c3e06470ca260d224701a9261bfa036
John Scipione <jscipione@gmail.com>
2023-12-17 13:31:20 -0500

    Tracker: Add Cut/Copy/Paste items to context menus
    Remove CUT_COPY_PASTE_IN_CONTEXT_MENU define entirely.
    
    Built on top of Shortcuts refactor.
    
    Change-Id: I457ee140c2bde7e5ab750528b3196adfe9eb7018

--- src/kits/tracker/ContainerWindow.cpp
+++ src/kits/tracker/ContainerWindow.cpp
@@ -2565,7 +2565,6 @@ BContainerWindow::AddPoseContextMenu(BMenu* menu)
 	// The "Move To", "Copy To", "Create Link" menus are inserted here,
 	// have a look at UpdateMenu() and SetupMoveCopyMenus().
 
-#ifdef CUT_COPY_PASTE_IN_CONTEXT_MENU
 	if (!TargetModel()->IsPrintersDir() && !TargetModel()->IsRoot() && !TargetModel()->IsTrash()
 		&& !TargetModel()->InTrash()) {
 		menu->AddItem(Shortcuts()->CutItem());
@@ -2573,7 +2572,6 @@ BContainerWindow::AddPoseContextMenu(BMenu* menu)
 		menu->AddItem(Shortcuts()->PasteItem());
 		menu->AddSeparatorItem();
 	}
-#endif
 
 	menu->AddItem(Shortcuts()->IdentifyItem());
 	menu->AddItem(new BMenuItem(new BMenu(Shortcuts()->AddOnsLabel())));
@@ -2617,13 +2615,11 @@ BContainerWindow::AddWindowContextMenu(BMenu* menu)
 		menu->AddSeparatorItem();
 	}
 
-#ifdef CUT_COPY_PASTE_IN_CONTEXT_MENU
 	if (!(TargetModel()->IsPrintersDir() || TargetModel()->IsVolume() || TargetModel()->IsRoot()
 			|| TargetModel()->IsTrash() || TargetModel()->InTrash())) {
 		menu->AddItem(Shortcuts()->PasteItem());
 		menu->AddSeparatorItem();
 	}
-#endif
 
 	if (TargetModel()->IsDesktop()) // "Clean up" on Desktop
 		menu->AddItem(Shortcuts()->CleanupItem());
