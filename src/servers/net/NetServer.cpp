3cdab34de71d6723ed7b5aa67f951626580c4e8e
Sayan Chaudhuri <sayanchaudhuri758@gmail.com>
2023-12-31 16:42:12 +0530

    network : Fix auto-config flags for ipv4 and ipv6
      This commit fixes the issue where either of ipv4 or ipv6
      was able to set the auto-config flag for the interface.In
      that case, if one is static and other one is auto-configured,
      it changed the status of the other static ip also
      to auto-config.
      We have disabled auto-config for ipv6 as of now both
      through networkstatus app as well as ifconfig.It will be
      enabled once we have auto-config mechanism for ipv6 in
      place.
      The latest patch takes care of the condition where setting
      static ip for ipv6 for the first time  affects the ipv4
      config mode in networkstatus app.
      Address family check for ipv6 auto-config moved inside
      NetServer::_ConfigureInterface.
      Added condition where during system bootup, if no
      settings exist inside "interfaces" file for ipv4,
      system will go for dhcpv4
      Anytime, "interfaces" file is modified, config
      for both ipv4 and ipv6 will be written into it
    Change-Id: Ie3c1b7a3ff97472bd1a94566102fea1c5997a69f

--- src/servers/net/NetServer.cpp
+++ src/servers/net/NetServer.cpp
@@ -466,8 +466,12 @@ NetServer::_ConfigureInterface(BMessage& message)
 		BNetworkInterfaceAddressSettings addressSettings(addressMessage);
 
 		if (addressSettings.IsAutoConfigure()) {
-			_QuitLooperForDevice(name);
-			startAutoConfig = true;
+			if(addressSettings.Family() == AF_INET) {
+				_QuitLooperForDevice(name);
+				startAutoConfig = true;
+			 } else {
+				continue;
+			}
 		}
 
 		// set address/mask/broadcast/peer
@@ -541,6 +545,16 @@ NetServer::_ConfigureInterface(BMessage& message)
 					strerror(status));
 			}
 		}
+
+		if (startAutoConfig) {
+			// start auto configuration
+			syslog(LOG_DEBUG,"Going for DHCP");
+			AutoconfigLooper* looper = new AutoconfigLooper(this, name);
+			looper->Run();
+
+			fDeviceMap[name] = looper;
+		} else if (!autoConfigured)
+			_QuitLooperForDevice(name);
 	}
 
 	// Join the specified networks
@@ -564,14 +578,7 @@ NetServer::_ConfigureInterface(BMessage& message)
 		}
 	}
 
-	if (startAutoConfig) {
-		// start auto configuration
-		AutoconfigLooper* looper = new AutoconfigLooper(this, name);
-		looper->Run();
-
-		fDeviceMap[name] = looper;
-	} else if (!autoConfigured)
-		_QuitLooperForDevice(name);
+	
 
 	return B_OK;
 }
@@ -762,9 +769,9 @@ NetServer::_ConfigureInterfacesFromSettings(BStringList& devicesSet,
 				continue;
 			}
 		}
-
-		if (_ConfigureInterface(interface) == B_OK)
+		if (_ConfigureInterface(interface) == B_OK) {
 			devicesSet.Add(device);
+		}
 	}
 }
 
