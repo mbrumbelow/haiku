b1d25c62b13abe98f349e3e453275b5a58120d70
Sayan Chaudhuri <sayanchaudhuri758@gmail.com>
2024-02-11 11:43:11 +0530

    network : Fix ping command for remote host
    When ping command is issued for a remote host but the host is
    unreachable, it is not printing the icmp error message
    sent by the destination router
    This patch fixes trac ticket 12310
    Change-Id: If9451a98c78a9c22df571f49d3ffd851469e19a9

--- src/add-ons/kernel/network/protocols/icmp/icmp.cpp
+++ src/add-ons/kernel/network/protocols/icmp/icmp.cpp
@@ -16,6 +16,7 @@
 
 #include <algorithm>
 #include <netinet/in.h>
+
 #include <new>
 #include <stdlib.h>
 #include <string.h>
@@ -31,7 +32,30 @@
 
 #include "ipv4.h"
 
+struct icmp_header {
+	uint8_t type;
+	uint8_t code;
+	uint16_t checksum;
+	union {
+		struct {
+			uint16_t id;
+			uint16_t sequence;
+		} echo;
+		struct {
+			in_addr_t gateway;
+		} redirect;
+		struct {
+			uint16_t _reserved;
+			uint16_t next_mtu;
+		} path_mtu;
+		struct {
+			uint8_t pointer;
+			uint8_t _reserved[3];
+		} parameter_problem;
 
+		uint32_t zero;
+	};
+};
 //#define TRACE_ICMP
 #ifdef TRACE_ICMP
 #	define TRACE(x...) dprintf(x)
@@ -39,31 +63,6 @@
 #	define TRACE(x...) ;
 #endif
 
-
-struct icmp_header {
-	uint8	type;
-	uint8	code;
-	uint16	checksum;
-	union {
-		struct {
-			uint16	id;
-			uint16	sequence;
-		} echo;
-		struct {
-			in_addr_t gateway;
-		} redirect;
-		struct {
-			uint16	_reserved;
-			uint16	next_mtu;
-		} path_mtu;
-		struct {
-			uint8	pointer;
-			uint8	_reserved[3];
-		} parameter_problem;
-
-		uint32 zero;
-	};
-};
 
 typedef NetBufferField<uint16, offsetof(icmp_header, checksum)>
 	ICMPChecksumField;
