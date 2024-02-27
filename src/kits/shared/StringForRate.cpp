1d6332a617cab9a62f31e44565bfe4afb84fdcbb
Emir SARI <bitigchi@me.com>
2024-02-28 01:06:15 +0300

    ActivityMonitor: use BNumberFormat for i18n
    Change-Id: I3179f84cbaee25624c2f4a7b092a28b5281a5f16

--- src/kits/shared/StringForRate.cpp
+++ src/kits/shared/StringForRate.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2018, Haiku Inc. All rights reserved.
+ * Copyright 2012-2024, Haiku Inc. All rights reserved.
  * Distributed under the terms of the MIT License.
  */
 
@@ -7,6 +7,7 @@
 
 #include <stdio.h>
 
+#include <NumberFormat.h>
 #include <StringFormat.h>
 #include <SystemCatalog.h>
 
@@ -23,42 +24,101 @@ namespace BPrivate {
 const char*
 string_for_rate(double rate, char* string, size_t stringSize)
 {
+	BNumberFormat numberFormat;
+	BString printedUsage;
+	BString trKey;
+
 	double kib = rate / 1024.0;
+	double mib = kib / 1024.0;
+	double gib = mib / 1024.0;
+	double tib = gib / 1024.0;
+	status_t precision;
+	status_t status;
+
 	if (kib < 1.0) {
 		BString tmp;
-		BStringFormat format(
-			gSystemCatalog.GetString(B_TRANSLATE_MARK(
-			"{0, plural, one{# byte/s} other{# bytes/s}}"),
-			B_TRANSLATION_CONTEXT, "bytes per second"));
-		format.Format(tmp, (int)rate);
+		BStringFormat format(gSystemCatalog.GetString(
+			B_TRANSLATE_MARK("{0, plural, one{# byte/s} other{# bytes/s}}"),
+			B_TRANSLATION_CONTEXT,
+			"bytes per second"));
 
-		strlcpy(string, tmp.String(), stringSize);
+		status = numberFormat.Format(printedUsage, (int)rate);
+
+		if (status == B_OK) {
+			format.Format(printedUsage, (int)rate);
+			strlcpy(string, printedUsage.String(), stringSize);
+		} else {
+			format.Format(tmp, (int)rate);
+			strlcpy(string, tmp.String(), stringSize);
+		}
+
 		return string;
 	}
-	double mib = kib / 1024.0;
+
 	if (mib < 1.0) {
-		const char* trKey = B_TRANSLATE_MARK("%3.2f KiB/s");
-		snprintf(string, stringSize, gSystemCatalog.GetString(trKey,
-			B_TRANSLATION_CONTEXT, "KiB per second"), kib);
+		precision = numberFormat.SetPrecision(2);
+		status = numberFormat.Format(printedUsage, kib);
+
+		if ((status == B_OK) && (precision == B_OK)) {
+			trKey = B_TRANSLATE_MARK("%s KiB/s");
+			snprintf(string, stringSize, gSystemCatalog.GetString(trKey, B_TRANSLATION_CONTEXT,
+				"KiB per second"), printedUsage.String());
+		} else {
+			trKey = B_TRANSLATE_MARK("%3.2f KiB/s");
+			snprintf(string, stringSize, gSystemCatalog.GetString(trKey, B_TRANSLATION_CONTEXT,
+				"KiB per second"), kib);
+		}
+
 		return string;
 	}
-	double gib = mib / 1024.0;
+
 	if (gib < 1.0) {
-		const char* trKey = B_TRANSLATE_MARK("%3.2f MiB/s");
-		snprintf(string, stringSize, gSystemCatalog.GetString(trKey,
-			B_TRANSLATION_CONTEXT, "MiB per second"), mib);
+		precision = numberFormat.SetPrecision(2);
+		status = numberFormat.Format(printedUsage, mib);
+
+		if ((status == B_OK) && (precision == B_OK)) {
+			trKey = B_TRANSLATE_MARK("%s MiB/s");
+			snprintf(string, stringSize, gSystemCatalog.GetString(trKey, B_TRANSLATION_CONTEXT,
+				"MiB per second"), printedUsage.String());
+		} else {
+			trKey = B_TRANSLATE_MARK("%3.2f MiB/s");
+			snprintf(string, stringSize, gSystemCatalog.GetString(trKey, B_TRANSLATION_CONTEXT,
+				"MiB per second"), mib);
+		}
+
 		return string;
 	}
-	double tib = gib / 1024.0;
+
 	if (tib < 1.0) {
-		const char* trKey = B_TRANSLATE_MARK("%3.2f GiB/s");
-		snprintf(string, stringSize, gSystemCatalog.GetString(trKey,
-			B_TRANSLATION_CONTEXT, "GiB per second"), gib);
+		precision = numberFormat.SetPrecision(2);
+		status = numberFormat.Format(printedUsage, gib);
+
+		if ((status == B_OK) && (precision == B_OK)) {
+			trKey = B_TRANSLATE_MARK("%s GiB/s");
+			snprintf(string, stringSize, gSystemCatalog.GetString(trKey, B_TRANSLATION_CONTEXT,
+				"GiB per second"), printedUsage.String());
+		} else {
+			trKey = B_TRANSLATE_MARK("%3.2f GiB/s");
+			snprintf(string, stringSize, gSystemCatalog.GetString(trKey, B_TRANSLATION_CONTEXT,
+				"GiB per second"), gib);
+		}
+
 		return string;
 	}
-	const char* trKey = B_TRANSLATE_MARK("%.2f TiB/s");
-	snprintf(string, stringSize, gSystemCatalog.GetString(trKey,
-		B_TRANSLATION_CONTEXT, "TiB per second"), tib);
+
+	precision = numberFormat.SetPrecision(3);
+	status = numberFormat.Format(printedUsage, tib);
+
+	if ((status == B_OK) && (precision == B_OK)) {
+		trKey = B_TRANSLATE_MARK("%s TiB/s");
+		snprintf(string, stringSize, gSystemCatalog.GetString(trKey, B_TRANSLATION_CONTEXT,
+			"TiB per second"), printedUsage.String());
+	} else {
+		trKey = B_TRANSLATE_MARK("%.3f TiB/s");
+		snprintf(string, stringSize, gSystemCatalog.GetString(trKey, B_TRANSLATION_CONTEXT,
+			"TiB per second"), tib);
+	}
+
 	return string;
 }
 
