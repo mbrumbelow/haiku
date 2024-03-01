4ec91d825548571b6e6d19b6b5a2169d1f895f23
Máximo Castañeda <antiswen@yahoo.es>
2024-03-01 22:10:09 +0100

    BFont: remove default values from ambiguous method
    Noticed by Joachim Mairböck after I hastily merged change 7402.
    
    Change-Id: I76de22181dc39559956c25987165be2f8a8c4a37

--- headers/os/interface/Font.h
+++ headers/os/interface/Font.h
@@ -287,7 +287,7 @@ public:
 			status_t			LoadFont(const area_id fontAreaID,
 									size_t size = 0, size_t offset = 0);
 			status_t			LoadFont(const area_id fontAreaID,
-									size_t size = 0, size_t offset = 0, uint16 index = 0);
+									size_t size, size_t offset, uint16 index);
 			status_t			UnloadFont();
 
 private:
