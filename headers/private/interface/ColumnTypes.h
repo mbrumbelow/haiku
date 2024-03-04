44154d6711938dde26085f91e52e8a5c794bd367
Emir SARI <bitigchi@me.com>
2024-03-04 21:32:43 +0300

    BColumnListView: use automatic formatting for i18n
    Use BDateTimeFormat, BDateFormat, and BNumberFormat for
    various date, time, decimal, and percent formatting.
    
    Change-Id: I994563d207db8bd28342ca6543b69a356ced38af

--- headers/private/interface/ColumnTypes.h
+++ headers/private/interface/ColumnTypes.h
@@ -16,9 +16,12 @@
 
 #include "ColumnListView.h"
 
-#include <String.h>
-#include <Font.h>
 #include <Bitmap.h>
+#include <DateFormat.h>
+#include <DateTimeFormat.h>
+#include <Font.h>
+#include <NumberFormat.h>
+#include <String.h>
 
 
 //=====================================================================
@@ -39,6 +42,7 @@ public:
 			float		FontHeight() const;
 
 	virtual float		GetPreferredWidth(BField* field, BView* parent) const;
+			BNumberFormat	fNumberFormat;
 
 private:
 			float		fFontHeight;
@@ -122,6 +126,8 @@ public:
 	virtual	int					CompareFields(BField* field1, BField* field2);
 
 private:
+			BDateTimeFormat	fDateTimeFormat;
+			BDateFormat		fDateFormat;
 			BString				fTitle;
 };
 
