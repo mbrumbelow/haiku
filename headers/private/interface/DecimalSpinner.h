f7222f32e904912ee290c4d43271c909591eccf2
Emir SARI <bitigchi@me.com>
2024-04-03 00:47:38 +0300

    DecimalSpinner: Use BNumberFormat for formatting
    Change-Id: Idb45e98a61a0f16893e20aa02b10d7b2e1705454

--- headers/private/interface/DecimalSpinner.h
+++ headers/private/interface/DecimalSpinner.h
@@ -9,6 +9,7 @@
 #define _DECIMAL_SPINNER_H
 
 
+#include <NumberFormat.h>
 #include <Spinner.h>
 
 
@@ -37,7 +38,7 @@ public:
 	virtual	void				SetEnabled(bool enable);
 
 			uint32				Precision() const { return fPrecision; };
-	virtual	void				SetPrecision(uint32 precision) { fPrecision = precision; };
+	virtual	void				SetPrecision(uint32 precision);
 
 			double				MinValue() const { return fMinValue; }
 	virtual	void				SetMinValue(double min);
@@ -87,9 +88,14 @@ private:
 			double				fStep;
 			double				fValue;
 			uint32				fPrecision;
+			BNumberFormat*			fNumberFormat;
 
-	// FBC padding
-			uint32				_reserved[20];
+			// FBC padding
+#ifdef B_HAIKU_64_BIT
+			uint32				_reserved[18];
+#else
+			uint32				_reserved[19];
+#endif
 };
 
 
