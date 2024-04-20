5f2f798cbeddcc48689fa81ac066d05739b269a3
Zardshard <0azrune6@zard.anonaddy.com>
2024-04-20 15:39:50 -0400

    HVIFTranslator: Support exporting hvif as rdef
    This functions the same as Icon-O-Matic's export as rdef feature.
    
    Co-authored-by: Alexander von Gluck IV <kallisti5@unixzen.com>
    Change-Id: Ie61de4d42c5b322ecb075fef326315c697f6ce62

--- src/add-ons/translators/hvif/HVIFTranslator.cpp
+++ src/add-ons/translators/hvif/HVIFTranslator.cpp
@@ -1,9 +1,11 @@
 /*
- * Copyright 2009, Haiku, Inc.
+ * Copyright 2009-2024, Haiku, Inc., All rights reserved.
  * Distributed under the terms of the MIT license.
  *
  * Authors:
  *		Michael Lotz, mmlr@mlotz.ch
+ *		Alexander von Gluck, alex@terarocket.io
+ *		Zardshard
  */
 
 #include "HVIFTranslator.h"
@@ -13,12 +15,16 @@
 #include <BitmapStream.h>
 #include <Catalog.h>
 #include <IconUtils.h>
+#include <Node.h>
+#include <NodeInfo.h>
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define HVIF_FORMAT_CODE				'HVIF'
+#define HVIF_RDEF_FORMAT_CODE			'HRDF'
+
 #define HVIF_TRANSLATION_QUALITY		1.0
 #define HVIF_TRANSLATION_CAPABILITY		1.0
 
@@ -46,7 +52,15 @@ static const translation_format sOutputFormats[] = {
 		0.4,
 		"image/x-be-bitmap",
 		"Be Bitmap format (HVIFTranslator)"
-	}
+	},
+	{
+		HVIF_RDEF_FORMAT_CODE,
+		B_TRANSLATOR_TEXT,
+		0.6,
+		0.6,
+		"text/x-vnd.Be.ResourceDef",
+		"Resource definition file with an array containing the hvif data (HVIFTranslator)"
+	},
 };
 
 
@@ -88,12 +102,13 @@ HVIFTranslator::~HVIFTranslator()
 
 
 status_t
-HVIFTranslator::DerivedIdentify(BPositionIO *inSource,
+HVIFTranslator::Identify(BPositionIO *inSource,
 	const translation_format *inFormat, BMessage *ioExtension,
 	translator_info *outInfo, uint32 outType)
 {
 	// TODO: we do the fully work twice!
-	if (outType != B_TRANSLATOR_BITMAP)
+	if (outType != B_TRANSLATOR_BITMAP && outType != B_TRANSLATOR_TEXT
+			&& outType != HVIF_RDEF_FORMAT_CODE)
 		return B_NO_TRANSLATOR;
 
 	// filter out invalid sizes
@@ -131,11 +146,12 @@ HVIFTranslator::DerivedIdentify(BPositionIO *inSource,
 
 
 status_t
-HVIFTranslator::DerivedTranslate(BPositionIO *inSource,
+HVIFTranslator::Translate(BPositionIO *inSource,
 	const translator_info *inInfo, BMessage *ioExtension, uint32 outType,
-	BPositionIO *outDestination, int32 baseType)
+	BPositionIO *outDestination)
 {
-	if (outType != B_TRANSLATOR_BITMAP)
+	if (outType != B_TRANSLATOR_BITMAP && outType != B_TRANSLATOR_TEXT
+			&& outType != HVIF_RDEF_FORMAT_CODE)
 		return B_NO_TRANSLATOR;
 
 	// filter out invalid sizes
@@ -152,26 +168,92 @@ HVIFTranslator::DerivedTranslate(BPositionIO *inSource,
 		return B_NO_TRANSLATOR;
 	}
 
-	int32 renderSize = fSettings->SetGetInt32(HVIF_SETTING_RENDER_SIZE);
-	if (renderSize <= 0 || renderSize > 1024)
-		renderSize = 64;
+	switch (outType) {
+		case HVIF_RDEF_FORMAT_CODE:
+		case B_TRANSLATOR_TEXT:
+		{
+			// TODO: It would be nice to reuse the code from RDefExporter::
+			// _Export. This would likely involve decoupling it and the rest of
+			// the import_export directory from Icon-O-Matic and moving it into
+			// the icon library. Then, the icon library can be linked into this
+			// executable and the function can be used directly.
 
-	BBitmap rendered(BRect(0, 0, renderSize - 1, renderSize - 1),
-		B_BITMAP_NO_SERVER_LINK, B_RGBA32);
-	if (BIconUtils::GetVectorIcon(buffer, size, &rendered) != B_OK) {
-		free(buffer);
-		return B_NO_TRANSLATOR;
+			char outBuffer[2048];
+			sprintf(outBuffer, "\nresource(<your resource id here>) #'VICN' array {\n");
+			outDestination->Write(outBuffer, strlen(outBuffer));
+
+			const uint8* b = buffer;
+
+			// print one line (32 values)
+			while (size >= 32) {
+				sprintf(outBuffer,
+					"	$\"%.2X%.2X%.2X%.2X"
+					"%.2X%.2X%.2X%.2X"
+					"%.2X%.2X%.2X%.2X"
+					"%.2X%.2X%.2X%.2X"
+					"%.2X%.2X%.2X%.2X"
+					"%.2X%.2X%.2X%.2X"
+					"%.2X%.2X%.2X%.2X"
+					"%.2X%.2X%.2X%.2X\"\n",
+					b[0], b[1], b[2], b[3],
+					b[4], b[5], b[6], b[7],
+					b[8], b[9], b[10], b[11],
+					b[12], b[13], b[14], b[15],
+					b[16], b[17], b[18], b[19],
+					b[20], b[21], b[22], b[23],
+					b[24], b[25], b[26], b[27],
+					b[28], b[29], b[30], b[31]);
+				outDestination->Write(outBuffer, strlen(outBuffer));
+
+				size -= 32;
+				b += 32;
+			}
+
+			// last line (up to 32 values)
+			if (size > 0) {
+				outDestination->Write("	$\"", 3);
+				for (int i = 0; i < size; i++) {
+					sprintf(outBuffer, "%.2X", b[i]);
+					outDestination->Write(outBuffer, strlen(outBuffer));
+				}
+				outDestination->Write("\"\n", 2);
+			}
+			outDestination->Write("};\n", 3);
+
+			// set mime type
+			BNode* outNode = dynamic_cast<BNode*>(outDestination);
+			if (outNode != NULL) {
+				BNodeInfo nodeInfo(outNode);
+				if (nodeInfo.InitCheck() == B_OK)
+					nodeInfo.SetType("text/x-vnd.Be.ResourceDef");
+			}
+
+			break;
+		}
+		case B_TRANSLATOR_BITMAP:
+			int32 renderSize = fSettings->SetGetInt32(HVIF_SETTING_RENDER_SIZE);
+			if (renderSize <= 0 || renderSize > 1024)
+				renderSize = 64;
+
+			BBitmap rendered(BRect(0, 0, renderSize - 1, renderSize - 1),
+				B_BITMAP_NO_SERVER_LINK, B_RGBA32);
+			if (BIconUtils::GetVectorIcon(buffer, size, &rendered) != B_OK) {
+				free(buffer);
+				return B_NO_TRANSLATOR;
+			}
+
+			BBitmapStream stream(&rendered);
+			stream.Seek(0, SEEK_SET);
+			ssize_t read = 0;
+
+			while ((read = stream.Read(buffer, size)) > 0)
+				outDestination->Write(buffer, read);
+
+			BBitmap *dummy = NULL;
+			stream.DetachBitmap(&dummy);
+			break;
 	}
 
-	BBitmapStream stream(&rendered);
-	stream.Seek(0, SEEK_SET);
-	ssize_t read = 0;
-
-	while ((read = stream.Read(buffer, size)) > 0)
-		outDestination->Write(buffer, read);
-
-	BBitmap *dummy = NULL;
-	stream.DetachBitmap(&dummy);
 	free(buffer);
 	return B_OK;
 }
--- src/add-ons/translators/hvif/HVIFTranslator.h
+++ src/add-ons/translators/hvif/HVIFTranslator.h
@@ -10,22 +10,28 @@
 
 #include "BaseTranslator.h"
 
-#define HVIF_TRANSLATOR_VERSION		B_TRANSLATION_MAKE_VERSION(1, 0, 0)
+#define HVIF_TRANSLATOR_VERSION		B_TRANSLATION_MAKE_VERSION(1, 1, 0)
 #define HVIF_SETTING_RENDER_SIZE	"hvif /renderSize"
 
 class HVIFTranslator : public BaseTranslator {
 public:
 							HVIFTranslator();
 
-virtual	status_t			DerivedIdentify(BPositionIO *inSource,
+virtual	status_t			Identify(BPositionIO *inSource,
 								const translation_format *inFormat,
 								BMessage *ioExtension, translator_info *outInfo,
 								uint32 outType);
+			// we override Identify instead of DerivedIdentify since the
+			// default Identify makes the faulty assumption that images
+			// can never be converted to text (we do), and does nothing else
+			// useful for us.
 
-virtual	status_t			DerivedTranslate(BPositionIO *inSource,
+virtual	status_t			Translate(BPositionIO *inSource,
 								const translator_info *inInfo,
 								BMessage *ioExtension, uint32 outType,
-								BPositionIO *outDestination, int32 baseType);
+								BPositionIO *outDestination);
+			// we override Translate instead of DerivedTranslate for the same
+			// reason as given above
 
 virtual	BView *				NewConfigView(TranslatorSettings *settings);
 
