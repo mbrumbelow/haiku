6a6942fa294bd829646f999f4ff410d952c8b190
Chris Roberts <cpr420@gmail.com>
2024-05-05 22:32:48 -0600

    syslog_daemon: add syslog_max_backups option
    This driver setting in ~/config/settings/kernel/drivers/kernel allows
    setting an arbitrary number of syslog backups to keep around.  The
    backups are now named with a numeric suffix instead of being called
    "syslog.bak".  The backup files are rotated so that syslog.1 is the
    newest backup, then syslog.2, and so on.
    
    Change-Id: I7e3d01caa34680553b161ceb27ab8c3b2eaf508c

--- src/servers/syslog_daemon/syslog_output.cpp
+++ src/servers/syslog_daemon/syslog_output.cpp
@@ -34,6 +34,7 @@ static char sLastMessage[1024];
 static thread_id sLastThread;
 static int32 sRepeatCount;
 static size_t sLogMaxSize = 524288;	// 512kB
+static int32 sMaxBackups = 1;
 static bool sLogTimeStamps = false;
 
 
@@ -71,12 +72,23 @@ prepare_output()
 
 		// move old file if it already exists
 		if (tooLarge) {
-			BPath oldlog(base);
-			oldlog.Append("syslog.old");
+			// remove latest syslog.X and rename others with a suffix incremented by one
+			// syslog.6 -> syslog.7, syslog.5 -> syslog.6, ...
+			for (int x = sMaxBackups; x >= 0; x--) {
+				BString oldlog(syslog.Path());
+				// no suffix on 0, just 'syslog'
+				if (x > 0)
+					oldlog << "." << x;
 
-			remove(oldlog.Path());
-			rename(syslog.Path(), oldlog.Path());
-
+				if (x == sMaxBackups)
+					remove(oldlog.String());
+				else {
+					// increment our suffix
+					BString rotateto(syslog.Path());
+					rotateto << "." << (x + 1);
+					rename(oldlog.String(), rotateto.String());
+				}
+			}
 			// ToDo: just remove old file if space on device is tight?
 		}
 
@@ -213,9 +225,14 @@ init_syslog_output(SyslogDaemon *daemon)
 	// get kernel syslog settings
 	handle = load_driver_settings("kernel");
 	if (handle != NULL) {
+		const char *param = get_driver_parameter(handle,
+			"syslog_max_backups", "1", "1");
+		sMaxBackups = strtol(param, NULL, 0);
+		if (sMaxBackups < 0)
+			sMaxBackups = 0;
 		sLogTimeStamps = get_driver_boolean_parameter(handle,
 			"syslog_time_stamps", false, false);
-		const char *param = get_driver_parameter(handle,
+		param = get_driver_parameter(handle,
 			"syslog_max_size", "0", "0");
 		int maxSize = strtol(param, NULL, 0);
 		if (strchr(param, 'k') || strchr(param, 'K'))
