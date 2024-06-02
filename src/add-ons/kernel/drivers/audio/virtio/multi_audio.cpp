c9a188ebf4f65dfee63991cc63baa0add4d58eea
Diego Roux <diegoroux04@protonmail.com>
2024-06-02 07:30:52 +0100

    virtio_sound: minimal playback functionality driver.
    * Playback working (disabled on high bitrates).
    * Recording disabled due to high bitrates.
    * Cleaning code style and maintaing proper code size.
    
    Change-Id: I4ec291dd7eff3a70f0fa6cfa6a34d89055c3a568
    Signed-off-by: Diego Roux <diegoroux04@protonmail.com>

---
+++ src/add-ons/kernel/drivers/audio/virtio/multi_audio.cpp
@@ -0,0 +1,753 @@
+/*
+ *  Copyright 2024, Diego Roux, diegoroux04 at proton dot me
+ *  Distributed under the terms of the MIT License.
+ */
+
+#include <hmulti_audio.h>
+#include <KernelExport.h>
+#include <kernel.h>
+
+#include <string.h>
+
+#include "virtio_sound.h"
+#include "driver.h"
+
+#define VIRTIO_MULTI_CONTROL_FIRST_ID	1024
+#define VIRTIO_MULTI_CONTROL_MASTER_ID	0
+
+
+static VirtIOSoundPCMInfo*
+get_stream(VirtIOSoundDriverInfo* info, uint8 direction)
+{
+	for (uint32 i = 0; i < info->nStreams; i++) {
+		VirtIOSoundPCMInfo* stream = &info->streams[i];
+
+		if (stream->direction == direction)
+			return stream;
+	}
+
+	return NULL;
+}
+
+
+static void
+create_multi_channel_info(VirtIOSoundDriverInfo* info, multi_channel_info* channels)
+{
+	uint32 index = 0;
+
+	for (uint32 i = 0; i < 2; i++) {
+		VirtIOSoundPCMInfo* stream = get_stream(info, i);
+		if (stream == NULL)
+			continue;
+
+		for (uint8 j = 0; j < stream->channels; j++) {
+			channels[index].channel_id = index;
+
+			channels[index].kind = (stream->direction == VIRTIO_SND_D_OUTPUT)
+				? B_MULTI_OUTPUT_CHANNEL : B_MULTI_INPUT_CHANNEL;
+
+			channels[index].designations = stream->chmap[j];
+
+			if (stream->channels == 2) {
+				channels[index].designations |= B_CHANNEL_STEREO_BUS;
+			} else if (stream->channels > 2) {
+				channels[index].designations |= B_CHANNEL_SURROUND_BUS;
+			}
+
+			channels[index].connectors = 0x00;
+
+			index++;
+		}
+	}
+
+	for (uint32 i = 0; i < 2; i++) {
+		VirtIOSoundPCMInfo* stream = get_stream(info, i);
+		if (stream == NULL)
+			continue;
+
+		channels[index].channel_id = index;
+		channels[index].kind = (stream->direction == VIRTIO_SND_D_OUTPUT)
+			? B_MULTI_OUTPUT_BUS : B_MULTI_INPUT_BUS;
+		channels[index].designations = B_CHANNEL_LEFT | B_CHANNEL_STEREO_BUS;
+		channels[index].connectors = B_CHANNEL_MINI_JACK_STEREO;
+		index++;
+
+		channels[index].channel_id = index;
+		channels[index].kind = (stream->direction == VIRTIO_SND_D_OUTPUT)
+			? B_MULTI_OUTPUT_BUS : B_MULTI_INPUT_BUS;
+		channels[index].designations = B_CHANNEL_RIGHT | B_CHANNEL_STEREO_BUS;
+		channels[index].connectors = B_CHANNEL_MINI_JACK_STEREO;
+		index++;
+	}
+
+	return;
+}
+
+
+static status_t
+get_description(VirtIOSoundDriverInfo* info, multi_description* desc)
+{
+	desc->interface_version = B_CURRENT_INTERFACE_VERSION;
+	desc->interface_minimum = B_CURRENT_INTERFACE_VERSION;
+
+	strcpy(desc->friendly_name, "Virtio Sound Device");
+	strcpy(desc->vendor_info, "Haiku");
+
+	desc->input_channel_count = 0;
+	desc->output_channel_count = 0;
+
+	desc->output_bus_channel_count = 0;
+	desc->input_bus_channel_count = 0;
+	desc->aux_bus_channel_count = 0;
+
+	desc->interface_flags = 0x00;
+
+	for (uint32 i = 0; i < 2; i++) {
+		VirtIOSoundPCMInfo* stream = get_stream(info, i);
+		if (stream == NULL)
+			continue;
+
+		switch (i) {
+			case VIRTIO_SND_D_OUTPUT:
+				desc->output_channel_count = stream->channels;
+				desc->output_bus_channel_count = 2;
+				
+				desc->output_rates = stream->rates;
+				desc->output_formats = stream->formats;
+				
+				desc->interface_flags |= B_MULTI_INTERFACE_PLAYBACK;
+				break;
+			case VIRTIO_SND_D_INPUT:
+				desc->input_channel_count = stream->channels;
+				desc->input_bus_channel_count = 2;
+				
+				desc->input_rates = stream->rates;
+				desc->input_formats = stream->formats;
+				
+				desc->interface_flags |= B_MULTI_INTERFACE_RECORD;
+				break;
+		}
+	}
+
+	int32 channels = desc->output_channel_count + desc->input_channel_count
+		+ desc->output_bus_channel_count + desc->input_bus_channel_count;
+
+	if (desc->request_channel_count >= channels) {
+		create_multi_channel_info(info, desc->channels);
+	}
+
+	desc->max_cvsr_rate = 384000;
+	desc->min_cvsr_rate = 0;
+
+	desc->lock_sources = B_MULTI_LOCK_INTERNAL;
+	desc->timecode_sources = 0;
+
+	desc->start_latency = 30000;
+
+	strcpy(desc->control_panel, "");
+
+	return B_OK;
+}
+
+
+static status_t
+get_enabled_channels(VirtIOSoundDriverInfo* info, multi_channel_enable* data)
+{
+	uint32 channels = 0;
+
+	for (uint32 i = 0; i < 2; i++) {
+		VirtIOSoundPCMInfo* stream = get_stream(info, i);
+		if (stream == NULL)
+			continue;
+
+		channels += stream->channels;
+	}
+
+	for (uint32 i = 0; i < channels; i++)
+		B_SET_CHANNEL(data->enable_bits, i, true);
+
+	data->lock_source = B_MULTI_LOCK_INTERNAL;
+
+	return B_OK;
+}
+
+
+static status_t
+get_global_format(VirtIOSoundDriverInfo* info, multi_format_info* data)
+{
+	memset((void*)data, 0x00, sizeof(multi_format_info));
+
+	data->info_size = sizeof(multi_format_info);
+
+	data->output_latency = 80;
+	data->input_latency = 80;
+
+	for (uint32 i = 0; i < 2; i++) {
+		VirtIOSoundPCMInfo* stream = get_stream(info, i);
+		if (stream == NULL)
+			continue;
+
+		_multi_format* reply;
+		switch (i) {
+			case VIRTIO_SND_D_OUTPUT:
+				reply = &data->output;
+				break;
+			case VIRTIO_SND_D_INPUT:
+				reply = &data->input;
+				break;
+		}
+
+		reply->format = stream->format;
+		reply->rate = stream->rate;
+		reply->cvsr = stream->cvsr;
+	}
+
+	return B_OK;
+}
+
+
+static uint8
+format_to_size(uint32 format)
+{
+	switch (format) {
+		case B_FMT_8BIT_S:
+		case B_FMT_8BIT_U:
+			return 1;
+		case B_FMT_16BIT:
+			return 2;
+		case B_FMT_20BIT:
+		case B_FMT_24BIT:
+		case B_FMT_32BIT:
+		case B_FMT_FLOAT:
+			return 4;
+		case B_FMT_DOUBLE:
+			return 8;
+		default:
+			return 0;
+	}
+}
+
+
+static uint32
+to_cvsr(uint32 rate)
+{
+	switch (rate) {
+		case B_SR_8000:
+			return 8000;
+		case B_SR_11025:
+			return 11025;
+		case B_SR_16000:
+			return 16000;
+		case B_SR_22050:
+			return 22050;
+		case B_SR_32000:
+			return 32000;
+		case B_SR_44100:
+			return 44100;
+		case B_SR_48000:
+			return 48000;
+		case B_SR_64000:
+			return 64000;
+		case B_SR_88200:
+			return 88200;
+		case B_SR_96000:
+			return 96000;
+		case B_SR_176400:
+			return 176400;
+		case B_SR_192000:
+			return 192000;
+		case B_SR_384000:
+			return 384000;
+		default:
+			return 0;
+	}
+}
+
+
+static const char*
+to_format_string(uint32 format)
+{
+	switch (format) {
+		case B_FMT_8BIT_S:
+			return "8-bit signed";
+		case B_FMT_8BIT_U:
+			return "8-bit unsigned";
+		case B_FMT_16BIT:
+			return "16-bit";
+		case B_FMT_20BIT:
+			return "20-bit";
+		case B_FMT_24BIT:
+			return "24-bit";
+		case B_FMT_32BIT:
+			return "32-bit";
+		case B_FMT_FLOAT:
+			return "float";
+		case B_FMT_DOUBLE:
+			return "double";
+		default:
+			return "unknown";
+	}
+}
+
+
+static status_t
+set_global_format(VirtIOSoundDriverInfo* info, multi_format_info* data)
+{
+	for (uint32 i = 0; i < 2; i++) {
+		VirtIOSoundPCMInfo* stream = get_stream(info, i);
+		if (stream == NULL)
+			continue;
+
+		_multi_format* request;
+		switch (i) {
+			case VIRTIO_SND_D_OUTPUT:
+				request = &data->output;
+				break;
+			case VIRTIO_SND_D_INPUT:
+				request = &data->input;
+				break;
+		}
+
+		if (!(stream->formats & request->format)) {
+			ERROR("unsupported format requested (%u)\n", request->format);
+			return B_BAD_VALUE;
+		}
+
+		if (!(stream->rates & request->rate)) {
+			ERROR("unsupported rate requested (%u)\n", request->rate);
+			return B_BAD_VALUE;
+		}
+
+		stream->format = request->format;
+		stream->rate = request->rate;
+		stream->cvsr = to_cvsr(request->rate);
+
+		stream->buffer_size = 1024 * 20;
+
+		stream->period_size = stream->channels * format_to_size(stream->format)
+			* stream->buffer_size;
+
+		status_t status;
+
+		if (stream->current_state == VIRTIO_SND_STATE_START) {
+			status = VirtIOSoundPCMStop(info, stream);
+
+			if (status != B_OK) {
+				ERROR("unable to stop stream [id: %u]\n", stream->stream_id);
+				return status;
+			}
+		}
+
+		if (stream->current_state == VIRTIO_SND_STATE_STOP) {
+			status = VirtIOSoundPCMRelease(info, stream);
+
+			if (status != B_OK) {
+				ERROR("unable to release stream [id: %u]\n", stream->stream_id);
+				return status;
+			}
+		}
+
+		status = VirtIOSoundPCMSetParams(info, stream, stream->period_size, stream->period_size);
+		if (status != B_OK) {
+			ERROR("set params failed (%s)\n", strerror(status));
+			return status;
+		}
+
+		LOG("%s stream [id: %u] set to %s format and %u Hz rate\n",
+			(stream->direction == VIRTIO_SND_D_OUTPUT) ? "output" : "input",
+			stream->stream_id, to_format_string(stream->format), stream->cvsr);
+	}
+
+	return B_OK;
+}
+
+
+static status_t
+list_mix_channels(VirtIOSoundDriverInfo* info, multi_mix_channel_info* data)
+{
+	return B_ERROR;
+}
+
+
+static status_t
+list_mix_controls(VirtIOSoundDriverInfo* info, multi_mix_control_info* data)
+{
+	uint8 idx = 0;
+	for (uint32 i = 0; i < 2; i++) {
+		VirtIOSoundPCMInfo* stream = get_stream(info, i);
+		if (stream == NULL)
+			continue;
+
+		multi_mix_control* controls = &data->controls[idx];
+
+		controls->id = VIRTIO_MULTI_CONTROL_FIRST_ID + idx;
+		controls->parent = 0;
+		controls->flags = B_MULTI_MIX_GROUP;
+		controls->master = VIRTIO_MULTI_CONTROL_MASTER_ID;
+		controls->string = S_null;
+		
+		switch (i) {
+			case VIRTIO_SND_D_OUTPUT:
+				strcpy(controls->name, "Playback");
+				break;
+			case VIRTIO_SND_D_INPUT:
+				strcpy(controls->name, "Record");
+				break;
+		}
+
+		idx++;
+	}
+
+	data->control_count = 2;
+
+	return B_OK;
+}
+
+
+static status_t
+list_mix_connections(VirtIOSoundDriverInfo* info, multi_mix_connection_info* data)
+{
+	return B_ERROR;
+}
+
+
+static status_t
+get_mix(VirtIOSoundDriverInfo* info, multi_mix_value_info* data)
+{
+	return B_ERROR;
+}
+
+
+static status_t
+set_mix(VirtIOSoundDriverInfo* info, multi_mix_value_info* data)
+{
+	return B_ERROR;
+}
+
+
+static status_t
+get_buffers(VirtIOSoundDriverInfo* info, multi_buffer_list* data)
+{
+	data->flags = 0x00;
+
+	for (uint32 i = 0; i < 2; i++) {
+		VirtIOSoundPCMInfo* stream = get_stream(info, i);
+		if (stream == NULL)
+			continue;
+
+		buffer_desc** buffers;
+		status_t status;
+		char* buf_ptr;
+
+		switch (i) {
+			case VIRTIO_SND_D_OUTPUT: {
+				status = VirtIOSoundTXQueueInit(info, stream);
+
+				data->flags |= B_MULTI_BUFFER_PLAYBACK;
+
+				data->return_playback_buffers = BUFFERS;
+				data->return_playback_channels = stream->channels;
+				data->return_playback_buffer_size = stream->buffer_size;
+
+				buffers = data->playback_buffers;
+
+				buf_ptr = (char*)info->txBuf;
+				break;
+			}
+			case VIRTIO_SND_D_INPUT: {
+				status = VirtIOSoundRXQueueInit(info, stream);
+
+				data->flags |= B_MULTI_BUFFER_RECORD;
+
+				data->return_record_buffers = BUFFERS;
+				data->return_record_channels = stream->channels;
+				data->return_record_buffer_size = stream->buffer_size;
+
+				buffers = data->record_buffers;
+
+				buf_ptr = (char*)info->rxBuf;
+				break;
+			}
+		}
+
+		if (status != B_OK)
+			return status;
+
+		uint32 format_size = format_to_size(stream->format);
+
+		for (uint32 buf_id = 0; buf_id < BUFFERS; buf_id++) {
+			if (!IS_USER_ADDRESS(buffers[buf_id]))
+				return B_BAD_ADDRESS;
+
+			struct buffer_desc buf_desc[stream->channels];
+
+			for (uint32 ch_id = 0; ch_id < stream->channels; ch_id++) {
+				buf_desc[ch_id].base = buf_ptr + (format_size * ch_id);
+				buf_desc[ch_id].stride = format_size * stream->channels;
+			}
+
+			status = user_memcpy(buffers[buf_id], buf_desc,
+				sizeof(struct buffer_desc) * stream->channels);
+
+			if (status < B_OK)
+				return B_BAD_ADDRESS;
+
+			buf_ptr += stream->period_size;
+		}
+
+		status = VirtIOSoundPCMPrepare(info, stream);
+		if (status != B_OK) {
+			ERROR("failed to prepare stream_%u (%s)\n", stream->stream_id, strerror(status));
+			return status;
+		}
+	}
+
+	return B_OK;
+}
+
+
+static status_t
+start_stream(VirtIOSoundDriverInfo* info, VirtIOSoundPCMInfo* stream)
+{
+	stream->buffer_cycle = 0;
+	stream->real_time = 0;
+	stream->frames_count = 0;
+
+	status_t status;
+	if ((void*)stream->xferBuf == NULL) {
+		stream->xferArea = create_area("virtio_snd xfer", (void**)&stream->xferBuf,
+			B_ANY_KERNEL_BLOCK_ADDRESS, B_PAGE_SIZE, B_FULL_LOCK,
+			B_KERNEL_READ_AREA | B_KERNEL_WRITE_AREA);
+
+		status = stream->xferArea;
+		if (status < 0) {
+			ERROR("unable to create xfer area (%s)\n", strerror(status));
+			return status;
+		}
+
+		physical_entry entry;
+		status = get_memory_map((void*)stream->xferBuf, B_PAGE_SIZE, &entry, 1);
+		if (status != B_OK) {
+			ERROR("unable to get xfer memory map (%s)\n", strerror(status));
+			goto err1;
+		}
+
+		stream->xferAddr = entry.address;
+
+		for (uint32 i = 0; i < BUFFERS; i++) {
+			struct virtio_snd_pcm_xfer* xfer = (struct virtio_snd_pcm_xfer*)(stream->xferBuf
+				+ (sizeof(struct virtio_snd_pcm_xfer) + sizeof(struct virtio_snd_pcm_status)) * i);
+
+			xfer->stream_id = stream->stream_id;
+		}
+	}
+
+	status = VirtIOSoundPCMStart(info, stream);
+	if (status != B_OK) {
+		ERROR("unable to start stream_%u (%s)\n", stream->stream_id, strerror(status));
+		goto err1;
+	}
+
+	LOG("stream [id: %u] started %s\n", stream->stream_id,
+		(stream->direction == VIRTIO_SND_D_OUTPUT) ? "playback" : "recording");
+
+	return B_OK;
+
+err1:
+	delete_area(stream->xferArea);
+	return status;
+}
+
+
+static status_t
+stream_buffer_exchange(VirtIOSoundDriverInfo* info, VirtIOSoundPCMInfo* stream)
+{
+	snooze_until(stream->real_time + 1000000L / stream->cvsr * stream->buffer_size,
+		CLOCK_REALTIME);
+
+	size_t writtenVectorCount = 0;
+	size_t readVectorCount = 0;
+	::virtio_queue queue = 0;
+	addr_t baseAddr = 0;
+
+	switch (stream->direction) {
+		case VIRTIO_SND_D_OUTPUT: {
+			baseAddr = info->txAddr;
+			queue = info->txQueue;
+
+			writtenVectorCount = 1;
+			readVectorCount = 2;
+			break;
+		}
+		case VIRTIO_SND_D_INPUT: {
+			baseAddr = info->rxAddr;
+			queue = info->rxQueue;
+
+			writtenVectorCount = 2;
+			readVectorCount = 1;
+			break;
+		}
+	}
+
+	if (!info->virtio->queue_is_empty(queue)) {
+		DEBUG("%s", "queue is not empty\n");
+		return B_ERROR;
+	}
+
+	physical_entry entries[3];
+
+	entries[0].address = stream->xferAddr + (sizeof(struct virtio_snd_pcm_xfer) +
+		sizeof(struct virtio_snd_pcm_status)) * stream->buffer_cycle;
+	entries[0].size = sizeof(struct virtio_snd_pcm_xfer);
+
+	entries[1].address = baseAddr + stream->period_size * stream->buffer_cycle;
+	entries[1].size = stream->period_size;
+
+	entries[2].address = entries[0].address + sizeof(struct virtio_snd_pcm_xfer);
+	entries[2].size = sizeof(struct virtio_snd_pcm_status);
+
+	status_t status = info->virtio->queue_request_v(queue, entries,
+		readVectorCount, writtenVectorCount, NULL);
+
+	if (status != B_OK) {
+		DEBUG("%s", "enqueue request failed\n");
+		return status;
+	}
+
+	while (!info->virtio->queue_dequeue(queue, NULL, NULL));
+
+	struct virtio_snd_pcm_status* hdr = (struct virtio_snd_pcm_status*)(stream->xferBuf
+		+ (sizeof(struct virtio_snd_pcm_xfer) + sizeof(struct virtio_snd_pcm_status))
+		* stream->buffer_cycle + sizeof(struct virtio_snd_pcm_xfer));
+
+	if (hdr->status != VIRTIO_SND_S_OK) {
+		ERROR("device (stream_%u) returned error signal on buffer exchange\n", stream->stream_id);
+		return B_ERROR;
+	}
+
+	stream->real_time = system_time();
+	stream->frames_count += stream->buffer_size;
+	stream->buffer_cycle = (stream->buffer_cycle + 1) % BUFFERS;
+
+	return B_OK;
+}
+
+
+static status_t
+buffer_exchange(VirtIOSoundDriverInfo* info, multi_buffer_info* data)
+{
+	VirtIOSoundPCMInfo* pStream = get_stream(info, VIRTIO_SND_D_OUTPUT);
+	VirtIOSoundPCMInfo* rStream = get_stream(info, VIRTIO_SND_D_INPUT);
+
+	if (pStream != NULL) {
+		if (pStream->current_state != VIRTIO_SND_STATE_START)
+			start_stream(info, pStream);
+	}
+
+	if (rStream != NULL) {
+		if (rStream->current_state != VIRTIO_SND_STATE_START)
+			start_stream(info, rStream);
+	}
+
+	if (!IS_USER_ADDRESS(data))
+		return B_BAD_ADDRESS;
+
+	multi_buffer_info buf_info;
+
+	status_t status = user_memcpy(&buf_info, data, sizeof(multi_buffer_info));
+	if (status < B_OK)
+		return B_BAD_ADDRESS;
+
+	buf_info.flags = 0x00;
+
+	if (pStream != NULL) {
+		acquire_sem(info->txSem);
+
+		status = stream_buffer_exchange(info, pStream);
+		if (status != B_OK)
+			return status;
+
+		buf_info.flags |= B_MULTI_BUFFER_PLAYBACK;
+
+		buf_info.playback_buffer_cycle = pStream->buffer_cycle;
+		buf_info.played_real_time = pStream->real_time;
+		buf_info.played_frames_count = pStream->frames_count;
+	}
+
+	if (rStream != NULL) {
+		acquire_sem(info->rxSem);
+
+		status = stream_buffer_exchange(info, rStream);
+		if (status != B_OK)
+			return status;
+
+		buf_info.flags |= B_MULTI_BUFFER_RECORD;
+
+		buf_info.record_buffer_cycle = rStream->buffer_cycle;
+		buf_info.recorded_real_time = rStream->real_time;
+		buf_info.recorded_frames_count = rStream->frames_count;
+	}
+
+	status = user_memcpy(data, &buf_info, sizeof(multi_buffer_info));
+	if (status < B_OK)
+		return B_BAD_ADDRESS;
+
+	return B_OK;
+}
+
+
+static status_t
+buffer_force_stop(VirtIOSoundDriverInfo* info)
+{
+	VirtIOSoundPCMInfo* pStream = get_stream(info, VIRTIO_SND_D_OUTPUT);
+	VirtIOSoundPCMInfo* rStream = get_stream(info, VIRTIO_SND_D_INPUT);
+
+	if (pStream != NULL) {
+		if (pStream->current_state == VIRTIO_SND_STATE_START) {
+			acquire_sem(info->txSem);
+
+			status_t status = VirtIOSoundPCMStop(info, pStream);
+
+			if (status != B_OK)
+				return status;
+
+			release_sem_etc(info->txSem, 1, B_DO_NOT_RESCHEDULE);
+		}
+	}
+
+	if (rStream != NULL) {
+		if (rStream->current_state == VIRTIO_SND_STATE_START) {
+			acquire_sem(info->rxSem);
+
+			status_t status = VirtIOSoundPCMStop(info, rStream);
+
+			if (status != B_OK)
+				return status;
+
+			release_sem_etc(info->rxSem, 1, B_DO_NOT_RESCHEDULE);
+		}
+	}
+
+	delete_area(info->txArea);
+	delete_area(info->rxArea);
+
+	info->txBuf = (addr_t)NULL;
+	info->rxBuf = (addr_t)NULL;
+
+	delete_sem(info->txSem);
+	delete_sem(info->rxSem);
+
+	return B_OK;
+}
+
+
+#define cookie_type VirtIOSoundDriverInfo
+#include "../generic/multi.c"
+
+status_t
+virtio_snd_ctrl(void* cookie, uint32 op, void* buffer, size_t length)
+{
+	VirtIOSoundDriverInfo* info = (VirtIOSoundDriverInfo*)cookie;
+
+	return multi_audio_control_generic(info, op, buffer, length);
+}
---
+++ src/add-ons/kernel/drivers/audio/virtio/snd_pcm.cpp
@@ -0,0 +1,386 @@
+/*
+ *  Copyright 2024, Diego Roux, diegoroux04 at proton dot me
+ *  Distributed under the terms of the MIT License.
+ */
+
+#include <virtio.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <hmulti_audio.h>
+
+#include "driver.h"
+#include "virtio_sound.h"
+
+
+#define	B_SR_NA		0x00
+#define B_FMT_NA	0x00
+
+/* 	High bitrates are currently disabled.
+ *	Buffer sizes can't maintain reasonable latency,
+ *	resulting in unbearable audio quality.
+ *	Until we fix it, they shall remain commented out. */
+
+static const uint32 supportedRates[] = {
+	B_SR_NA,		// VIRTIO_SND_PCM_RATE_5512
+	B_SR_8000, 		// VIRTIO_SND_PCM_RATE_8000
+	B_SR_11025, 	// VIRTIO_SND_PCM_RATE_11025
+	B_SR_16000,		// VIRTIO_SND_PCM_RATE_16000
+	B_SR_22050,		// VIRTIO_SND_PCM_RATE_22050
+	B_SR_32000,		// VIRTIO_SND_PCM_RATE_32000
+	B_SR_44100,		// VIRTIO_SND_PCM_RATE_44100
+	B_SR_48000,		// VIRTIO_SND_PCM_RATE_48000
+	B_SR_64000,		// VIRTIO_SND_PCM_RATE_64000
+	B_SR_88200,		// VIRTIO_SND_PCM_RATE_88200
+	B_SR_96000,		// VIRTIO_SND_PCM_RATE_96000
+	B_SR_NA,		// B_SR_176400,	// VIRTIO_SND_PCM_RATE_176400
+	B_SR_NA,		// B_SR_192000,	// VIRTIO_SND_PCM_RATE_192000
+	B_SR_NA,		// B_SR_384000,	// VIRTIO_SND_PCM_RATE_384000
+};
+
+
+static const uint32 supportedFormats[] = {
+	B_FMT_NA, 		// VIRTIO_SND_PCM_FMT_IMA_ADPCM
+	B_FMT_NA,		// VIRTIO_SND_PCM_FMT_MU_LAW
+	B_FMT_NA,		// VIRTIO_SND_PCM_FMT_A_LAW
+	B_FMT_8BIT_S,	// VIRTIO_SND_PCM_FMT_S8
+	B_FMT_8BIT_U,	// VIRTIO_SND_PCM_FMT_U8
+	B_FMT_16BIT,	// VIRTIO_SND_PCM_FMT_S16
+	B_FMT_NA,		// VIRTIO_SND_PCM_FMT_U16
+	B_FMT_NA,		// VIRTIO_SND_PCM_FMT_S18_3
+	B_FMT_NA,		// VIRTIO_SND_PCM_FMT_U18_3
+	B_FMT_NA,		// VIRTIO_SND_PCM_FMT_S20_3
+	B_FMT_NA,		// VIRTIO_SND_PCM_FMT_U20_3
+	B_FMT_NA,		// VIRTIO_SND_PCM_FMT_S24_3
+	B_FMT_NA,		// VIRTIO_SND_PCM_FMT_U24_3
+	B_FMT_20BIT,	// VIRTIO_SND_PCM_FMT_S20
+	B_FMT_NA,		// VIRTIO_SND_PCM_FMT_U20
+	B_FMT_24BIT,	// VIRTIO_SND_PCM_FMT_S24
+	B_FMT_NA,		// VIRTIO_SND_PCM_FMT_U24
+	B_FMT_32BIT,	// VIRTIO_SND_PCM_FMT_S32
+	B_FMT_NA,		// VIRTIO_SND_PCM_FMT_U32
+	B_FMT_FLOAT, 	// VIRTIO_SND_PCM_FMT_FLOAT
+	B_FMT_DOUBLE,	// VIRTIO_SND_PCM_FMT_FLOAT64
+	B_FMT_NA,		// VIRTIO_SND_PCM_FMT_DSD_U8
+	B_FMT_NA,		// VIRTIO_SND_PCM_FMT_DSD_U16
+	B_FMT_NA,		// VIRTIO_SND_PCM_FMT_DSD_U32
+	B_FMT_NA,		// VIRTIO_SND_PCM_FMT_IEC958_SUBFRAME
+};
+
+
+static uint32
+rates_to_multiaudio(struct virtio_snd_pcm_info info)
+{
+	uint64 rate = (1 << VIRTIO_SND_PCM_RATE_384000);
+	uint8 i = VIRTIO_SND_PCM_RATE_384000;
+
+    uint64 rates = B_SR_NA;
+	while (rate != 0) {
+		if (info.rates & rate)
+			rates |= supportedRates[i];
+
+		rate = rate >> 1;
+		i--;
+	}
+
+	return rates;
+}
+
+
+static uint32
+fmts_to_multiaudio(struct virtio_snd_pcm_info info)
+{
+	uint64 format = (1 << VIRTIO_SND_PCM_FMT_FLOAT64);
+	uint8 i = VIRTIO_SND_PCM_FMT_FLOAT64;
+
+    uint64 fmts = B_FMT_NA;
+	while (format != 0) {
+		if (info.formats & format)
+			fmts |= supportedFormats[i];
+
+		format = format >> 1;
+		i--;
+	}
+
+	return fmts;
+}
+
+
+status_t
+VirtIOSoundQueryStreamInfo(VirtIOSoundDriverInfo* info)
+{
+	struct virtio_snd_pcm_info stream_info[info->nStreams];
+
+	status_t status = VirtIOSoundQueryInfo(info, VIRTIO_SND_R_PCM_INFO, 0,
+		info->nStreams, sizeof(struct virtio_snd_pcm_info), (void*)stream_info);
+
+	if (status != B_OK)
+		return status;
+
+	info->streams = (VirtIOSoundPCMInfo*)calloc(info->nStreams, sizeof(VirtIOSoundPCMInfo));
+	if (info->streams == NULL)
+		return B_NO_MEMORY;
+
+	uint32 index = 0;
+
+	for (uint32 i = 0; i < info->nStreams; i++) {
+		VirtIOSoundPCMInfo* stream = &info->streams[index++];
+
+		// Dynamic buffer sizes and higher latency settings
+		// broke down recording for most bitrates.
+		if (stream_info[i].direction == VIRTIO_SND_D_INPUT)
+			continue;
+
+		stream->stream_id = i;
+		stream->nid = stream_info[i].hdr.hda_fn_nid;
+
+		stream->features = stream_info[i].features;
+
+		stream->formats = fmts_to_multiaudio(stream_info[i]);
+		if (!stream->formats) {
+			ERROR("stream_%u: unsupported PCM formats (%u)\n", i, stream->formats);
+
+			status = B_ERROR;
+			goto err1;
+		}
+
+		stream->rates = rates_to_multiaudio(stream_info[i]);
+		if (!stream->rates) {
+			ERROR("stream_%u: unsupported PCM rates (%u)\n", i, stream->formats);
+
+			status = B_ERROR;
+			goto err1;
+		}
+
+		switch (stream_info[i].direction) {
+			case VIRTIO_SND_D_OUTPUT:
+				info->outputStreams++;
+				break;
+			case VIRTIO_SND_D_INPUT:
+				info->inputStreams++;
+				break;
+			default:
+				ERROR("unknown stream direction (%u)\n", stream_info[i].direction);
+				status = B_ERROR;
+				goto err1;
+		}
+
+		stream->direction = stream_info[i].direction;
+
+		if (stream_info[i].channels_min > stream_info[i].channels_max) {
+			ERROR("invalid channel range (%u-%u)\n", stream_info[i].channels_min,
+				stream_info[i].channels_max);
+
+			status = B_ERROR;
+			goto err1;
+		}
+
+		stream->channels_min = stream_info[i].channels_min;
+		stream->channels_max = stream_info[i].channels_max;
+	}
+
+	return B_OK;
+
+err1:
+	free(info->streams);
+	return status;
+}
+
+
+static uint8
+to_virtio_fmt(uint32 format)
+{
+	switch (format) {
+		case B_FMT_8BIT_S:
+			return VIRTIO_SND_PCM_FMT_S8;
+		case B_FMT_8BIT_U:
+			return VIRTIO_SND_PCM_FMT_U8;
+		case B_FMT_16BIT:
+			return VIRTIO_SND_PCM_FMT_S16;
+		case B_FMT_20BIT:
+			return VIRTIO_SND_PCM_FMT_S20;
+		case B_FMT_24BIT:
+			return VIRTIO_SND_PCM_FMT_S24;
+		case B_FMT_32BIT:
+			return VIRTIO_SND_PCM_FMT_S32;
+		case B_FMT_FLOAT:
+			return VIRTIO_SND_PCM_FMT_FLOAT;
+		case B_FMT_DOUBLE:
+			return VIRTIO_SND_PCM_FMT_FLOAT64;
+		default:
+			return B_FMT_NA;
+	}
+}
+
+
+static uint8
+to_virtio_rate(uint32 rate)
+{
+	switch (rate) {
+		case B_SR_8000:
+			return VIRTIO_SND_PCM_RATE_8000;
+		case B_SR_11025:
+			return VIRTIO_SND_PCM_RATE_11025;
+		case B_SR_16000:
+			return VIRTIO_SND_PCM_RATE_16000;
+		case B_SR_22050:
+			return VIRTIO_SND_PCM_RATE_22050;
+		case B_SR_32000:
+			return VIRTIO_SND_PCM_RATE_32000;
+		case B_SR_44100:
+			return VIRTIO_SND_PCM_RATE_44100;
+		case B_SR_48000:
+			return VIRTIO_SND_PCM_RATE_48000;
+		case B_SR_64000:
+			return VIRTIO_SND_PCM_RATE_64000;
+		case B_SR_88200:
+			return VIRTIO_SND_PCM_RATE_88200;
+		case B_SR_96000:
+			return VIRTIO_SND_PCM_RATE_96000;
+		case B_SR_176400:
+			return VIRTIO_SND_PCM_RATE_176400;
+		case B_SR_192000:
+			return VIRTIO_SND_PCM_RATE_192000;
+		case B_SR_384000:
+			return VIRTIO_SND_PCM_RATE_384000;
+		default:
+			return B_SR_NA;
+	}
+}
+
+
+status_t
+VirtIOSoundPCMSetParams(VirtIOSoundDriverInfo* info, VirtIOSoundPCMInfo* stream,
+	uint32 buffer, uint32 period)
+{
+	if ((stream->current_state != VIRTIO_SND_STATE_SET_PARAMETERS) &&
+		(stream->current_state != VIRTIO_SND_STATE_PREPARE) &&
+		(stream->current_state != VIRTIO_SND_STATE_RELEASE))
+		return B_NOT_ALLOWED;
+
+	struct virtio_snd_pcm_set_params data;
+
+	data.hdr.hdr.code = VIRTIO_SND_R_PCM_SET_PARAMS;
+	data.hdr.stream_id = stream->stream_id;
+
+	data.buffer_bytes = buffer;
+	data.period_bytes = period;
+
+	data.features = 0;
+
+	data.channels = stream->channels;
+	data.format = to_virtio_fmt(stream->format);
+	data.rate = to_virtio_rate(stream->rate);
+
+	data.padding = 0;
+
+	status_t status = VirtIOSoundPCMControlRequest(info, (void*)&data,
+		sizeof(struct virtio_snd_pcm_set_params));
+
+	if (status != B_OK) {
+		ERROR("PCM control request failed.\n");
+		return status;
+	}
+
+	stream->current_state = VIRTIO_SND_STATE_SET_PARAMETERS;
+
+	return B_OK;
+}
+
+
+status_t
+VirtIOSoundPCMPrepare(VirtIOSoundDriverInfo* info, VirtIOSoundPCMInfo* stream)
+{
+	if ((stream->current_state != VIRTIO_SND_STATE_SET_PARAMETERS) &&
+		(stream->current_state != VIRTIO_SND_STATE_PREPARE) &&
+		(stream->current_state != VIRTIO_SND_STATE_RELEASE))
+		return B_NOT_ALLOWED;
+
+	struct virtio_snd_pcm_prepare data;
+
+	data.hdr.hdr.code = VIRTIO_SND_R_PCM_PREPARE;
+	data.hdr.stream_id = stream->stream_id;
+
+	status_t status = VirtIOSoundPCMControlRequest(info, (void*)&data,
+		sizeof(struct virtio_snd_pcm_prepare));
+
+	if (status != B_OK)
+		return status;
+
+	stream->current_state = VIRTIO_SND_STATE_PREPARE;
+
+	return B_OK;
+}
+
+
+status_t
+VirtIOSoundPCMStart(VirtIOSoundDriverInfo* info, VirtIOSoundPCMInfo* stream)
+{
+	if ((stream->current_state != VIRTIO_SND_STATE_PREPARE) &&
+		(stream->current_state != VIRTIO_SND_STATE_STOP))
+		return B_NOT_ALLOWED;
+
+	struct virtio_snd_pcm_start data;
+
+	data.hdr.hdr.code = VIRTIO_SND_R_PCM_START;
+	data.hdr.stream_id = stream->stream_id;
+
+	status_t status = VirtIOSoundPCMControlRequest(info, (void*)&data,
+		sizeof(struct virtio_snd_pcm_prepare));
+
+	if (status != B_OK)
+		return status;
+
+	stream->current_state = VIRTIO_SND_STATE_START;
+
+	return B_OK;
+}
+
+
+status_t
+VirtIOSoundPCMStop(VirtIOSoundDriverInfo* info, VirtIOSoundPCMInfo* stream)
+{
+	if (stream->current_state != VIRTIO_SND_STATE_START)
+		return B_NOT_ALLOWED;
+
+	struct virtio_snd_pcm_start data;
+
+	data.hdr.hdr.code = VIRTIO_SND_R_PCM_STOP;
+	data.hdr.stream_id = stream->stream_id;
+
+	status_t status = VirtIOSoundPCMControlRequest(info, (void*)&data,
+		sizeof(struct virtio_snd_pcm_prepare));
+
+	if (status != B_OK)
+		return status;
+
+	stream->current_state = VIRTIO_SND_STATE_STOP;
+
+	return B_OK;
+}
+
+
+status_t
+VirtIOSoundPCMRelease(VirtIOSoundDriverInfo* info, VirtIOSoundPCMInfo* stream)
+{
+	if ((stream->current_state != VIRTIO_SND_STATE_PREPARE) &&
+		(stream->current_state != VIRTIO_SND_STATE_STOP))
+		return B_NOT_ALLOWED;
+
+	struct virtio_snd_pcm_start data;
+
+	data.hdr.hdr.code = VIRTIO_SND_R_PCM_RELEASE;
+	data.hdr.stream_id = stream->stream_id;
+
+	status_t status = VirtIOSoundPCMControlRequest(info, (void*)&data,
+		sizeof(struct virtio_snd_pcm_prepare));
+
+	if (status != B_OK)
+		return status;
+
+	stream->current_state = VIRTIO_SND_STATE_RELEASE;
+
+	return B_OK;
+}
