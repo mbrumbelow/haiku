c1f44f4ae91436243b86365055f60ee881cc8326
Abdelrahman Youssef <abdelrahmanyossef12@gmail.com>
2024-06-21 16:41:27 +0200

    Icon-O-Matic: restors default position when it starts out of screen
    fixes #18865
    
    Change-Id: If8c286c730b5660be6c20e159a9de6637af25f47

--- src/apps/icon-o-matic/IconEditorApp.cpp
+++ src/apps/icon-o-matic/IconEditorApp.cpp
@@ -23,6 +23,7 @@
 #include <Message.h>
 #include <Mime.h>
 #include <Path.h>
+#include <Screen.h>
 
 #include "support_settings.h"
 
@@ -291,6 +292,9 @@ IconEditorApp::_NewWindow()
 	MainWindow* window = new MainWindow(fLastWindowFrame, this,
 		&fLastWindowSettings);
 	fWindowCount++;
+
+	window->MoveOnScreen(B_MOVE_IF_PARTIALLY_OFFSCREEN);
+
 	return window;
 }
 
