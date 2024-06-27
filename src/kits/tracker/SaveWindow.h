65ab13fb205c137ab5ea49a5203b58ef7d870d83
calisto-mathias <calisto.mathias.25@gmail.com>
2024-06-27 13:48:24 +0000

    Tracker: MenuBar Integration
    This commit introduces enhancements to the Find Panel, focusing
    on improving user experience and functionality. The primary
    changes include:
    
    1. Menu Bar Integration
     - Added a Menu Bar and migrated the more-options section into this
       Menu Bar for a more organized and intuitive interface.
    
    2. Saving a Query
     - Save as Option: This allows users to save a query to any location
       on their storage drive. A save panel enables marking the saved
       query as a template or a query file.
    
     - Save Option: Treats Queries and templates as documents, enabling users
       to override changes to the file without opening the Save as Panel
       repeatedly. Context-sensitive activation of this option is also enabled.
    
    3. Opening a Query:
     - Users can open a query via the Find Panel, which brings up a File Panel for
       Selection, streamlining query/query template management.
    
    4. Templates Menu:
     - Provides a convenient way to select templates. Newly saved templates
       immediately updated in this menu.
    
    5. Options Menu:
     - Clear Templates: Deletes all the templates from the filesystem and
       refreshes the template menu.
    
     - Clear History: Deletes either all query files or only temporary query
       files from the filesystem.
    
    6. Improved User Flow For Saving Queries:
     - Queries are treated similarly to document files, containing properties
       specific to the search. Users can override and save details using the
       same file.
    
    7. History Menu:
     - The History menu has also been moved into the menu bar. Here it is
       segregated into temporary and saved query files. This is helpful to
       slowly shift the chain of thought to only show the saved queries in the
       history menu.
    
    Summing up, these changes aim to shift parts of the Find Panel into the
    Menu Bar, fitting it along the newer design. It also adds in the required
    features to treat queries and query template files as mutable documents
    which can be easily saved or retrieved.
    
    Change-Id: I9c13ddd77a7628a0440e59c57b6bb22dab6437c0

---
+++ src/kits/tracker/SaveWindow.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2004-2007, Haiku, Inc. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ *		Calisto Abel Mathias, calisto.mathias.25@gmail.com
+ */
+#ifndef _SAVE_WINDOW_H
+#define _SAVE_WINDOW_H
+
+#include <Window.h>
+
+
+class BButton;
+class BCheckBox;
+class BMessenger;
+class BTextControl;
+
+namespace BPrivate {
+
+const uint32 kOpenSaveQueryPanel = 'Fosv';
+const uint32 kCloseSaveQueryPanel = 'Fcsv';
+const uint32 kNameEdited ='FNmE';
+
+class BSaveWindow : public BWindow{
+public:
+								BSaveWindow(BMessenger*);
+	virtual						~BSaveWindow();
+	virtual	bool				QuitRequested();
+
+protected:
+	virtual	void				MessageReceived(BMessage*);
+
+private:
+			BTextControl*		fQueryName;
+			BCheckBox*			fIncludeInTemplates;
+			BCheckBox*			fSaveInDefaultDirectory;
+			BButton*			fButton;
+			BMessenger*			fMessenger;
+};
+
+} // namespace BPrivate
+
+using namespace BPrivate;
+
+#endif
