c29b77e9d8b7fef7e630ecddf95f86e307becf07
calisto-mathias <calisto.mathias.25@gmail.com>
2024-06-27 13:48:24 +0000

    Tracker: MenuBar Integration
    This commit introduces enhancements to the Find Panel, focusing
    on improving user experience and functionality. The primary
    changes include:
    
    1. Menu Bar Integration
     - Added a Menu Bar and migrated the more-options section into this
       Menu Bar for a more organized and intuitive interface.
    
    2. Saving a Query
     - Save as Option: This allows users to save a query to any location
       on their storage drive. A save panel enables marking the saved
       query as a template or a query file.
    
     - Save Option: Treats Queries and templates as documents, enabling users
       to override changes to the file without opening the Save as Panel
       repeatedly. Context-sensitive activation of this option is also enabled.
    
    3. Opening a Query:
     - Users can open a query via the Find Panel, which brings up a File Panel for
       Selection, streamlining query/query template management.
    
    4. Templates Menu:
     - Provides a convenient way to select templates. Newly saved templates
       immediately updated in this menu.
    
    5. Options Menu:
     - Clear Templates: Deletes all the templates from the filesystem and
       refreshes the template menu.
    
     - Clear History: Deletes either all query files or only temporary query
       files from the filesystem.
    
    6. Improved User Flow For Saving Queries:
     - Queries are treated similarly to document files, containing properties
       specific to the search. Users can override and save details using the
       same file.
    
    7. History Menu:
     - The History menu has also been moved into the menu bar. Here it is
       segregated into temporary and saved query files. This is helpful to
       slowly shift the chain of thought to only show the saved queries in the
       history menu.
    
    Summing up, these changes aim to shift parts of the Find Panel into the
    Menu Bar, fitting it along the newer design. It also adds in the required
    features to treat queries and query template files as mutable documents
    which can be easily saved or retrieved.
    
    Change-Id: I9c13ddd77a7628a0440e59c57b6bb22dab6437c0

---
+++ src/kits/tracker/SaveWindow.cpp
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2004-2007, Haiku, Inc. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ *
+ * Authors:
+ *		Calisto Abel Mathias, calisto.mathias.25@gmail.com
+ */
+
+#include "SaveWindow.h"
+
+#include <Button.h>
+#include <Catalog.h>
+#include <CheckBox.h>
+#include <LayoutBuilder.h>
+#include <Locale.h>
+#include <Messenger.h>
+#include <TextControl.h>
+
+
+#undef B_TRANSLATION_CONTEXT
+#define B_TRANSLATION_CONTEXT "Find Panel"
+
+
+BSaveWindow::BSaveWindow(BMessenger* receiver)
+	:
+	BWindow(BRect(), B_TRANSLATE("Save Query"), B_TITLED_WINDOW_LOOK, B_FLOATING_APP_WINDOW_FEEL,
+	B_NOT_ZOOMABLE | B_NOT_RESIZABLE | B_AUTO_UPDATE_SIZE_LIMITS),
+	fMessenger(new BMessenger(*receiver))
+{
+
+	// Initializing All Private Member Variables
+	fQueryName = new BTextControl("Query Name", B_TRANSLATE("Query Name"), "", NULL);
+	fIncludeInTemplates = new BCheckBox("Include in Templates",
+		B_TRANSLATE("Include in Templates"), NULL);
+	fSaveInDefaultDirectory = new BCheckBox("Save In Default Directory",
+		B_TRANSLATE("Save in Default Directory"), NULL);
+	fButton = new BButton(B_TRANSLATE("Save"), new BMessage(kOpenSaveQueryPanel));
+
+	// Setting Initial States
+	fQueryName->MakeFocus();
+	fQueryName->SetModificationMessage(new BMessage(kNameEdited));
+	fButton->SetEnabled(false);
+
+	// Setting Targets
+	fQueryName->SetTarget(this);
+	fButton->SetTarget(this);
+
+	// Laying Out All the Elements
+	BLayoutBuilder::Group<>(this, B_VERTICAL)
+		.SetInsets(B_USE_DEFAULT_SPACING)
+		.Add(fQueryName)
+		.AddGroup(B_HORIZONTAL)
+			.AddGroup(B_VERTICAL, 0.0f)
+				.Add(fIncludeInTemplates)
+				.Add(fSaveInDefaultDirectory)
+			.End()
+			.AddGlue()
+			.Add(fButton)
+		.End()
+	.End();
+
+	CenterOnScreen();
+
+	fQueryName->MakeFocus();
+	fButton->MakeDefault(true);
+}
+
+
+BSaveWindow::~BSaveWindow()
+{
+	delete fMessenger;
+}
+
+
+bool
+BSaveWindow::QuitRequested()
+{
+	return true;
+}
+
+
+void
+BSaveWindow::MessageReceived(BMessage* message)
+{
+	switch(message->what) {
+		case kNameEdited:
+		{
+			if (strcmp(fQueryName->Text(), ""))
+				fButton->SetEnabled(true);
+			else
+				fButton->SetEnabled(false);
+			break;
+		}
+
+		case kOpenSaveQueryPanel:
+		{
+			Hide();
+
+			const char* queryName = fQueryName->Text();
+			bool includeInTemplates = fIncludeInTemplates->Value() == B_CONTROL_ON;
+			bool saveInDefaultDirectory = fSaveInDefaultDirectory->Value() == B_CONTROL_ON;
+
+			BMessage* closeMessage = new BMessage(kCloseSaveQueryPanel);
+			closeMessage->AddString("Query Name", queryName);
+			closeMessage->AddBool("Include In Templates", includeInTemplates);
+			closeMessage->AddBool("Save In Default Directory", saveInDefaultDirectory);
+
+			fMessenger->SendMessage(closeMessage);
+			Quit();
+
+			break;
+		}
+	}
+}
