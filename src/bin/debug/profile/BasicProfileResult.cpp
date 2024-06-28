1fc56f52913e723a1decc94df2ba1011736fc423
Augustin Cavalier <waddlesplash@gmail.com>
2024-06-28 12:13:54 -0400

    profiler: Report CPU times and use them to compute "missed" ticks.
    This shows that the profiler is still pretty broken, because we
    are missing quite a lot of ticks on average. One run of
    "profile pkgman search" here produced an output with 66 total ticks
    and 423 (!) missed ticks. A brief run of WebPositive was not quite
    as bad (main thread: 1078 total ticks, 157 missed ticks.)

--- src/bin/debug/profile/BasicProfileResult.cpp
+++ src/bin/debug/profile/BasicProfileResult.cpp
@@ -119,9 +119,17 @@ BasicProfileResult::BasicProfileResult()
 	:
 	fTotalTicks(0),
 	fUnkownTicks(0),
+	fExpectedTicks(0),
 	fDroppedTicks(0),
 	fTotalSampleCount(0)
 {
+}
+
+
+void
+BasicProfileResult::AddExpectedTicks(int32 expected)
+{
+	fExpectedTicks += expected;
 }
 
 
@@ -172,6 +180,8 @@ BasicProfileResult::PrintResults(ImageProfileResultContainer* container)
 		std::sort(hitSymbols, hitSymbols + hitSymbolCount);
 
 	int64 totalTicks = fTotalTicks;
+	const int64 missedTicks = fExpectedTicks - fTotalTicks;
+
 	fprintf(gOptions.output, "\nprofiling results for %s \"%s\" "
 		"(%" B_PRId32 "):\n", fEntity->EntityType(), fEntity->EntityName(),
 		fEntity->EntityID());
@@ -182,6 +192,7 @@ BasicProfileResult::PrintResults(ImageProfileResultContainer* container)
 		totalTicks, totalTicks * fInterval);
 	if (totalTicks == 0)
 		totalTicks = 1;
+
 	fprintf(gOptions.output,
 		"  unknown ticks:  %" B_PRId64 " (%" B_PRId64 " us, %6.2f%%)\n",
 		fUnkownTicks, fUnkownTicks * fInterval,
@@ -190,6 +201,9 @@ BasicProfileResult::PrintResults(ImageProfileResultContainer* container)
 		"  dropped ticks:  %" B_PRId64 " (%" B_PRId64 " us, %6.2f%%)\n",
 		fDroppedTicks, fDroppedTicks * fInterval,
 		100.0 * fDroppedTicks / totalTicks);
+	fprintf(gOptions.output,
+		"  missed ticks:   %" B_PRId64 " (%" B_PRId64 " us)\n",
+		missedTicks, missedTicks * fInterval);
 	if (gOptions.analyze_full_stack) {
 		fprintf(gOptions.output, "  samples/tick:   %.1f\n",
 			(double)fTotalSampleCount / totalTicks);
