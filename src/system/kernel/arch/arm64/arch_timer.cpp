cac25247baca494b61425be58366db038601532a
Owen Anderson <resistor@mac.com>
2024-08-23 06:58:33 +0000

    arm64: Disable timer when not in use.
    * ARM documentation indicates this may save power.
    * Notably, the timer keeps ticking even when "disabled", but
      cannot be read by the core without temporarily "enabling"
      the timer.
    
    Change-Id: Iccff84915c611b43ee7a3c53ed2f8a3e426eda06

--- src/system/kernel/arch/arm64/arch_timer.cpp
+++ src/system/kernel/arch/arm64/arch_timer.cpp
@@ -15,8 +15,11 @@
 static uint64 sTimerTicksUS;
 static bigtime_t sTimerMaxInterval;
 
-#define TIMER_ARMED (1)
-#define TIMER_MASKED (2 | 1)
+#define TIMER_DISABLED (0)
+#define TIMER_ENABLE (1)
+#define TIMER_IMASK (2)
+#define TIMER_ISTATUS (4)
+
 #define TIMER_IRQ 27
 
 
@@ -27,21 +30,21 @@ arch_timer_set_hardware_timer(bigtime_t timeout)
 		timeout = sTimerMaxInterval;
 
 	WRITE_SPECIALREG(CNTV_TVAL_EL0, timeout * sTimerTicksUS);
-	WRITE_SPECIALREG(CNTV_CTL_EL0, TIMER_ARMED);
+	WRITE_SPECIALREG(CNTV_CTL_EL0, TIMER_ENABLE);
 }
 
 
 void
 arch_timer_clear_hardware_timer()
 {
-	WRITE_SPECIALREG(CNTV_CTL_EL0, TIMER_MASKED);
+	WRITE_SPECIALREG(CNTV_CTL_EL0, TIMER_DISABLED);
 }
 
 
 int32
 arch_timer_interrupt(void *data)
 {
-	WRITE_SPECIALREG(CNTV_CTL_EL0, TIMER_MASKED);
+	WRITE_SPECIALREG(CNTV_CTL_EL0, TIMER_DISABLED);
 	return timer_interrupt();
 }
 
@@ -52,7 +55,7 @@ arch_init_timer(kernel_args *args)
 	sTimerTicksUS = READ_SPECIALREG(CNTFRQ_EL0) / 1000000;
 	sTimerMaxInterval = INT32_MAX / sTimerTicksUS;
 
-	WRITE_SPECIALREG(CNTV_CTL_EL0, TIMER_MASKED);
+	WRITE_SPECIALREG(CNTV_CTL_EL0, TIMER_DISABLED);
 	install_io_interrupt_handler(TIMER_IRQ, &arch_timer_interrupt, NULL, 0);
 
 	return B_OK;
@@ -62,5 +65,17 @@ arch_init_timer(kernel_args *args)
 bigtime_t
 system_time(void)
 {
-	return READ_SPECIALREG(CNTVCT_EL0) / sTimerTicksUS;
+	uint64 ctl = READ_SPECIALREG(CNTV_CTL_EL0);
+
+	if (ctl & TIMER_ENABLE) {
+		return READ_SPECIALREG(CNTVCT_EL0) / sTimerTicksUS;
+	} else {
+		// The timer continues to tick while it is "disabled",
+		// but we have to temporarily enable it in order to
+		// read the value.
+		WRITE_SPECIALREG(CNTV_CTL_EL0, ctl | TIMER_ENABLE);
+		uint64 r = READ_SPECIALREG(CNTV_TVAL_EL0);
+		WRITE_SPECIALREG(CNTV_CTL_EL0, ctl);
+		return r;
+	}
 }
