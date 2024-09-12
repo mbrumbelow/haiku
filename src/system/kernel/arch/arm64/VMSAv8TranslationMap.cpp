bdf0fa093c703ecdba80d5ccfe2ef594d58ee650
Owen Anderson <resistor@mac.com>
2024-09-12 08:35:07 +0000

    arm64: Remove unnecessary TLB flush
    Change-Id: I4c1c68dffee4fe054100293410b8988f623a8e9d

--- src/system/kernel/arch/arm64/VMSAv8TranslationMap.cpp
+++ src/system/kernel/arch/arm64/VMSAv8TranslationMap.cpp
@@ -243,10 +243,6 @@ void
 VMSAv8TranslationMap::Unlock()
 {
 	TRACE("VMSAv8TranslationMap::Unlock()\n");
-	if (recursive_lock_get_recursion(&fLock) == 1) {
-		// we're about to release it for the last time
-		Flush();
-	}
 	recursive_lock_unlock(&fLock);
 }
 
@@ -880,7 +876,6 @@ VMSAv8TranslationMap::ClearAccessedAndModified(
 void
 VMSAv8TranslationMap::Flush()
 {
-	ThreadCPUPinner pinner(thread_get_current_thread());
-
-	arch_cpu_global_TLB_invalidate();
+	// Necessary invalidation is performed during mapping,
+	// no need to do anything more here.
 }
