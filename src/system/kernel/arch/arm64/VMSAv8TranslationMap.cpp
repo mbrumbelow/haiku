b4ee90d0052df0b9f5727b2e045c9fe503ce3223
Owen Anderson <resistor@mac.com>
2024-09-12 08:32:54 +0000

    arm64: Introduce SWDIRTY bit on page table entries.
    * This allows us to correctly track dirty pages through RW->RO
      remappings.
    
    Change-Id: I4309d5170ad66a175e884d1b28f31430adb7d67f

--- src/system/kernel/arch/arm64/VMSAv8TranslationMap.cpp
+++ src/system/kernel/arch/arm64/VMSAv8TranslationMap.cpp
@@ -78,6 +78,9 @@ alloc_first_free_asid(void)
 static bool
 is_pte_dirty(uint64_t pte)
 {
+	if (pte & kAttrSWDIRTY)
+		return true;
+
 	return !(pte & kAttrAPReadOnly);
 }
 
@@ -88,13 +91,14 @@ set_pte_dirty(uint64_t pte)
 	if (pte & kAttrSWDBM)
 		return pte & ~kAttrAPReadOnly;
 
-	return pte;
+	return pte | kAttrSWDIRTY;
 }
 
 
 static uint64_t
 set_pte_clean(uint64_t pte)
 {
+	pte &= ~kAttrSWDIRTY;
 	return pte | kAttrAPReadOnly;
 }
 
