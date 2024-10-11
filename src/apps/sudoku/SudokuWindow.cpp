a2367ba20238c9be883f8438e06076635515346c
Augustin Cavalier <waddlesplash@gmail.com>
2024-10-11 15:40:33 -0400

    Add MoveOnScreen() calls to a number of applications.
    Technically reverts 039f93ce5eeef9ff027f71ad6b69c9bc78c2b0fc,
    but just moves the call elsewhere.
    
    Should fix #19164.

--- src/apps/sudoku/SudokuWindow.cpp
+++ src/apps/sudoku/SudokuWindow.cpp
@@ -156,11 +156,10 @@ SudokuWindow::SudokuWindow()
 	if (settings.FindRect("window frame", &frame) == B_OK) {
 		MoveTo(frame.LeftTop());
 		ResizeTo(frame.Width(), frame.Height());
-		frame.OffsetTo(B_ORIGIN);
+		MoveOnScreen(B_MOVE_IF_PARTIALLY_OFFSCREEN);
 	} else {
 		float scaling = std::max(1.0f, be_plain_font->Size() / 12.0f);
 		ResizeTo(Frame().Width() * scaling, Frame().Height() * scaling);
-		frame = Bounds();
 	}
 
 	MoveOnScreen();
