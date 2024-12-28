0253287c8022b51d87c33e0dba581dda39ec21e5
X512 <danger_mail@list.ru>
2024-12-29 04:54:51 +0900

    add C23 checked integer arithmetic
    Change-Id: Ibca84debc3a4f2249b8b79bd5fa9d57371e75958

---
+++ headers/posix/stdckdint.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2024, Haiku, Inc. All rights reserved.
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef __STDC_VERSION_STDCKDINT_H__
+#define __STDC_VERSION_STDCKDINT_H__ 202311L
+
+
+#define ckd_add(res, a, b) __builtin_add_overflow((a), (b), (r))
+#define ckd_sub(res, a, b) __builtin_sub_overflow((a), (b), (r))
+#define ckd_mul(res, a, b) __builtin_mul_overflow((a), (b), (r))
+
+
+#endif	// __STDC_VERSION_STDCKDINT_H__
