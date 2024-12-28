81bd4da2226fdad33c7970fb169589e7ba25f157
Jenny Vermeltfoort <jennyvermeltfoort@outlook.com>
2024-12-28 15:49:18 +0000

    ShowImage: remove kMsgNextSlide wrap around.
    Change-Id: I00f49951ad3e272d92ed6ee12936c10c34004cb8

--- src/apps/showimage/ShowImageWindow.cpp
+++ src/apps/showimage/ShowImageWindow.cpp
@@ -907,12 +907,7 @@ ShowImageWindow::MessageReceived(BMessage* message)
 			break;
 
 		case MSG_FILE_NEXT:
-		case kMsgNextSlide:
-			if (_ClosePrompt()) {
-				if (!fNavigator.NextFile()) {
-					// Wrap back around
-					fNavigator.FirstFile();
-				}
+			if (_ClosePrompt() && fNavigator.NextFile()) {
 				_LoadImage();
 			}
 			break;
