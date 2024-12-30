9e0edad2f0dc097c187ee70dcce6eb3f24a360bb
Lt-Henry <quiqueiii@gmail.com>
2024-12-30 15:07:37 +0100

    busses/i2c: Added a Designware i2c bus manager
    This is adds supports for Designware i2c busses, available nowadays on
    almost any laptop and needed for touchpads to work.
    
    The driver is based (almost carbon-copy) from Jerome's PCH i2c one.
    I took it as a reference and then I realized that designware registers were
    almost equal to pch ones.
    
    Keep in mind that this is a topic I barely have experience on. I have access to almost all
    Slimbook laptops and I wanted to have touchpad working so I put my hands on it.
    
    The driver relies on ACPI, there is no PCI bus scan. Does it work?
    It does, but I have some mixed results:
    
    On one laptop, It seems to work, i2c cli finds avialable devices and i2c_hid detects the touchpad.
    However, movement on Y axis is erratic, and after a lot of debugging, it looks like an issue on
    the device itself, perhaps it needs some specific quirk.
    
    On the other laptop, the trackpad is properly detected by i2c_hid (sot at least, report transaction
    works), but it looks like no report is sent when I move the fingers. Antoher quirk to wake the
    device up? Who knows.
    
    Tbh, This is actually a bit complex for me and I could spend months trying to figure it out if
    I am properly settings i2c registers or not, so I hope once I did the heavy and boring work, someone
    of you may fix the bits missing.
    
    PS: I put myself on copyright header but I feel like Jerome should be there too, as I almost
    copy-paste it. Feel free to change it.
    
    Update: I've added some code to compute hcnt and lcnt values. We were trusting whatever default values were there but all drivers I've seen have this (or similar) functions as fallback. Perhaps this can be configurable in the future. Apart from this, there are some small clean ups and fixes but nothing special. At this moment, I got it working on two laptops, a third one the bus is detected, the report descriptor is fetched, but I always get the same input report. I have the feeling that we have a bug (or we need a quirk) on interrupt handling function, but I am not ready to fix it yet.
    
    I know there are style issues to fix, I'll do once we have clear how to proceed.
    
    Change-Id: Ia447d98af7231a9da7fa2f25e865e2c338b6eb67

---
+++ src/add-ons/kernel/busses/i2c/designware/dw_i2c_acpi.cpp
@@ -0,0 +1,261 @@
+/*
+ * Copyright 2024, Enrique Medina Gremaldos, quique@necos.es.
+ * Distributed under the terms of the MIT License.
+ */
+
+
+#include <new>
+#include <stdio.h>
+#include <string.h>
+
+#include <ACPI.h>
+#include <ByteOrder.h>
+#include <condition_variable.h>
+
+#include "dw_i2c.h"
+
+typedef struct {
+	const char *hid;
+	uint32 flags;
+} dw_device_info;
+
+dw_device_info device_list[] = {
+	{"INT33C2", 0},
+	{"INT33C3", 0},
+	{"INT3432", 0},
+	{"INT3433", 0},
+	{"80860F41", 0},
+	{"808622C1", 0},
+	{"AMD0010", 0},
+	{"AMDI0010", 0},
+	{"AMDI0510", 0},
+	{"APMC0D0F", 0},
+	{0, 0}
+};
+
+typedef struct {
+	dw_i2c_sim_info info;
+	acpi_device_module_info* acpi;
+	acpi_device device;
+
+} dw_i2c_acpi_sim_info;
+
+
+static status_t
+dw_i2c_acpi_set_powerstate(dw_i2c_acpi_sim_info* info, uint8 power)
+{
+	status_t status = info->acpi->evaluate_method(info->device,
+		power == 1 ? "_PS0" : "_PS3", NULL, NULL);
+	return status;
+}
+
+
+
+static acpi_status
+dw_i2c_scan_parse_callback(ACPI_RESOURCE *res, void *context)
+{
+	struct dw_i2c_crs* crs = (struct dw_i2c_crs*)context;
+
+	if (res->Type == ACPI_RESOURCE_TYPE_IRQ) {
+		crs->irq = res->Data.Irq.Interrupts[0];
+		crs->irq_triggering = res->Data.Irq.Triggering;
+		crs->irq_polarity = res->Data.Irq.Polarity;
+		crs->irq_shareable = res->Data.Irq.Shareable;
+	} else if (res->Type == ACPI_RESOURCE_TYPE_EXTENDED_IRQ) {
+		crs->irq = res->Data.ExtendedIrq.Interrupts[0];
+		crs->irq_triggering = res->Data.ExtendedIrq.Triggering;
+		crs->irq_polarity = res->Data.ExtendedIrq.Polarity;
+		crs->irq_shareable = res->Data.ExtendedIrq.Shareable;
+	} else if (res->Type == ACPI_RESOURCE_TYPE_FIXED_MEMORY32) {
+		crs->addr_bas = res->Data.FixedMemory32.Address;
+		crs->addr_len = res->Data.FixedMemory32.AddressLength;
+	}
+
+	return B_OK;
+}
+
+
+//	#pragma mark -
+
+
+static status_t
+acpi_scan_bus(i2c_bus_cookie cookie)
+{
+	CALLED();
+	dw_i2c_acpi_sim_info* bus = (dw_i2c_acpi_sim_info*)cookie;
+
+	bus->acpi->walk_namespace(bus->device, ACPI_TYPE_DEVICE, 1,
+		dw_i2c_scan_bus_callback, NULL, bus, NULL);
+
+	return B_OK;
+}
+
+
+static status_t
+register_child_devices(void* cookie)
+{
+	CALLED();
+
+	dw_i2c_acpi_sim_info* bus = (dw_i2c_acpi_sim_info*)cookie;
+	device_node* node = bus->info.driver_node;
+
+	char prettyName[32];
+	sprintf(prettyName, "DesignWare I2C Controller");
+
+	device_attr attrs[] = {
+		// properties of this controller for i2c bus manager
+		{ B_DEVICE_PRETTY_NAME, B_STRING_TYPE,
+			{ .string = prettyName }},
+		{ B_DEVICE_FIXED_CHILD, B_STRING_TYPE,
+			{ .string = I2C_FOR_CONTROLLER_MODULE_NAME }},
+
+		// private data to identify the device
+		{ NULL }
+	};
+
+	return gDeviceManager->register_node(node, DW_I2C_SIM_MODULE_NAME,
+		attrs, NULL, NULL);
+}
+
+
+static status_t
+init_device(device_node* node, void** device_cookie)
+{
+	CALLED();
+	status_t status = B_OK;
+
+	dw_i2c_acpi_sim_info* bus = (dw_i2c_acpi_sim_info*)calloc(1,
+		sizeof(dw_i2c_acpi_sim_info));
+	if (bus == NULL)
+		return B_NO_MEMORY;
+
+	acpi_device_module_info* acpi;
+	acpi_device device;
+	{
+		device_node* acpiParent = gDeviceManager->get_parent_node(node);
+		gDeviceManager->get_driver(acpiParent, (driver_module_info**)&acpi,
+			(void**)&device);
+		gDeviceManager->put_node(acpiParent);
+	}
+
+	bus->acpi = acpi;
+	bus->device = device;
+	bus->info.driver_node = node;
+	bus->info.scan_bus = acpi_scan_bus;
+
+	// Attach devices for I2C resources
+	struct dw_i2c_crs crs;
+	status = acpi->walk_resources(device, (ACPI_STRING)"_CRS",
+		dw_i2c_scan_parse_callback, &crs);
+	if (status != B_OK) {
+		ERROR("Error while getting I2C devices\n");
+		free(bus);
+		return status;
+	}
+	if (crs.addr_bas == 0 || crs.addr_len == 0) {
+		TRACE("skipping non configured I2C devices\n");
+		free(bus);
+		return B_BAD_VALUE;
+	}
+
+	bus->info.base_addr = crs.addr_bas;
+	bus->info.map_size = crs.addr_len;
+	bus->info.irq = crs.irq;
+
+	dw_i2c_acpi_set_powerstate(bus, 1);
+
+	*device_cookie = bus;
+	return B_OK;
+}
+
+
+static void
+uninit_device(void* device_cookie)
+{
+	dw_i2c_acpi_sim_info* bus = (dw_i2c_acpi_sim_info*)device_cookie;
+	free(bus);
+}
+
+
+static status_t
+register_device(device_node* parent)
+{
+	device_attr attrs[] = {
+		{B_DEVICE_PRETTY_NAME, B_STRING_TYPE, {.string = "DesignWare I2C ACPI"}},
+		{}
+	};
+
+	return gDeviceManager->register_node(parent,
+		DW_I2C_ACPI_DEVICE_MODULE_NAME, attrs, NULL, NULL);
+}
+
+
+static float
+supports_device(device_node* parent)
+{
+	CALLED();
+	const char* bus;
+
+	// make sure parent is a DesignWare I2C ACPI device node
+	if (gDeviceManager->get_attr_string(parent, B_DEVICE_BUS, &bus, false)
+		< B_OK) {
+		return -1;
+	}
+
+	if (strcmp(bus, "acpi") != 0)
+		return 0.0f;
+
+	TRACE("found an acpi node\n");
+
+	// check whether it's really a device
+	uint32 device_type;
+	if (gDeviceManager->get_attr_uint32(parent, ACPI_DEVICE_TYPE_ITEM,
+			&device_type, false) != B_OK
+		|| device_type != ACPI_TYPE_DEVICE) {
+		return 0.0;
+	}
+	TRACE("found an acpi device\n");
+
+	// check whether it's a DesignWare I2C device
+	const char *name;
+	if (gDeviceManager->get_attr_string(parent, ACPI_DEVICE_HID_ITEM, &name,
+		false) != B_OK) {
+		return 0.0;
+	}
+	TRACE("found an acpi device hid %s\n", name);
+
+	dw_device_info *device_info = device_list;
+
+	while (device_info && device_info->hid) {
+		if (strcmp(name, device_info->hid) == 0) {
+			TRACE("DesignWare I2C device found! name %s\n", name);
+			return 0.6f;
+		}
+
+		device_info++;
+	}
+
+	return 0.0f;
+}
+
+
+//	#pragma mark -
+
+
+driver_module_info gDwI2cAcpiDevice = {
+	{
+		DW_I2C_ACPI_DEVICE_MODULE_NAME,
+		0,
+		NULL
+	},
+
+	supports_device,
+	register_device,
+	init_device,
+	uninit_device,
+	register_child_devices,
+	NULL,	// rescan
+	NULL,	// device removed
+};
+
+
---
+++ src/add-ons/kernel/busses/i2c/designware/dw_i2c_hardware.h
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2024, Enrique Medina Gremaldos, quique@necos.es.
+ *
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef _DW_I2C_HARDWARE_H
+#define _DW_I2C_HARDWARE_H
+
+
+#define DW_CLK_STANDARD		100000
+#define DW_CLK_FAST			400000
+#define DW_CLK_HIGH			1000000
+
+
+#define DW_IC_CON				0x00
+#define DW_IC_TAR				0x04
+#define DW_IC_SAR				0x08
+#define DW_IC_DATA_CMD			0x10
+#define DW_IC_SS_SCL_HCNT		0x14
+#define DW_IC_SS_SCL_LCNT		0x18
+#define DW_IC_FS_SCL_HCNT		0x1c
+#define DW_IC_FS_SCL_LCNT		0x20
+#define DW_IC_HS_SCL_HCNT		0x24
+#define DW_IC_HS_SCL_LCNT		0x28
+#define DW_IC_INTR_STAT			0x2c
+#define DW_IC_INTR_MASK			0x30
+#define DW_IC_RX_TL				0x38
+#define DW_IC_TX_TL				0x3c
+#define DW_IC_CLR_INTR			0x40
+#define DW_IC_CLR_RX_UNDER		0x44
+#define DW_IC_CLR_RX_OVER		0x48
+#define DW_IC_CLR_TX_OVER		0x4c
+#define DW_IC_CLR_RD_REQ		0x50
+#define DW_IC_CLR_TX_ABRT		0x54
+#define DW_IC_CLR_RX_DONE		0x58
+#define DW_IC_CLR_ACTIVITY		0x5c
+#define DW_IC_CLR_STOP_DET		0x60
+#define DW_IC_CLR_START_DET		0x64
+#define DW_IC_CLR_GEN_CALL		0x68
+
+
+#define DW_IC_ENABLE			0x6c
+#define DW_IC_STATUS			0x70
+#define DW_IC_TXFLR				0x74
+#define DW_IC_RXFLR				0x78
+#define DW_IC_SDA_HOLD			0x7c
+#define DW_IC_ENABLE_STATUS		0x9c
+#define DW_IC_FS_SPKLEN			0xa0
+#define DW_IC_COMP_PARAM1		0xf4
+#define DW_IC_COMP_VERSION		0xf8
+#define DW_IC_COMP_TYPE			0xfc
+
+#define DW_IC_COMP_PARAM1_RX(x)	(1 + (((x) >> 8) & 0xff))
+#define DW_IC_COMP_PARAM1_TX(x)	(1 + (((x) >> 16) & 0xff))
+
+
+#define DW_IC_CON_MASTER				(1 << 0)
+#define DW_IC_CON_SPEED_STANDARD			0x1
+#define DW_IC_CON_SPEED_FAST				0x2
+#define DW_IC_CON_SPEED_FAST_PLUS			0x2
+#define DW_IC_CON_SPEED_HIGH				0x3
+#define DW_IC_CON_10BITADDR_SLAVE		(1 << 3)
+#define DW_IC_CON_10BITADDR_MASTER		(1 << 4)
+#define DW_IC_CON_RESTART_EN			(1 << 5)
+#define DW_IC_CON_SLAVE_DISABLE			(1 << 6)
+
+#define DW_IC_DATA_CMD_READ				(1 << 8)
+#define DW_IC_DATA_CMD_STOP				(1 << 9)
+#define DW_IC_DATA_CMD_RESTART			(1 << 10)
+
+#define DW_IC_INTR_STAT_RX_UNDER		(1 << 0)
+#define DW_IC_INTR_STAT_RX_OVER			(1 << 1)
+#define DW_IC_INTR_STAT_RX_FULL			(1 << 2)
+#define DW_IC_INTR_STAT_TX_OVER			(1 << 3)
+#define DW_IC_INTR_STAT_TX_EMPTY		(1 << 4)
+#define DW_IC_INTR_STAT_RD_REQ			(1 << 5)
+#define DW_IC_INTR_STAT_TX_ABRT			(1 << 6)
+#define DW_IC_INTR_STAT_RX_DONE			(1 << 7)
+#define DW_IC_INTR_STAT_ACTIVITY		(1 << 8)
+#define DW_IC_INTR_STAT_STOP_DET		(1 << 9)
+#define DW_IC_INTR_STAT_START_DET		(1 << 10)
+#define DW_IC_INTR_STAT_GEN_CALL		(1 << 11)
+#define DW_IC_INTR_STAT_RESTART_DET		(1 << 12)
+#define DW_IC_INTR_STAT_MST_ON_HOLD		(1 << 13)
+
+#define DW_IC_STATUS_MASTER_ACTIVITY	(1 << 5)
+
+
+
+#endif // _DW_I2C_HARDWARE_H
