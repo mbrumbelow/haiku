9fca7a8deed25b48c6318937e7a8e5045c0dc35a
Niklas Poslovski <ni.pos@yandex.com>
2025-01-05 18:16:06 +0100

    PlaySound: Replace placeholder with actual playback
    It's not feature-complete yet and may need further refactoring to
    enable support for willMix and asynchronous willWait, but it's
    probably good enough to enable support for short sound effects in apps.
    
    Change-Id: Ib3f7bc1fc7d2b267f0194d72d3c6d7fb331e653b

--- src/kits/media/PlaySound.cpp
+++ src/kits/media/PlaySound.cpp
@@ -8,8 +8,29 @@
 #include <OS.h>
 #include <Entry.h>
 
+#include <MediaFile.h>
+#include <MediaTrack.h>
 #include <PlaySound.h>
-#include "MediaDebug.h"
+#include <SoundPlayer.h>
+
+sem_id finished = -1;
+BMediaTrack* playTrack;
+media_format playFormat;
+BSoundPlayer* player = 0;
+
+void
+play_buffer(void *cookie, void *buffer, size_t size, const media_raw_audio_format &format)
+{
+	int64 frames = 0;
+
+	playTrack->ReadFrames(buffer, &frames);
+
+	if (frames <= 0) {
+		player->SetHasData(false);
+		release_sem(finished);
+	}
+}
+
 
 sound_handle play_sound(const entry_ref *soundRef,
 						bool mix,
@@ -17,18 +38,61 @@ sound_handle play_sound(const entry_ref *soundRef,
 						bool background
 						)
 {
-	UNIMPLEMENTED();
-	return (sound_handle)1;
+	BMediaFile* playFile = new BMediaFile(soundRef);
+
+	if (playFile->InitCheck() != B_OK) {
+		delete playFile;
+		return -1;
+	}
+
+	// FIXME: Mixing multiple sounds is not implemented yet, the argument is ignored.
+
+	if (player != 0 && player->HasData()) {
+		if (queue) {
+			// FIXME: This can't work without blocking at least until
+			// the previous playback completed.
+			acquire_sem(finished);
+		} else {
+			player->Stop();
+			release_sem(finished);
+		}
+	}
+
+	for (int i = 0; i < playFile->CountTracks(); i++) {
+		BMediaTrack* track = playFile->TrackAt(i);
+		if (track != NULL) {
+			playFormat.type = B_MEDIA_RAW_AUDIO;
+			if ((track->DecodedFormat(&playFormat) == B_OK)
+				&& (playFormat.type == B_MEDIA_RAW_AUDIO)) {
+				playTrack = track;
+				break;
+			}
+			playFile->ReleaseTrack(track);
+		}
+	}
+
+	finished = create_sem(0, "finish wait");
+
+	player = new BSoundPlayer(&playFormat.u.raw_audio, "playfile", play_buffer);
+	player->SetVolume(1.0f);
+	player->SetHasData(true);
+	player->Start();
+
+	if (!background)
+		acquire_sem(finished);
+
+	return (sound_handle)finished;
 }
 
 status_t stop_sound(sound_handle handle)
 {
-	UNIMPLEMENTED();
+	player->Stop();
+	release_sem(handle);
 	return B_OK;
 }
 
 status_t wait_for_sound(sound_handle handle)
 {
-	UNIMPLEMENTED();
+	acquire_sem(handle);
 	return B_OK;
 }
