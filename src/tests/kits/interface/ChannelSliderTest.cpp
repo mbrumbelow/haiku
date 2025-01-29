87bfd3af9c307bb96c25499b8a45c008801cffbf
Pascal Abresch <nep-git@packageloss.eu>
2025-01-29 17:32:01 +0100

    tests interface: ChannelSliderTest use layout
    The BChannelSlider itself doesn't seem to work all too well in this
    layout, however the previous version was also wierdly intersecting.
    So the right solution here is likely to fix it to behave nicely, in this
    layout :)
    
    Change-Id: I6a2f9a9166b143ea685ea27aff3ee763515965d9

--- src/tests/kits/interface/ChannelSliderTest.cpp
+++ src/tests/kits/interface/ChannelSliderTest.cpp
@@ -1,6 +1,8 @@
 #include <Application.h>
 #include <ChannelSlider.h>
+#include <View.h>
 #include <Window.h>
+#include <LayoutBuilder.h>
 
 #include <string>
 
@@ -23,45 +25,41 @@ public:
 	MainWindow()
 		:BWindow(BRect(50, 50, 250, 360), "window", B_TITLED_WINDOW, B_ASYNCHRONOUS_CONTROLS)
 	{
-		BChannelSlider *slider = new BChannelSlider(BRect(10, 10, 20, 20),
+		BChannelSlider *verticalSlider = new BChannelSlider(BRect(10, 10, 20, 20),
 			"vertical slider", "Verticalp", new BMessage('test'), 4);
-		slider->SetOrientation(B_VERTICAL);
-		AddChild(slider);
-		slider->ResizeToPreferred();
+		verticalSlider->SetOrientation(B_VERTICAL);
 
-		slider = new BChannelSlider(BRect(10, 10, 20, 20), "vertical slider",
+		BChannelSlider *limitVerticalSlider = new BChannelSlider(BRect(10, 10, 20, 20), "vertical slider",
 			"Verticalp", new BMessage('test'), B_VERTICAL,  4);
-		AddChild(slider);
-		slider->SetLimitLabels("Wminp", "Wmaxp");
-		slider->ResizeToPreferred();
-		slider->MoveBy(slider->Bounds().Width() + 10.0, 0.0);
+		limitVerticalSlider->SetLimitLabels("Wminp", "Wmaxp");
 
-		BChannelSlider *horizontal = new BChannelSlider(BRect(150, 10, 160, 20),
+		BChannelSlider *horizontalSlider = new BChannelSlider(BRect(150, 10, 160, 20),
 			 "horizontal slider", "Horizontal", new BMessage('test'), 3);
-		AddChild(horizontal);
-		horizontal->ResizeToPreferred();
 
-		horizontal = new BChannelSlider(BRect(150, 10, 160, 20),
+		BChannelSlider *limitHorizontalSlider = new BChannelSlider(BRect(150, 10, 160, 20),
 			 "horizontal slider", "Horizontalp", new BMessage('test'),
 			 B_HORIZONTAL, 3);
-		AddChild(horizontal);
-		horizontal->SetLimitLabels("Wminp", "Wmaxp");
-		horizontal->ResizeToPreferred();
-		horizontal->MoveBy(0.0, horizontal->Bounds().Height() + 10.0);
+		limitHorizontalSlider->SetLimitLabels("Wminp", "Wmaxp");
 
-		ResizeTo(horizontal->Frame().right + 10, slider->Frame().bottom + 10);
-
-		for (int32 i = 0; i < horizontal->CountChannels(); i++) {
-			horizontal->SetLimitLabelsFor(i, kLabels[i].min_label.c_str(),
+		for (int32 i = 0; i < horizontalSlider->CountChannels(); i++) {
+			horizontalSlider->SetLimitLabelsFor(i, kLabels[i].min_label.c_str(),
 												kLabels[i].max_label.c_str());
 		}
 
-		for (int32 i = 0; i < horizontal->CountChannels(); i++) {
-			if (strcmp(horizontal->MinLimitLabelFor(i), kLabels[i].min_label.c_str()) != 0)
+		for (int32 i = 0; i < horizontalSlider->CountChannels(); i++) {
+			if (strcmp(horizontalSlider->MinLimitLabelFor(i), kLabels[i].min_label.c_str()) != 0)
 				printf("wrong min label for channel %ld\n", i);
-			if (strcmp(horizontal->MaxLimitLabelFor(i), kLabels[i].max_label.c_str()) != 0)
+			if (strcmp(horizontalSlider->MaxLimitLabelFor(i), kLabels[i].max_label.c_str()) != 0)
 				printf("wrong max label for channel %ld\n", i);
 		}
+
+		BLayoutBuilder::Group<>(this, B_VERTICAL)
+			.SetInsets(0, 0, 0, 0)
+			.AddGrid()
+				.Add(verticalSlider, 0, 0)
+				.Add(limitVerticalSlider, 1, 0)
+				.Add(horizontalSlider, 0, 1)
+				.Add(limitHorizontalSlider, 1, 1);
 	}
 
 	virtual bool QuitRequested() { be_app->PostMessage(B_QUIT_REQUESTED); return BWindow::QuitRequested() ; }
