829b4fd744703f60e2714251fd7a119f2619d105
John Scipione <jscipione@gmail.com>
2025-01-30 02:16:50 -0500

    Tracker: Fix off by one label redraw issues
    Most noticable in mini-icon mode but the label is off by a pixel
    vertically in other modes as well. X512's ActualFontHeight() calculation
    (from hrev57736) is good but there was an off-by-one bug in BTextWidget.
    
    A small refactor is included -- rename _IconRect() to _ListIconRect() to
    reflect its use in list-mode, create a new _IconRect() method that works
    in icon-mode and create a new _MiniIconRect() method that works in
    mini-icon mode. _MiniIconRect() is is used in two places: PointInPose()
    and CalcRect().
    
    Fixes #18826
    
    Change-Id: If96ce2089cbf8345d147d39b091317168bcf5c1a

--- src/kits/tracker/Pose.h
+++ src/kits/tracker/Pose.h
@@ -135,8 +135,9 @@ private:
 	void EditPreviousNextWidgetCommon(BPoseView* poseView, bool next);
 	void CreateWidgets(BPoseView*);
 
-			BRect				_IconRect(const BPoseView* poseView,
-									BPoint location) const;
+	BRect _ListIconRect(const BPoseView* poseView, BPoint location) const;
+	BRect _IconRect(const BPoseView* poseView, BPoint location) const;
+	BRect _MiniIconRect(const BPoseView* poseView, BPoint location) const;
 
 	Model* fModel;
 	BObjectList<BTextWidget> fWidgetList;
