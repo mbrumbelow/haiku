86875d366bb21cc08e8ae158c74c5383a4097198
John Scipione <jscipione@gmail.com>
2025-02-13 23:31:42 -0500

    Tracker: Take precautions against rounded invert rect, fixes #18826
    Change-Id: Ic4a33b960d8be6c5242d66f21aae2e0184c5cbc8

--- src/kits/tracker/TextWidget.cpp
+++ src/kits/tracker/TextWidget.cpp
@@ -628,7 +628,7 @@ BTextWidget::SelectAll(BPoseView* view)
 
 
 void
-BTextWidget::Draw(BRect eraseRect, BRect textRect, BPoseView* view, BView* drawView,
+BTextWidget::Draw(BRect invertRect, BRect textRect, BPoseView* view, BView* drawView,
 	bool selected, uint32 clipboardMode, BPoint offset)
 {
 	ASSERT(view != NULL);
@@ -655,7 +655,12 @@ BTextWidget::Draw(BRect eraseRect, BRect textRect, BPoseView* view, BView* drawV
 			if (direct) {
 				// erase selection rect background
 				drawView->SetDrawingMode(B_OP_COPY);
-				drawView->FillRect(textRect, B_SOLID_LOW);
+				BRect invertRect(textRect);
+				invertRect.left = ceilf(invertRect.left);
+				invertRect.top = ceilf(invertRect.top);
+				invertRect.right = floorf(invertRect.right);
+				invertRect.bottom = floorf(invertRect.bottom);
+				drawView->FillRect(invertRect, B_SOLID_LOW);
 			}
 			drawView->SetDrawingMode(B_OP_OVER);
 
