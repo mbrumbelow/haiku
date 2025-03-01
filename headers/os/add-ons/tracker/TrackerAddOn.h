4bf35f9788821557e8effa7ec7f08f0fd9f32eda
John Scipione <jscipione@gmail.com>
2025-03-01 02:25:00 -0500

    Tracker: export B_MESSAGE_LOAD_ADDON in TrackerAddOn.h
    ... for add-ons to respond to on load.
    
    be_message_load_addon is also defined for use in the C API.
    
    Change-Id: Iad2ff09a4d88c1018c6b8fea326ce7b53a622dd6

--- headers/os/add-ons/tracker/TrackerAddOn.h
+++ headers/os/add-ons/tracker/TrackerAddOn.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, Haiku, Inc.
+ * Copyright (c) 2007-2025, Haiku, Inc. All rights reserved.
  * Distributed under the terms of the MIT license.
  */
 #ifndef _TRACKER_ADDON_H
@@ -11,9 +11,12 @@ class BMenu;
 class BHandler;
 
 extern "C" {
+	static const uint32 be_message_load_addon = 'Tlda';
 	void process_refs(entry_ref directory, BMessage* refs, void* reserved);
 	void populate_menu(BMessage* msg, BMenu* menu, BHandler* handler);
 	void message_received(BMessage* msg);
 }
+
+static const uint32 B_MESSAGE_LOAD_ADDON = be_message_load_addon;
 
 #endif	// _TRACKER_ADDON_H
