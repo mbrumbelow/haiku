28314bd9726c03d34001d868f2d6760b834fb1fe
PawanYr <pawan.yerramilli@gmail.com>
2025-03-17 00:49:14 -0400

    Screenshot: Add select area option
    Add 'Select area' button to Screenshot that when clicked, allows the
    use of crosshairs to select a region to screenshot; functionally similar
    to and inspired by Snipping Tool/Win+Shift+S on Windows and Cmd+Ctrl+4
    on Mac when selecting using the primary mouse button. When using the
    secondary mouse button or clicking while shift is held, the selected
    area can be resized and moved, and confirmed by pressing enter or double
    clicking.
    
    Also, add a shortcut such that if the super key is held when prtscrn is
    pressed, Screenshot will be launched in area select mode.
    
    Also, add radio buttons to select between whole screen, active window,
    and selected area, and disable active window, window border, and
    selected area options if inapplicable.
    
    See #18568 and #13456
    
    Change-Id: Ida0f441e65154c9cd7788de32a606ab4ca8dc31e

---
+++ src/apps/screenshot/SelectAreaView.cpp
@@ -0,0 +1,226 @@
+/*
+ * Copyright 2025, Haiku, Inc.
+ * Authors:
+ *     Pawan Yerramilli <me@pawanyerramilli.com>
+ * All rights reserved. Distributed under the terms of the MIT license.
+ */
+
+
+#include "SelectAreaView.h"
+
+#include <Application.h>
+#include <Bitmap.h>
+#include <Screen.h>
+#include <Window.h>
+
+#include "Cursor.h"
+#include "InterfaceDefs.h"
+#include "Point.h"
+#include "Rect.h"
+#include "Utility.h"
+
+
+SelectAreaView::SelectAreaView(BBitmap* frame)
+	: BView(BScreen().Frame(), "", B_FOLLOW_NONE, B_WILL_DRAW | B_NAVIGABLE),
+	fScreenShot(frame),
+	fCursor(BCursor(B_CURSOR_ID_CROSS_HAIR)),
+	fIsCurrentlyDragging(false),
+	fStartCorner(B_ORIGIN),
+	fEndCorner(B_ORIGIN),
+	fResizable(false),
+	fCurrentHandle(DRAG_NO_HANDLE),
+	fMoveDelta(-1, -1)
+{
+}
+
+void
+SelectAreaView::AttachedToWindow()
+{
+	MakeFocus(true);
+	BBitmap* darkenedShot = new BBitmap(Bounds(), fScreenShot->ColorSpace(), true);
+	BView* darkenedView = new BView(darkenedShot->Bounds(), "", B_FOLLOW_NONE, 0);
+	darkenedShot->AddChild(darkenedView);
+	darkenedShot->Lock();
+	darkenedView->DrawBitmap(fScreenShot);
+	darkenedView->SetDrawingMode(B_OP_ALPHA);
+	darkenedView->SetHighColor(0, 0, 0, 128);
+	darkenedView->FillRect(Bounds());
+	darkenedView->Sync();
+	SetViewBitmap(darkenedShot);
+	darkenedShot->RemoveChild(darkenedView);
+	delete darkenedView;
+	delete darkenedShot;
+}
+
+void
+SelectAreaView::Draw(BRect updateRect)
+{
+	BRect frame = _CurrentFrame();
+	SetViewCursor(&fCursor);
+	DrawBitmap(fScreenShot, frame, frame);
+
+	if (fResizable && frame.Width() > 20 && frame.Height() > 20) {
+		FillRect(_GetHandle(DRAG_TOP_LEFT));
+		FillRect(_GetHandle(DRAG_TOP_RIGHT));
+		FillRect(_GetHandle(DRAG_BOTTOM_LEFT));
+		FillRect(_GetHandle(DRAG_BOTTOM_RIGHT));
+	}
+}
+
+void
+SelectAreaView::MessageReceived(BMessage* message)
+{
+	int32 clicks = 0;
+	if (message->what == B_MOUSE_DOWN && message->FindInt32("clicks",  &clicks) == B_OK
+		&& clicks == 2)
+		_SaveSelection();
+	else
+		BView::MessageReceived(message);
+}
+
+void
+SelectAreaView::KeyDown(const char* bytes, int32 numBytes)
+{
+	if (bytes[0] == B_ESCAPE)
+		be_app->PostMessage(B_QUIT_REQUESTED);
+	else if (bytes[0] == B_ENTER)
+		_SaveSelection();
+}
+
+
+void
+SelectAreaView::MouseDown(BPoint point)
+{
+	BPoint mouseLoc;
+	uint32 buttons;
+	GetMouse(&mouseLoc, &buttons);
+	uint32 shiftDown = modifiers() & B_SHIFT_KEY;
+	Handle handle = _FindSelectedHandle(point);
+
+	if (fStartCorner == fEndCorner) {
+		fResizable = (buttons & B_SECONDARY_MOUSE_BUTTON) || shiftDown;
+		fIsCurrentlyDragging = true;
+		fStartCorner = fEndCorner = point;
+	} else if (fResizable && handle != DRAG_NO_HANDLE)
+		fCurrentHandle = handle;
+	else if (fResizable && _CurrentFrame().Contains(point)) {
+		fMoveDelta = point;
+		fCursor = B_CURSOR_ID_GRABBING;
+	}
+
+	Invalidate();
+}
+
+void
+SelectAreaView::MouseMoved(BPoint point, uint32 transit, const BMessage* message)
+{
+	if (fIsCurrentlyDragging && point != fEndCorner)
+		fEndCorner = point;
+
+	if (fResizable && (_FindSelectedHandle(point) != DRAG_NO_HANDLE
+		|| !_CurrentFrame().Contains(point)))
+		fCursor = B_CURSOR_ID_SYSTEM_DEFAULT;
+	else if (fMoveDelta != BPoint(-1, -1))
+		fCursor = B_CURSOR_ID_GRABBING;
+	else if (fResizable && _CurrentFrame().Contains(point))
+		fCursor = B_CURSOR_ID_GRAB;
+	else
+		fCursor = B_CURSOR_ID_CROSS_HAIR;
+
+	if (fCurrentHandle != DRAG_NO_HANDLE) {
+		switch(fCurrentHandle) {
+			case DRAG_TOP_LEFT:
+				fStartCorner = point;
+				break;
+			case DRAG_TOP_RIGHT:
+				fStartCorner.y = point.y;
+				fEndCorner.x = point.x;
+				break;
+			case DRAG_BOTTOM_LEFT:
+				fStartCorner.x = point.x;
+				fEndCorner.y = point.y;
+				break;
+			case DRAG_BOTTOM_RIGHT:
+				fEndCorner = point;
+			default:
+				break;
+		}
+	}
+
+	if (fMoveDelta != BPoint(-1, -1)) {
+		fStartCorner += (point - fMoveDelta);
+		fEndCorner += (point - fMoveDelta);
+		fStartCorner.ConstrainTo(Window()->Frame());
+		fEndCorner.ConstrainTo(Window()->Frame());
+		fMoveDelta = point;
+	}
+
+	Invalidate();
+}
+
+void
+SelectAreaView::MouseUp(BPoint point)
+{
+	if (!fResizable && fIsCurrentlyDragging && fStartCorner != fEndCorner)
+		_SaveSelection();
+	else if (fResizable) {
+		fIsCurrentlyDragging = false;
+		BRect frame = _CurrentFrame();
+		fStartCorner = frame.LeftTop();
+		fEndCorner = frame.RightBottom();
+		fCurrentHandle = DRAG_NO_HANDLE;
+		if (fMoveDelta != BPoint(-1, -1))
+			fCursor = B_CURSOR_ID_GRAB;
+		fMoveDelta = BPoint(-1, -1);
+		Invalidate();
+	}
+}
+
+void
+SelectAreaView::_SaveSelection()
+{
+	BMessage message;
+	message.what = SS_SELECT_AREA_FRAME;
+	message.AddRect("selectAreaFrame", _CurrentFrame());
+	be_app->PostMessage(&message);
+	Window()->Quit();
+}
+
+BRect
+SelectAreaView::_CurrentFrame(bool zeroed)
+{
+	BPoint topLeft = BPoint(min_c(fStartCorner.x, fEndCorner.x),
+		min_c(fStartCorner.y, fEndCorner.y));
+	BPoint bottomRight = BPoint(max_c(fStartCorner.x, fEndCorner.x),
+		max_c(fStartCorner.y, fEndCorner.y));
+	return zeroed ? BRect(B_ORIGIN, bottomRight - topLeft) : BRect(topLeft, bottomRight);
+}
+
+Handle
+SelectAreaView::_FindSelectedHandle(BPoint point)
+{
+	for (int i = DRAG_TOP_LEFT; i < DRAG_NO_HANDLE; i++) {
+		if (_GetHandle((Handle)i).Contains(point))
+			return (Handle)i;
+	}
+
+	return DRAG_NO_HANDLE;
+}
+
+BRect
+SelectAreaView::_GetHandle(Handle handle)
+{
+	BRect frame = _CurrentFrame();
+	switch (handle) {
+		case DRAG_TOP_LEFT:
+			return BRect(frame.LeftTop(), frame.LeftTop() + BPoint(8, 8));
+		case DRAG_TOP_RIGHT:
+			return BRect(frame.RightTop() - BPoint(8, 0), frame.RightTop() + BPoint(0, 8));
+		case DRAG_BOTTOM_LEFT:
+			return BRect(frame.LeftBottom() - BPoint(0, 8), frame.LeftBottom() + BPoint(8, 0));
+		case DRAG_BOTTOM_RIGHT:
+			return BRect(frame.RightBottom() - BPoint(8, 8), frame.RightBottom());
+		default:
+			return BRect(-1, -1, -1, -1);
+	}
+}
