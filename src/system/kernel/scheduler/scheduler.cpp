6d61109788d5773e4e878d59a0667767fbd18fd8
Jérôme Duval <jerome.duval@gmail.com>
2025-04-01 23:19:53 +0200

    kernel & libbsd.so: add getloadavg() and _user_get_loadavg()
    the algorithm and reference values are from FreeBSD.

--- src/system/kernel/scheduler/scheduler.cpp
+++ src/system/kernel/scheduler/scheduler.cpp
@@ -74,6 +74,14 @@ static scheduler_mode_operations* sSchedulerModes[] = {
 static int32* sCPUToCore;
 static int32* sCPUToPackage;
 
+// load average algorithm from FreeBSD, see kern_sync.c
+const static int kFShift = 11;
+const static long kFScale = 1 << kFShift;
+struct loadavg sAverageRunnable = { { 0, 0, 0}, kFScale };
+const static uint64 sCExp[3] = { (uint64)(0.9200444146293232 * kFScale),
+	(uint64)(0.9834714538216174 * kFScale), (uint64)(0.9944598480048967 * kFScale) };
+static struct timer sLoadavgTimer = { 0 };
+
 
 static void enqueue(Thread* thread, bool newOne);
 
@@ -762,6 +770,30 @@ scheduler_init()
 }
 
 
+static int32
+_LoadavgUpdate(struct timer* timer)
+{
+	uint64 threadCount = 0;
+	for (int i = 0; i < gCoreCount; i++)
+		threadCount += gCoreEntries[i].ThreadCount();
+	for (int i = 0; i < 3; i++) {
+		sAverageRunnable.ldavg[i] = (sCExp[i] * sAverageRunnable.ldavg[i]
+			+ threadCount * kFScale * (kFScale - sCExp[i])) >> kFShift;
+	}
+	add_timer(timer, &_LoadavgUpdate, 5000000, B_ONE_SHOT_RELATIVE_TIMER);
+
+	return B_HANDLED_INTERRUPT;
+}
+
+
+status_t
+scheduler_loadavg_init()
+{
+	add_timer(&sLoadavgTimer, &_LoadavgUpdate, 5000000, B_ONE_SHOT_RELATIVE_TIMER);
+	return B_OK;
+}
+
+
 void
 scheduler_enable_scheduling()
 {
@@ -873,3 +905,16 @@ _user_get_scheduler_mode()
 	return gCurrentModeID;
 }
 
+
+status_t
+_user_get_loadavg(struct loadavg* userInfo, size_t size)
+{
+	if (userInfo == NULL || !IS_USER_ADDRESS(userInfo))
+		return B_BAD_ADDRESS;
+	if (size != sizeof(struct loadavg))
+		return B_BAD_VALUE;
+	if (user_memcpy(userInfo, &sAverageRunnable, sizeof(struct loadavg)) < B_OK)
+			return B_BAD_ADDRESS;
+
+	return B_OK;
+}
